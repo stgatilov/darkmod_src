430da9c74baa4025b2d9c5f3026d22749e942543
sophisticatedzombie <sophisticatedzombie@49c82d7f-2e2a-0410-a16f-ae8f201b507f>
2006-05-17 05:38:09 +0000

    These modules implement the CAIComm_Stim class which acts as an outbound Stim interface for CAIComm_Messages; and CAIComm_Response which handles AI responses to overheard messages.
    git-svn-id: http://svn.thedarkmod.com/svn/darkmod_src/trunk@414 49c82d7f-2e2a-0410-a16f-ae8f201b507f

---
+++ DarkMod/AIComm_StimResponse.cpp
@@ -0,0 +1,319 @@
+#include "../idlib/precompiled.h"
+#pragma hdrstop
+
+#include "DarkModGlobals.h"
+#include "AIComm_StimResponse.h"
+
+#define MAX_COMMUNICATION_RADIUS 5000.0
+
+/*
+**************************************************************************
+* CAIComm_Response
+**************************************************************************
+*/
+
+/*----------------------------------------------------------------*/
+
+CAIComm_Response::CAIComm_Response(idEntity* Owner, int Type) : CResponse (Owner, Type)
+{
+}
+
+/*----------------------------------------------------------------*/
+
+CAIComm_Response::~CAIComm_Response(void)
+{
+}
+
+/*----------------------------------------------------------------*/
+
+void CAIComm_Response::TriggerResponse(idEntity *StimEnt)
+{
+	DM_LOG(LC_STIM_RESPONSE, LT_DEBUG)LOGSTRING("Response for Id %s triggered (Action: %s)\r", m_StimTypeName.c_str(), m_ScriptFunction.c_str());
+
+	// Get the communications stim
+	CAIComm_Stim* p_CommStim = NULL;
+
+	CStimResponseCollection* p_StimResponseCollection = StimEnt->GetStimResponseCollection();
+	if (p_StimResponseCollection == NULL)
+	{
+		DM_LOG(LC_STIM_RESPONSE, LT_DEBUG)LOGSTRING("Entity has no stim response collection\r");
+		return;
+	}
+	else
+	{
+		CStim* p_stim = p_StimResponseCollection->GetStim (ST_COMMUNICATION);
+		if (p_stim == NULL)
+		{
+			DM_LOG(LC_STIM_RESPONSE, LT_DEBUG)LOGSTRING("Source entity has no communications stim\r");
+			return;
+		}
+		p_CommStim = (CAIComm_Stim*) p_stim;
+	}
+
+	// Get the number of messages
+	unsigned long numMessages = p_CommStim->getNumMessages();
+
+	// Get the script function specified in this response
+	const function_t *pScriptFkt = m_Owner->scriptObject.GetFunction(m_ScriptFunction.c_str());
+	if(pScriptFkt == NULL)
+	{
+		DM_LOG(LC_STIM_RESPONSE, LT_DEBUG)LOGSTRING("Action: %s not found in local space, checking for global.\r", m_ScriptFunction.c_str());
+		pScriptFkt = gameLocal.program.FindFunction(m_ScriptFunction.c_str());
+		if (pScriptFkt == NULL)
+		{
+			DM_LOG(LC_STIM_RESPONSE, LT_DEBUG)LOGSTRING("Action: %s not found in global space\r", m_ScriptFunction.c_str());
+			return;
+
+		}
+	}
+
+	// If there is a script function in the response
+	if(pScriptFkt)
+	{
+		unsigned long iterationHandle;
+		CAIComm_Message* p_message = NULL;
+		p_message = p_CommStim->getFirstMessage (iterationHandle);
+
+		// Call the script function for each of the messages
+		while (p_message != NULL)
+		{
+			DM_LOG(LC_STIM_RESPONSE, LT_DEBUG)LOGSTRING("Got message to respond\r");
+			
+			// Get the message parameters
+			CAIComm_Message::TCommType commType = p_message->getCommunicationType();
+			float maxRadiusForResponse = p_message->getMaximumRadiusInWorldCoords();
+			idEntity* p_issuingEntity = p_message->getIssuingEntity();
+			idEntity* p_recipientEntity = p_message->getRecipientEntity();
+			idEntity* p_directObjectEntity = p_message->getDirectObjectEntity();
+			idVec3 directObjectLocation = p_message->getDirectObjectLocation();
+			idVec3 positionOfIssuance = p_message->getPositionOfIssuance();
+
+			// Calculate distance of the owner of the response from the position of the message at issuance
+			float distanceFromIssuance = maxRadiusForResponse + 1.0;
+			
+			idPhysics* p_phys = m_Owner->GetPhysics();
+			if (p_phys != NULL)
+			{
+				idVec3 ownerOrigin = p_phys->GetOrigin();
+				distanceFromIssuance = (ownerOrigin - positionOfIssuance).Length();
+			}
+
+			// Only respond if response owner is within maximum radius of issuance position
+			if (distanceFromIssuance <= maxRadiusForResponse)
+			{
+				idThread *pThread = new idThread(pScriptFkt);
+				int n = pThread->GetThreadNum();
+
+				DM_LOG(LC_STIM_RESPONSE, LT_DEBUG)LOGSTRING("Running AIComm_StimResponse ResponseScript %s, threadID = %d, commType = %d \r", m_ScriptFunction.c_str(), n, commType);
+
+				pThread->CallFunctionArgs
+				(
+					pScriptFkt, 
+					true, 
+					"eeffeeev", 
+					m_Owner, 
+					StimEnt, 
+					(float) n,
+					(float) commType,
+					p_issuingEntity,
+					p_recipientEntity,
+					p_directObjectEntity,
+					&directObjectLocation
+				);
+
+				// Run the response script
+				pThread->DelayedStart(0);
+
+				DM_LOG(LC_STIM_RESPONSE, LT_DEBUG)LOGSTRING("Done running AIComm_StimResponse ResponseScript %s\r", m_ScriptFunction.c_str());
+
+			} // Responder was within maximum radius of message
+
+			// Get next message
+			p_message = p_CommStim->getNextMessage (iterationHandle);
+
+		} // Loop until no more messages in Stim
+	}
+	else
+	{
+		DM_LOG(LC_STIM_RESPONSE, LT_ERROR)LOGSTRING("ResponseActionScript not found! [%s]\r", m_ScriptFunction.c_str());
+	}
+
+	// Continue the chain if we have a followup response to be triggered.
+	if(m_FollowUp != NULL)
+	{
+		DM_LOG(LC_STIM_RESPONSE, LT_DEBUG)LOGSTRING("Followup: %08lX\r", m_FollowUp);
+		m_FollowUp->TriggerResponse(StimEnt);
+	}
+}
+
+/*
+**************************************************************************
+* CAIComm_Stim
+**************************************************************************
+*/
+
+CAIComm_Stim::CAIComm_Stim (idEntity* Owner, int Type) : CStim (Owner, Type)
+{
+	messageCount = 0;
+	p_firstMessage = NULL;
+	p_lastMessage = NULL;
+
+	// Set radius
+	m_Radius = MAX_COMMUNICATION_RADIUS;
+
+}
+
+/*-------------------------------------------------------------------------*/
+
+CAIComm_Stim::~CAIComm_Stim(void)
+{
+	clearMessages();
+}
+
+/*-------------------------------------------------------------------------*/
+
+void CAIComm_Stim::clearMessages()
+{
+	TAICommMessageNode* p_temp;
+	TAICommMessageNode* p_cursor = p_firstMessage;
+
+	// Ride the lightning
+	while (p_cursor != NULL)
+	{
+		p_temp = p_cursor->p_next;
+
+		if (p_cursor->p_message != NULL)
+		{
+			delete (p_cursor->p_message);
+		}
+
+		delete p_cursor;
+		p_cursor = p_temp;
+	}
+
+	p_firstMessage = 0;
+	p_lastMessage = 0;
+	messageCount = 0;
+}
+
+/*-------------------------------------------------------------------------*/
+
+unsigned long CAIComm_Stim::getNumMessages()
+{
+	return messageCount;
+}
+
+/*-------------------------------------------------------------------------*/
+
+bool CAIComm_Stim::addMessage
+(
+	CAIComm_Message::TCommType in_commType,
+	float in_maximumRadiusOfResponse,
+	idEntity* in_p_issuingEntity,
+	idEntity* in_p_recipientEntity,
+	idEntity* in_p_directObjectEntity,
+	const idVec3& in_directObjectLocation
+)
+{
+	// Create new node
+	TAICommMessageNode* p_node = new TAICommMessageNode;
+	if (p_node == NULL)
+	{
+		// Out of memory
+		DM_LOG(LC_STIM_RESPONSE, LT_ERROR)LOGSTRING("Out of Memory! Failed to allocate communication message node\r");
+		return false;
+	}
+
+	// Initialize the communication message
+	p_node->p_message = new CAIComm_Message (in_commType, in_maximumRadiusOfResponse, in_p_issuingEntity, in_p_recipientEntity, in_p_directObjectEntity, in_directObjectLocation);
+	if (p_node->p_message == NULL)
+	{
+		// Out of memory
+		DM_LOG(LC_STIM_RESPONSE, LT_ERROR)LOGSTRING("Out of Memory! Failed to allocate communication message\r");
+		return false;
+	}
+
+	// Link node to front of list	
+	p_node->p_next = p_firstMessage;
+	p_node->p_prev = NULL;
+
+	// Link list after node
+	if (p_firstMessage == NULL)
+	{
+		// Was empty list
+		p_firstMessage = p_node;
+		p_lastMessage = p_node;
+
+		// If we were disabled, we are now enabled
+		if (m_State == SS_DISABLED)
+		{
+			m_State = SS_ENABLED;
+		}
+	}
+	else
+	{
+		// Wasn't an empty list
+		p_firstMessage->p_prev = p_node;
+		p_firstMessage = p_node;
+	}
+
+	// One more message
+	messageCount ++;
+
+	// Success
+	return true;
+
+}
+
+/*-------------------------------------------------------------------------*/
+
+CAIComm_Message* CAIComm_Stim::getFirstMessage (unsigned long& out_iterationHandle)
+{
+	// Init iteration cursor
+	out_iterationHandle = (unsigned long) p_firstMessage;
+
+	// Return message
+	if (p_firstMessage != NULL)
+	{
+		return p_firstMessage->p_message;
+	}
+	else
+	{
+		return NULL;
+	}
+}
+
+/*-------------------------------------------------------------------------*/
+
+CAIComm_Message* CAIComm_Stim::getNextMessage (unsigned long& inout_iterationHandle)
+{
+	// Advance iteration cursor
+	TAICommMessageNode* p_cursor = (TAICommMessageNode*) inout_iterationHandle;
+	if (p_cursor != NULL)
+	{
+		p_cursor = p_cursor->p_next;
+	}
+	inout_iterationHandle = (unsigned long) p_cursor;
+
+	// Return message
+	if (p_cursor != NULL)
+	{
+		return p_cursor->p_message;
+	}
+	else
+	{
+		return NULL;
+	}
+}
+
+/*-------------------------------------------------------------------------*/
+
+void CAIComm_Stim::PostFired (int numResponses)
+{
+	// Clear all the messages
+	clearMessages();
+
+	// We are no longer active until a message is added
+	m_State = SS_DISABLED;
+
+}
---
+++ DarkMod/AIComm_StimResponse.h
@@ -0,0 +1,137 @@
+#pragma once
+
+#include "stimresponse.h"
+#include "AIComm_Message.h"
+
+/**
+* This class manifests an AI's reponse link to 
+* a response to a AI Communications Stim
+*/
+class CAIComm_Response:
+	public CResponse
+{
+public:
+	CAIComm_Response(idEntity* Owner, int Type);
+	virtual ~CAIComm_Response(void);
+
+public:
+
+	/**
+	* This override of the base class version
+	* calls a script function of a different format
+	* than the default stim response, and passes
+	* in the communication stim parameters.
+	*/
+	virtual void TriggerResponse(idEntity *Stim);
+
+
+
+};
+
+/*------------------------------------------------------------------*/
+
+typedef struct tagTAICommMessageNode
+{
+	// The message
+	CAIComm_Message* p_message;
+
+	// Node links
+	tagTAICommMessageNode* p_prev;
+	tagTAICommMessageNode* p_next;
+
+} TAICommMessageNode;
+
+/*------------------------------------------------------------------*/
+
+/**
+* This class manifests an AI's outbound communications
+* stim and contains their outbound communication messages.
+*/
+class CAIComm_Stim: 
+	public CStim
+{
+protected:
+
+	// The number of comm messages in the list
+	unsigned long messageCount;
+
+	// List header and trailer pointers
+	TAICommMessageNode* p_firstMessage;
+	TAICommMessageNode* p_lastMessage;
+
+public:
+	/*!
+	* Constructor
+	*/
+	CAIComm_Stim (idEntity* Owner, int Type);
+
+	/*!
+	* Destructor
+	*/
+	virtual ~CAIComm_Stim(void);
+
+	/*!
+	* This method clears all the messages from the list and destroys
+	* them.
+	*/
+	void clearMessages();
+
+	/*!
+	* This method adds a communication message to this stim
+	*
+	* @param in_commType The type of the communication message
+	*
+	* @param in_maximumRadiusOfResponse The maximum radius away that
+	*	the message should be responded to (due to audability)
+	* 
+	* @param in_p_issuingEntity The entity speaking the communication
+	*
+	* @param in_p_recipientEntity The entity intended to hear the communication
+	*
+	* @param in_p_directObjectEntity The entity that the communication is about
+	*
+	* @param in_directObjectLocation The location that the communication is about
+	*
+	*/
+	bool addMessage
+	(
+		CAIComm_Message::TCommType in_commType,
+		float in_maximumRadiusOfResponse,
+		idEntity* in_p_issuingEntity,
+		idEntity* in_p_recipientEntity,
+		idEntity* in_p_directObjectEntity,
+		const idVec3& in_directObjectLocation
+	);
+
+	/**
+	* Get the number of messages in the stim
+	*/
+	unsigned long getNumMessages();
+
+	/**
+	* Gets the first message in the stim
+	*
+	* @return pointer to the message
+	* @return NULL if no messages
+	*/
+	CAIComm_Message* getFirstMessage (unsigned long& out_iterationHandle);
+
+	/**
+	* Gets the next message in the stim.
+	*
+	* @return pointer to the message
+	* @return NULL if no more messages
+	*/
+	CAIComm_Message* getNextMessage (unsigned long& inout_iterationHandle);
+
+	/**
+	* BASE CLASS VIRTUAL OVERRIDE
+	*
+	* This version removes any messages in the list of messages as they
+	* have been distributed.
+	*/
+	virtual void PostFired (int numResponses);
+
+
+};
+
