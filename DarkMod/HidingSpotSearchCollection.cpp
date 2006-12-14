307b4b8fd16f59d064bed58d4a29909efc64dde1
sophisticatedzombie <sophisticatedzombie@49c82d7f-2e2a-0410-a16f-ae8f201b507f>
2006-12-14 09:56:48 +0000

    This is a singleton object class that maintains the current searches and controls their number.
    git-svn-id: http://svn.thedarkmod.com/svn/darkmod_src/trunk@661 49c82d7f-2e2a-0410-a16f-ae8f201b507f

---
+++ DarkMod/HidingSpotSearchCollection.cpp
@@ -0,0 +1,264 @@
+#include "../idlib/precompiled.h"
+#pragma hdrstop
+
+#include ".\hidingspotsearchcollection.h"
+
+//---------------------------------------------------------------------------
+
+#define MAX_NUM_HIDING_SPOT_SEARCHES 4
+
+//--------------------------------------------------------------------
+
+// Global instance
+CHidingSpotSearchCollection HidingSpotSearchCollection;
+
+//--------------------------------------------------------------------
+
+// Constructor
+
+CHidingSpotSearchCollection::CHidingSpotSearchCollection(void)
+{
+	numSearchesInUse = 0;
+	p_firstSearch = NULL;
+}
+
+//--------------------------------------------------------------------
+
+// Destructor
+
+CHidingSpotSearchCollection::~CHidingSpotSearchCollection(void)
+{
+	clear();
+}
+
+//--------------------------------------------------------------------
+
+void CHidingSpotSearchCollection::clear()
+{
+	// Destroy all searches
+	TDarkmodHidingSpotSearchNode* p_cursor = p_firstSearch;
+	TDarkmodHidingSpotSearchNode* p_temp;
+	while (p_cursor != NULL)
+	{
+		p_temp = p_cursor->p_next;
+		delete p_cursor;
+		p_cursor = p_temp;
+	}
+
+	p_firstSearch = NULL;
+
+	// No active searches any more
+	numSearchesInUse = 0;
+
+	// Done
+
+}
+
+//--------------------------------------------------------------------
+
+THidingSpotSearchHandle CHidingSpotSearchCollection::getUnusedSearch()
+{
+	if (numSearchesInUse >= MAX_NUM_HIDING_SPOT_SEARCHES)
+	{
+		return NULL_HIDING_SPOT_SEARCH_HANDLE;
+	}
+	else
+	{
+		TDarkmodHidingSpotSearchNode* p_node = new TDarkmodHidingSpotSearchNode;
+		if (p_node == NULL)
+		{
+			return NULL_HIDING_SPOT_SEARCH_HANDLE;
+		}
+
+		// We are returning to somebody, so they have a reference
+		p_node->refCount = 1;
+
+		p_node->p_prev = NULL;
+		p_node->p_next = p_firstSearch;
+		if (p_firstSearch != NULL)
+		{
+			p_firstSearch->p_prev = p_node;
+		}
+		p_firstSearch = p_node;
+
+		// One more search in use
+		numSearchesInUse ++;
+
+		return (THidingSpotSearchHandle) p_node;
+	}
+	
+}
+
+
+//**********************************************************************
+// Public
+//**********************************************************************
+
+darkModAASFindHidingSpots* CHidingSpotSearchCollection::getSearchByHandle
+(
+	THidingSpotSearchHandle searchHandle
+)
+{
+	TDarkmodHidingSpotSearchNode* p_node = (TDarkmodHidingSpotSearchNode*) searchHandle;
+	if (p_node != NULL)
+	{
+		if (p_node->refCount <= 0)
+		{
+			return NULL;
+		}
+		return &(p_node->search);
+	}
+	else
+	{
+		return NULL;
+	}
+}
+
+//----------------------------------------------------------------------------------
+
+darkModAASFindHidingSpots* CHidingSpotSearchCollection::getSearchAndReferenceCountByHandle
+(
+	THidingSpotSearchHandle searchHandle,
+	unsigned int& out_refCount
+)
+{
+	TDarkmodHidingSpotSearchNode* p_node = (TDarkmodHidingSpotSearchNode*) searchHandle;
+	if (p_node != NULL)
+	{
+		if (p_node->refCount <= 0)
+		{
+			out_refCount = 0;
+			return NULL;
+		}
+
+		out_refCount = p_node->refCount;
+		return &(p_node->search);
+	}
+	else
+	{
+		return NULL;
+	}
+}
+
+
+//------------------------------------------------------------------------------
+
+void CHidingSpotSearchCollection::dereference (THidingSpotSearchHandle searchHandle)
+{
+	TDarkmodHidingSpotSearchNode* p_node = (TDarkmodHidingSpotSearchNode*) searchHandle;
+	if (p_node != NULL)
+	{
+		// Decrement Refcount
+		p_node->refCount --;
+
+		if (p_node->refCount <= 0)
+		{
+			if (p_node->p_prev != NULL)
+			{
+				p_node->p_prev->p_next = p_node->p_next;
+			}
+			else
+			{
+				p_firstSearch = p_node->p_next;
+			}
+
+			if (p_node->p_next != NULL)
+			{
+				p_node->p_next->p_prev = p_node;
+			}
+
+			delete p_node;
+		
+			// One less search
+			numSearchesInUse --;
+		}
+	}
+	
+}
+
+//------------------------------------------------------------------------------
+
+THidingSpotSearchHandle CHidingSpotSearchCollection::findSearchByBounds 
+(
+	idBounds bounds
+)
+{
+	TDarkmodHidingSpotSearchNode* p_node = p_firstSearch;
+	while (p_node != NULL)
+	{
+		idBounds existingBounds = p_node->search.getSearchLimits();
+		if (existingBounds.Compare (bounds, 50.0))
+		{
+			// Reuse this one
+			p_node->refCount ++;
+			return (THidingSpotSearchHandle) p_node;
+		}
+
+		p_node = p_node->p_next;
+	}
+
+	// None found
+	return NULL_HIDING_SPOT_SEARCH_HANDLE;
+
+}
+
+//------------------------------------------------------------------------------
+
+THidingSpotSearchHandle CHidingSpotSearchCollection::getOrCreateSearch
+(
+	const idVec3 &hideFromPos, 
+	idAAS* in_p_aas, 
+	float in_hidingHeight,
+	idBounds in_searchLimits, 
+	int in_hidingSpotTypesAllowed, 
+	idEntity* in_p_ignoreEntity,
+	int frameIndex,
+	bool& out_b_searchCompleted
+)
+{
+	/* Search with same bounds already? */
+	THidingSpotSearchHandle hSearch = findSearchByBounds (in_searchLimits);
+	if (hSearch != NULL_HIDING_SPOT_SEARCH_HANDLE)
+	{
+		darkModAASFindHidingSpots* p_search = getSearchByHandle(hSearch);
+		out_b_searchCompleted = p_search->isSearchCompleted();
+		return hSearch;
+	}
+
+	/* Make new search */
+	hSearch = getUnusedSearch();
+	if (hSearch == NULL_HIDING_SPOT_SEARCH_HANDLE)
+	{
+		return hSearch;
+	}
+
+	/* Initialize the search */
+	darkModAASFindHidingSpots* p_search = getSearchByHandle(hSearch);
+	p_search->initialize
+	(
+		hideFromPos, 
+		in_p_aas, 
+		in_hidingHeight,
+		in_searchLimits, 
+		in_hidingSpotTypesAllowed, 
+		in_p_ignoreEntity
+	);
+
+	// Start search
+	DM_LOG(LC_AI, LT_DEBUG).LogString ("Starting search for hiding spots\n");
+	bool b_moreProcessingToDo = p_search->startHidingSpotSearch
+	(
+		p_search->hidingSpotList,
+		g_Global.m_maxNumHidingSpotPointTestsPerAIFrame,
+		frameIndex
+	);
+	DM_LOG(LC_AI, LT_DEBUG).LogString ("First pass of hiding spot search found %d spots\n", p_search->hidingSpotList.getNumSpots());
+
+	// Is search completed?
+	out_b_searchCompleted = !b_moreProcessingToDo;
+
+	// Search created, return search to caller
+	return hSearch;
+		
+
+}
---
+++ DarkMod/HidingSpotSearchCollection.h
@@ -0,0 +1,138 @@
+#pragma once
+/**
+* The purpose of this class is to contain all the hiding spot searches in use by
+* the AIs in the game.  Hiding spot searches can be shared, so this holds all
+* the searches and controls whether or not new searches are required.  
+*
+* An AI that would like a search done makes a call to the  singleton instance
+* of this class and asks for a search for a given stimulus. If it is simlar
+* enough to an existing search that search is re-used.
+*
+*/
+
+// Required includes
+#include "darkModAASFindHidingSpots.h"
+
+//---------------------------------------------------------------------------
+
+typedef struct tagTDarkmodHidingSpotSearchNode
+{
+	int refCount;
+	darkModAASFindHidingSpots search;
+
+	tagTDarkmodHidingSpotSearchNode* p_prev;
+	tagTDarkmodHidingSpotSearchNode* p_next;
+	
+} TDarkmodHidingSpotSearchNode;
+
+//---------------------------------------------------------------------------
+
+/**
+* This is the handle type by which an AI references searches in which it is 
+* interested.
+*/
+typedef void* THidingSpotSearchHandle;
+
+#define NULL_HIDING_SPOT_SEARCH_HANDLE NULL
+
+//---------------------------------------------------------------------------
+
+class CHidingSpotSearchCollection
+{
+public:
+
+	/**
+	* Constructor
+	*/
+	CHidingSpotSearchCollection(void);
+
+	/**
+	* Destructor
+	*/
+	~CHidingSpotSearchCollection(void);
+
+	/**
+	* This gets a search by its handle
+	*/
+	darkModAASFindHidingSpots* getSearchByHandle
+	(
+		THidingSpotSearchHandle searchHandle
+	);
+
+	/**
+	* This gets a search by its handle and indicates how many people
+	* (including the caller) have a reference handle to the search.
+	*/
+	darkModAASFindHidingSpots* getSearchAndReferenceCountByHandle
+	(
+		THidingSpotSearchHandle searchHandle,
+		unsigned int& out_refCount
+	);
+
+	/**
+	* This should be called to dereference a hiding spot search. It ensures
+	* the search is destroyed when the last user deallocates it.
+	*
+	* Once this is called, the handle should be considered invalid and never
+	* be used again.
+	*/
+	void dereference (THidingSpotSearchHandle hSearch);
+
+	/**
+	* This attempts to get or create a new search. If the search
+	* already exists, the existing one is returned.
+	*
+	* The search is referenced if the handle returned is not the null
+	* value, so the caller must eventually dereference it.
+	*/
+	THidingSpotSearchHandle getOrCreateSearch
+	(
+		const idVec3 &hideFromPos, 
+		idAAS* in_p_aas, 
+		float in_hidingHeight,
+		idBounds in_searchLimits, 
+		int in_hidingSpotTypesAllowed, 
+		idEntity* in_p_ignoreEntity,
+		int frameIndex,
+		bool& out_b_searchCompleted
+	);
+		
+
+	
+protected:
+
+	/**
+	* The list of hiding spot searches. 
+	*/
+	TDarkmodHidingSpotSearchNode* p_firstSearch;
+	
+	/**
+	* The number of active hiding spot searches
+	*/
+	unsigned int numSearchesInUse;
+
+	/**
+	* This destroys all searches. Don't call it unless you are shutting down the game.
+	*/
+	void clear();
+
+	/**
+	* This gets an empty hiding spot search from the list and references
+	* it before returning it to the caller.
+	*
+	*/
+	THidingSpotSearchHandle getUnusedSearch();
+
+	/**
+	* This searches the list for a search with similar bounds.
+	*/
+	THidingSpotSearchHandle findSearchByBounds 
+	(
+		idBounds bounds
+	);
+
+
+
+};
+
+extern CHidingSpotSearchCollection HidingSpotSearchCollection;
