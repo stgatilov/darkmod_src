deebd9c3d6c4d38d46d82deceb88a36c80ca0ad8
ishtvan <ishtvan@49c82d7f-2e2a-0410-a16f-ae8f201b507f>
2007-05-28 06:31:55 +0000

    added CONTENTS_MELEEWEAP for future use in melee combat
    git-svn-id: http://svn.thedarkmod.com/svn/darkmod_src/trunk@998 49c82d7f-2e2a-0410-a16f-ae8f201b507f

--- renderer/material.h
+++ renderer/material.h
@@ -271,6 +271,7 @@ typedef enum {
 	CONTENTS_AAS_OBSTACLE		= BIT(14),	// used to compile an obstacle into AAS that can be enabled/disabled
 	CONTENTS_FLASHLIGHT_TRIGGER	= BIT(15),	// used for triggers that are activated by the flashlight
 	CONTENTS_RESPONSE			= BIT(16),  // TDM: Used for stim/response optimization.
+	CONTENTS_MELEEWEAP			= BIT(17),  // TDM: Used for melee weapons and shields
 
 	// contents used by utils
 	CONTENTS_AREAPORTAL			= BIT(20),	// portal separating renderer areas
