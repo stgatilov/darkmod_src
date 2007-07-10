4eda8cc9f6897760b008d04393edd8c3185d37de
greebo <greebo@49c82d7f-2e2a-0410-a16f-ae8f201b507f>
2007-07-10 14:22:21 +0000

    Added empty file pairs for the various Inventory classes (@sparhawk: please add these files to the project file). The files will be filled with the corresponding code in the next few commits.
    git-svn-id: http://svn.thedarkmod.com/svn/darkmod_src/trunk@1070 49c82d7f-2e2a-0410-a16f-ae8f201b507f

---
+++ DarkMod/Inventory/Item.cpp
@@ -0,0 +1,18 @@
+/***************************************************************************
+ *
+ * PROJECT: The Dark Mod
+ * $Revision: 987 $
+ * $Date: 2007-05-12 15:36:09 +0200 (Sa, 12 Mai 2007) $
+ * $Author: greebo $
+ *
+ ***************************************************************************/
+
+#include "../idlib/precompiled.h"
+#pragma hdrstop
+
+#pragma warning(disable : 4533 4800)
+
+static bool init_version = FileVersionList("$Id: Item.cpp 987 2007-05-12 13:36:09Z greebo $", init_version);
+
+#include "Item.h"
+
---
+++ DarkMod/Inventory/Item.h
@@ -0,0 +1,14 @@
+/***************************************************************************
+ *
+ * PROJECT: The Dark Mod
+ * $Revision: 915 $
+ * $Date: 2007-04-19 22:10:27 +0200 (Do, 19 Apr 2007) $
+ * $Author: greebo $
+ *
+ ***************************************************************************/
+#ifndef __DARKMOD_INVENTORYITEM_H__
+#define __DARKMOD_INVENTORYITEM_H__
+
+
+
+#endif /* __DARKMOD_INVENTORYITEM_H__ */
