77028619c0c81e58007ff510637976e89c0dae04
joebarnin <joebarnin@49c82d7f-2e2a-0410-a16f-ae8f201b507f>
2007-07-23 15:44:33 +0000

    Commit the correct version of DifficultyMenu.cpp and DifficultyMenu.h (previous commit was bogus)
    git-svn-id: http://svn.thedarkmod.com/svn/darkmod_src/trunk@1203 49c82d7f-2e2a-0410-a16f-ae8f201b507f

--- DarkMod/DifficultyMenu.cpp
+++ DarkMod/DifficultyMenu.cpp
@@ -1,30 +1,68 @@
 #include "../idlib/precompiled.h"
 #pragma hdrstop
 #include "DifficultyMenu.h"
+#include "../DarkMod/shop.h"
+#include "../DarkMod/MissionData.h"
 
-CDifficultyMenu::CDifficultyMenu(void)
+CDifficultyMenu::CDifficultyMenu()
 {
 }
 
-CDifficultyMenu::~CDifficultyMenu(void)
+CDifficultyMenu::~CDifficultyMenu()
 {
 }
 
+// Handle mainmenu commands
 void CDifficultyMenu::HandleCommands(const char *menuCommand, idUserInterface *gui)
 {
 	if (idStr::Icmp(menuCommand, "diffLoad") == 0)
 	{
-		// Initialize the shop
-		//Init();
+		// read in the objectives from map
+		InitializeDifficulty(gui);
+
+		// show the top of the "easy" list
+		scrollPos = 0;
+		g_skill.SetInteger(0);
+		GenerateObjectivesDisplay();
+		DisplayDifficulty(gui);
+	}
+	else if (idStr::Icmp(menuCommand, "diffSelect") == 0)
+	{
+		// change the difficulty (skill) level to selected value, redisplay objectives
+		scrollPos = 0;
+		g_skill.SetInteger(gui->GetStateInt("diffSelect", "0"));
+		GenerateObjectivesDisplay();
+		DisplayDifficulty(gui);
+	}
+	else if (idStr::Icmp(menuCommand, "diffUp") == 0)
+	{
+		// scroll up one objective
+		scrollPos -= scrollPos == 0 ? 0 : 1;
+		GenerateObjectivesDisplay();
+		DisplayDifficulty(gui);
+	}
+	else if (idStr::Icmp(menuCommand, "diffDown") == 0)
+	{
+		// scroll down one objective
+		scrollPos += scrollPos + 1 >= diffObjectives[g_skill.GetInteger()].Num() ? 0 : 1;
+		GenerateObjectivesDisplay();
 		DisplayDifficulty(gui);
 	}
 }
 
+// set the "objectives" GUI variable to the difficulty-based objectives string
 void CDifficultyMenu::DisplayDifficulty(idUserInterface *gui)
 {
-	const char * mapName = "";//tdm_mapName.GetString();
-	const char * filename = va("maps/%s", mapName);
+	gui->SetStateString("objectives", objectivesDisplay);
+}
 
+// generate a list of objective description strings for each difficulty level
+void CDifficultyMenu::InitializeDifficulty(idUserInterface *gui)
+{
+	// read the map
+	const char * mapName = tdm_mapName.GetString();
+	const char * filename = va("maps/%s", mapName);
+	scrollPos = 0;
 	idMapFile* mapFile = new idMapFile;
 	if ( !mapFile->Parse( idStr( filename ) + ".map") ) {
 		delete mapFile;
@@ -32,17 +70,130 @@ void CDifficultyMenu::DisplayDifficulty(idUserInterface *gui)
 		gameLocal.Warning( "Couldn't load %s", filename );
 		return;
 	}
-	idMapEntity* mapEnt = mapFile->GetEntity( 0 );
-	idDict mapDict = mapEnt->epairs;
+	idMapEntity* worldSpawn = mapFile->GetEntity( 0 );
+	idDict mapDict = worldSpawn->epairs;
 
+	// Determine the difficulty level string. The defaults are the "difficultyMenu" entityDef.
+	// Maps can override these values by use of the difficulty#Name value on the spawnargs of 
+	// the worldspawn.
 	const idDecl * diffDecl = declManager->FindType(DECL_ENTITYDEF, "difficultyMenu", false);
 	const idDeclEntityDef *diffDef = static_cast<const idDeclEntityDef *>( diffDecl );
-	const char* diff0 = mapDict.GetString("difficulty0Name", diffDef->dict.GetString("diff0default", ""));
-	const char* diff1 = mapDict.GetString("difficulty1Name", diffDef->dict.GetString("diff0default", ""));
-	const char* diff2 = mapDict.GetString("difficulty2Name", diffDef->dict.GetString("diff0default", ""));
+	for (int diffLevel = 0; diffLevel < DIFFICULTY_COUNT; diffLevel++)
+	{
+		const char* diffName = mapDict.GetString(va("difficulty%dName",diffLevel),
+			diffDef->dict.GetString(va("diff%ddefault",diffLevel), ""));
+		gui->SetStateString(va("diff%dName",diffLevel), diffName);
+	}
+
+	// Show/hide appropriate menus
 	gui->SetStateInt("isDiffMenuVisible", 1);
 	gui->SetStateInt("isNewGameRootMenuVisible", 0);
-	gui->SetStateString("diff0", diff0);
-	gui->SetStateString("diff1", diff1);
-	gui->SetStateString("diff2", diff2);
+
+	// Search map for targets; find the first target that points to objectives
+	idMapEntity* objectiveEnt = NULL;
+	const idKeyValue* targetKey = NULL;
+	do {
+		targetKey = mapDict.MatchPrefix("target", targetKey);
+		if (targetKey == NULL)
+		{
+			continue;
+		}
+		objectiveEnt = mapFile->FindEntity(targetKey->GetValue());
+		if (objectiveEnt != NULL && idStr::Icmp(objectiveEnt->epairs.GetString("classname"), "target_tdm_addobjectives") == 0)
+		{
+			break;
+		}
+	} while (targetKey != NULL);
+
+	// found some objectives?
+	if (objectiveEnt != NULL)
+	{
+		idDict objDict = objectiveEnt->epairs;
+		int objCount = 0;
+		idStr prefix;
+		idStr diffs;
+		idLexer	src;
+		idToken	token;
+
+		// loop through each objective
+		while (true)
+		{
+			objCount++;
+			prefix = va("obj%d_", objCount);
+
+			if (objDict.FindKeyIndex(prefix + "desc") == -1)
+			{
+				// no more objectives, exit
+				break;
+			}
+
+			// If this objective is:
+			//  * not waiting for a trigger, and
+			//  * visible, and
+			//  * state is incomplete, or state is complete and it is ongoing
+			// then it can be displayed
+			if (!objDict.GetBool(prefix + "wait_for_trigger", "0") &&
+				objDict.GetBool(prefix + "visible", "1") &&
+				(objDict.GetInt(prefix + "state", "0") == STATE_INCOMPLETE ||
+				(objDict.GetInt(prefix + "state", "0") == STATE_COMPLETE &&
+				 objDict.GetBool(prefix + "ongoing", "0"))))
+			{
+				// Parse difficulty level. If difficulty not specified, then
+				// this objective applies to all levels.
+				diffs = objDict.GetString( prefix + "difficulty", "" );
+				bool diffApplies[DIFFICULTY_COUNT] = {true};
+				for (int diffCount = 0; diffCount < DIFFICULTY_COUNT; diffCount++)
+				{
+					diffApplies[diffCount] = true;
+				}
+				if (diffs.Length() > 0) {
+					// Difficulties are specified for this objective, so initialize to false
+					for (int diffCount = 0; diffCount < DIFFICULTY_COUNT; diffCount++)
+					{
+						diffApplies[diffCount] = false;
+					}
+					src.LoadMemory( diffs.c_str(), diffs.Length(), "" );
+					while( src.ReadToken( &token ) )
+					{
+						if( token.IsNumeric() )
+						{
+							int diffValue = token.GetIntValue();
+							if (diffValue >= 0 && diffValue <= DIFFICULTY_COUNT-1)
+							{
+								// Objective applies to specified level of difficulty
+								diffApplies[diffValue] = true;
+							}
+						}
+					}
+					src.FreeSource();
+				}
+				
+				// Add objective descriptoin to each difficulties' list of objectives
+				for (int diffV = 0; diffV < DIFFICULTY_COUNT; diffV++)
+				{
+					if (diffApplies[diffV])
+					{
+						diffObjectives[diffV].Append(objDict.GetString(prefix + "desc", "0"));
+					}
+				}
+			}
+		}
+	}
+}
+
+// Generate a single string containing all objective descriptions for the current
+// difficulty level. Separate each objective description with a blank line.
+void CDifficultyMenu::GenerateObjectivesDisplay()
+{
+	objectivesDisplay.Clear();
+	int difficulty = g_skill.GetInteger();
+	int numObjs = diffObjectives[difficulty].Num();
+	for (int i = scrollPos; i < numObjs; i++)
+	{
+		if (i > scrollPos)
+		{
+			objectivesDisplay.Append("\n\n");
+		}
+		objectivesDisplay.Append(diffObjectives[difficulty][i]);
+	}
 }
--- DarkMod/DifficultyMenu.h
+++ DarkMod/DifficultyMenu.h
@@ -3,16 +3,31 @@
 
 #pragma once
 
+// number of difficulty levels
+#define DIFFICULTY_COUNT 3
+
 class CDifficultyMenu
 {
 public:
-	CDifficultyMenu(void);
-	~CDifficultyMenu(void);
+	CDifficultyMenu();
+	~CDifficultyMenu();
 
 	// handles main menu commands
 	void HandleCommands(const char *menuCommand, idUserInterface *gui);
+
+	// initialize the difficulty data (read in objectives)
+	void CDifficultyMenu::InitializeDifficulty(idUserInterface *gui);
+
+	// set the GUI variables to display difficulty/objectives
 	void CDifficultyMenu::DisplayDifficulty(idUserInterface *gui);
 
+	// generate the list of objectives based on difficulty choice
+	void CDifficultyMenu::GenerateObjectivesDisplay();
+
+private:
+	idList<const char *> diffObjectives[DIFFICULTY_COUNT]; 
+	idStr	objectivesDisplay;
+	int		scrollPos;
 };
 
 #endif	/* !__DIFF_H__ */
