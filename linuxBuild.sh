baae947ea1abb60ef170dc739eac4022ecb643be
tels <tels@49c82d7f-2e2a-0410-a16f-ae8f201b507f>
2007-10-09 20:39:45 +0000

    revert the accidentily changed linuxBuild.sh
    git-svn-id: http://svn.thedarkmod.com/svn/darkmod_src/trunk@1416 49c82d7f-2e2a-0410-a16f-ae8f201b507f

--- linuxBuild.sh
+++ linuxBuild.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 mkdir -p ~/.doom3/darkmod
-#scons -j2 BUILD_GAMEPAK=1 && cp game01-base.pk4 ~/.doom3/darkmod/
-scons BUILD_GAMEPAK=1 && cp game01-base.pk4 ~/.doom3/darkmod/
+scons -j2 BUILD_GAMEPAK=1 && cp game01-base.pk4 ~/.doom3/darkmod/
 
