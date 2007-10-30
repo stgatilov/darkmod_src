6b625d984a8230cb0d85feeccd8e0f156e90cf0d
greebo <greebo@49c82d7f-2e2a-0410-a16f-ae8f201b507f>
2007-10-30 13:29:34 +0000

    Moved forking code from script to State.cpp.
    git-svn-id: http://svn.thedarkmod.com/svn/darkmod_src/branches/AI_greebo@1600 49c82d7f-2e2a-0410-a16f-ae8f201b507f

--- DarkMod/AI/States/State.cpp
+++ DarkMod/AI/States/State.cpp
@@ -35,7 +35,139 @@ void State::OnAICommMessage(CAIComm_Message* message)
 		DM_LOG(LC_AI, LT_INFO).LogString("Got incoming message from %s\r", issuingEntity->name.c_str());
 	}
 
-	
+	switch (commType)
+	{
+		case CAIComm_Message::Greeting_CommType:
+			DM_LOG(LC_AI, LT_INFO).LogString("Message Type: Greeting_CommType\r");
+			break;
+		case CAIComm_Message::FriendlyJoke_CommType:
+			DM_LOG(LC_AI, LT_INFO).LogString("Message Type: FriendlyJoke_CommType\r");
+			break;
+		case CAIComm_Message::Insult_CommType:
+			DM_LOG(LC_AI, LT_INFO).LogString("Message Type: Insult_CommType\r");
+			break;
+		case CAIComm_Message::RequestForHelp_CommType:
+			DM_LOG(LC_AI, LT_INFO).LogString("Message Type: RequestForHelp_CommType\r");
+			break;
+		case CAIComm_Message::RequestForMissileHelp_CommType:
+			DM_LOG(LC_AI, LT_INFO).LogString("Message Type: RequestForMissileHelp_CommType\r");
+			break;
+		case CAIComm_Message::RequestForMeleeHelp_CommType:
+			DM_LOG(LC_AI, LT_INFO).LogString("Message Type: RequestForMeleeHelp_CommType\r");
+			break;
+		case CAIComm_Message::RequestForLight_CommType:
+			DM_LOG(LC_AI, LT_INFO).LogString("Message Type: RequestForLight_CommType\r");
+			break;
+		case CAIComm_Message::DetectedSomethingSuspicious_CommType:
+			DM_LOG(LC_AI, LT_INFO).LogString("Message Type: DetectedSomethingSuspicious_CommType\r");
+			break;
+		case CAIComm_Message::DetectedEnemy_CommType:
+			DM_LOG(LC_AI, LT_INFO).LogString("Message Type: DetectedEnemy_CommType\r");
+			break;
+		case CAIComm_Message::FollowOrder_CommType:
+			DM_LOG(LC_AI, LT_INFO).LogString("Message Type: FollowOrder_CommType\r");
+			break;
+		case CAIComm_Message::GuardLocationOrder_CommType:
+			DM_LOG(LC_AI, LT_INFO).LogString("Message Type: GuardLocationOrder_CommType\r");
+			break;
+		case CAIComm_Message::GuardEntityOrder_CommType:
+			DM_LOG(LC_AI, LT_INFO).LogString("Message Type: GuardEntityOrder_CommType\r");
+			break;
+		case CAIComm_Message::PatrolOrder_CommType:
+			DM_LOG(LC_AI, LT_INFO).LogString("Message Type: PatrolOrder_CommType\r");
+			break;
+		case CAIComm_Message::SearchOrder_CommType:
+			DM_LOG(LC_AI, LT_INFO).LogString("Message Type: SearchOrder_CommType\r");
+			break;
+		case CAIComm_Message::AttackOrder_CommType:
+			DM_LOG(LC_AI, LT_INFO).LogString("Message Type: AttackOrder_CommType\r");
+			break;
+		case CAIComm_Message::GetOutOfTheWayOrder_CommType:
+			DM_LOG(LC_AI, LT_INFO).LogString("Message Type: GetOutOfTheWayOrder_CommType\r");
+			break;
+		case CAIComm_Message::ConveyWarning_EvidenceOfIntruders_CommType:
+			DM_LOG(LC_AI, LT_INFO).LogString("Message Type: ConveyWarning_EvidenceOfIntruders_CommType\r");
+			break;
+		case CAIComm_Message::ConveyWarning_ItemsHaveBeenStolen_CommType:
+			DM_LOG(LC_AI, LT_INFO).LogString("Message Type: ConveyWarning_ItemsHaveBeenStolen_CommType\r");
+			break;
+		case CAIComm_Message::ConveyWarning_EnemiesHaveBeenSeen_CommType:
+			DM_LOG(LC_AI, LT_INFO).LogString("Message Type: ConveyWarning_EnemiesHaveBeenSeen_CommType\r");
+			break;
+	}
+
+	/*if (MessageType == Greeting_MessageType)
+	{
+		responseTo_Greeting (issuingEntity, intendedRecipientEntity, directObjectEntity, directObjectLocation);
+	}
+	else if (MessageType == FriendlyJoke_MessageType)
+	{
+		responseTo_FriendlyJoke (issuingEntity, intendedRecipientEntity, directObjectEntity, directObjectLocation);
+	}
+	else if (MessageType == ConveyWarning_EvidenceOfIntruders_MessageType)
+	{
+		responseTo_conveyWarningEvidenceOfIntruders (issuingEntity, intendedRecipientEntity, directObjectEntity, directObjectLocation);
+	}
+	else if (MessageType == ConveyWarning_ItemsHaveBeenStolen_MessageType)
+	{
+		responseTo_conveyWarningItemsStolen (issuingEntity, intendedRecipientEntity, directObjectEntity, directObjectLocation);
+	}
+	else if (MessageType == ConveyWarning_EnemiesHaveBeenSeen_MessageType)
+	{
+		responseTo_conveyWarningEnemiesSeen (issuingEntity, intendedRecipientEntity, directObjectEntity, directObjectLocation);
+	}
+	else if (MessageType == Insult_MessageType)
+	{
+		responseTo_Insult (issuingEntity, intendedRecipientEntity, directObjectEntity, directObjectLocation);
+	}
+	else if (MessageType == RequestForHelp_MessageType)
+	{
+		responseTo_RequestForHelp (issuingEntity, intendedRecipientEntity, directObjectEntity, directObjectLocation);
+	}
+	else if (MessageType == RequestForMissileHelp_MessageType)
+	{
+		responseTo_RequestForMissileHelp (issuingEntity, intendedRecipientEntity, directObjectEntity, directObjectLocation);
+	}
+	else if (MessageType == RequestForMeleeHelp_MessageType)
+	{
+		responseTo_RequestForMeleeHelp (issuingEntity, intendedRecipientEntity, directObjectEntity, directObjectLocation);
+	}
+	else if (MessageType == RequestForLight_MessageType)
+	{
+		responseTo_RequestForLight(issuingEntity, intendedRecipientEntity, directObjectEntity, directObjectLocation);
+	}
+	else if (MessageType == DetectedSomethingSuspicious_MessageType)
+	{
+		responseTo_DetectedSomethingSuspicious(issuingEntity, intendedRecipientEntity, directObjectEntity, directObjectLocation);
+	}
+	else if (MessageType == DetectedEnemy_MessageType)
+	{
+		responseTo_DetectedEnemy(issuingEntity, intendedRecipientEntity, directObjectEntity, directObjectLocation);
+	}
+	else if (MessageType == FollowOrder_MessageType)
+	{
+		responseTo_FollowOrder(issuingEntity, intendedRecipientEntity, directObjectEntity, directObjectLocation);
+	}
+	else if (MessageType == GuardLocationOrder_MessageType)
+	{
+		responseTo_GuardLocationOrder(issuingEntity, intendedRecipientEntity, directObjectEntity, directObjectLocation);
+	}
+	else if (MessageType == GuardEntityOrder_MessageType)
+	{
+		responseTo_GuardEntityOrder(issuingEntity, intendedRecipientEntity, directObjectEntity, directObjectLocation);
+	}
+	else if (MessageType == PatrolOrder_MessageType)
+	{
+		responseTo_PatrolOrder(issuingEntity, intendedRecipientEntity, directObjectEntity, directObjectLocation);
+	}
+	else if (MessageType == SearchOrder_MessageType)
+	{
+		responseTo_SearchOrder(issuingEntity, intendedRecipientEntity, directObjectEntity, directObjectLocation);
+	}
+	else if (MessageType == AttackOrder_MessageType)
+	{
+		responseTo_AttackOrder(issuingEntity, intendedRecipientEntity, directObjectEntity, directObjectLocation);
+	}*/
 }
 
 } // namespace ai
