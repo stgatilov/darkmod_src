e0006dde055b323467074627c8b799273c961d8d
joebarnin <joebarnin@49c82d7f-2e2a-0410-a16f-ae8f201b507f>
2007-11-09 04:44:39 +0000

    Use Boost filesystem instead of idFileSystem
    git-svn-id: http://svn.thedarkmod.com/svn/darkmod_src/trunk@1742 49c82d7f-2e2a-0410-a16f-ae8f201b507f

--- DarkMod/ModMenu.cpp
+++ DarkMod/ModMenu.cpp
@@ -3,6 +3,7 @@
 #include "ModMenu.h"
 #include "../DarkMod/shop.h"
 #include "../DarkMod/MissionData.h"
+#include "boost/filesystem.hpp"
 #ifdef _WINDOWS
 #include <process.h>
 #else
@@ -20,6 +21,7 @@ CModMenu::~CModMenu()
 
 extern int errorno;
 idCVar tdm_mapName( "tdm_mapName", "", CVAR_GUI, "" );
+namespace fs = boost::filesystem;
 
 // Handle mainmenu commands
 void CModMenu::HandleCommands(const char *menuCommand, idUserInterface *gui)
@@ -31,6 +33,7 @@ void CModMenu::HandleCommands(const char *menuCommand, idUserInterface *gui)
 		idFileList * modDirs = idLib::fileSystem->ListFiles( "fms", "/", true);
 		for (int i = 0; i < modDirs->GetNumFiles(); i++)
 		{
+			// filter out the .svn directory
 			if (idStr::Icmp(modDirs->GetFile(i), ".svn") != 0) {
 				modsAvailable.Append(modDirs->GetFile(i));
 			}
@@ -91,40 +94,97 @@ void CModMenu::HandleCommands(const char *menuCommand, idUserInterface *gui)
 		// TODO: get current mission, copy savefiles directory contents to this mission's directory
 		// if no current mission, copy to savefiles.original directory (or throw away?)
 
-		// Get the path to the darkmod directory
-		const char * darkmodPath = idLib::fileSystem->RelativePathToOSPath("", "fs_basepath");
-
-		// copy the mod file selected by the user
+		// Get selected mod
 		int modNum = gui->GetStateInt("modSelected", "0");
 		modNum += modTop;
-		const char * modDir = modsAvailable[modNum];
-		const char * modName = va("%s%s", modDir, ".pk4");
-		const char * sourceFile = va("%sfms/%s/%s", darkmodPath, modDir, modName);
-		const char * destFile = va("%s%s", darkmodPath, modName);
-		idLib::fileSystem->CopyFile(sourceFile, destFile);
+		const char * modDirName = modsAvailable[modNum];
 
-		// Get the current mod
+		// The name of the mod .pk4 file
+		const char * modFileName = va("%s%s", modDirName, ".pk4");
+
+		// Path to the darkmod directory
+		fs::path darkmodPath(idLib::fileSystem->RelativePathToOSPath("", "fs_basepath"));
+
+		// Path to the savegames directory
+		fs::path savegamePath(darkmodPath / "savegames");
+
+		// Path to mod directory in fms folder
+		fs::path modDirPath(darkmodPath / "fms" / modDirName);
+
+		// Path to file that holds the current FM name
+		fs::path currentFMPath(darkmodPath / "currentfm.txt");
+
+		// Path to the PK4 file in the mod directory
+		fs::path modpk4(modDirPath / modFileName);
+
+		// Path to copy the mod PK4 file to
+		fs::path modpk4dest(darkmodPath / modFileName);
+
+		// Copy the PK4
+		if (!fs::exists(modpk4dest)) {
+			fs::copy_file(modpk4, modpk4dest);
+		}
+
+		// Get the current mod name
 		char * current = NULL;
-		idLib::fileSystem->ReadFile("currentfm.txt", ( void ** )&current);
+		FILE* currentFM = fopen(currentFMPath.file_string().c_str(), "r");
 
-		// Make the new mod the current mod
-		idLib::fileSystem->WriteFile("currentfm.txt", modDir, strlen(modDir), "fs_basepath");
+		fs::path pk4ToDelete;
+		if (currentFM) {
+			// read the mod name
+			current = new char[100];
+			fgets(current, 100, currentFM);
+			fclose(currentFM);
+
+			// current pk4
+			pk4ToDelete = darkmodPath / va("%s.pk4", current);
+
+			// Path to the savegames of the current mod (create if necessary)
+			fs::path modSavegamesPath(darkmodPath / "fms" / current / "savegames");
+			fs::create_directory(modSavegamesPath);
+
+			// Move all current savegames to mod directory in fms folder
+			fs::directory_iterator end_iter;
+		    for (fs::directory_iterator dir_itr(savegamePath); dir_itr != end_iter; ++dir_itr)
+		    {
+				if (!fs::is_directory(dir_itr->status()))
+				{
+					fs::path dest(modSavegamesPath / dir_itr->path().leaf());
+					if (fs::exists(dest)) {
+						fs::remove(dest);
+					}
+					fs::rename(dir_itr->path(), dest);
+				}
+			}
+			delete current;
+		}
+		// Copy all existing savegames from mod directory in fms folder to current savegames folder
+		fs::path newmodSavegamesPath(darkmodPath / "fms" / modDirName / "savegames");
+		if (fs::exists(newmodSavegamesPath)) {
+			fs::directory_iterator end_iter;
+			for (fs::directory_iterator dir_itr(newmodSavegamesPath); dir_itr != end_iter; ++dir_itr)
+			{
+				if (!fs::is_directory(dir_itr->status()))
+				{
+					fs::copy_file(dir_itr->path(), savegamePath / dir_itr->path().leaf());
+				}
+			}
+		}
+
+		// Save the name of the new mod
+		currentFM = fopen(currentFMPath.file_string().c_str(), "w+");
+		fputs(modDirName, currentFM);
+		fclose(currentFM);
 		
 		// Set DOOM3.EXE path
-		darkmodPath = idLib::fileSystem->RelativePathToOSPath("", "fs_basepath");
 #ifdef _WINDOWS
-		char * doomExe = va("%s../doom3.exe", darkmodPath);
-		char * launcherExe = va("%sdmlauncher.exe", darkmodPath);
+		fs::path doomExe(darkmodPath);
+		doomExe = doomExe.remove_leaf().remove_leaf();
+		doomExe /= "doom3.exe";
+		fs::path launcherExe(darkmodPath / "dmlauncher.exe");
 #else
-		char * doomExe = va("%s../doom.x86", darkmodPath);
+		char * doomExe = va("%s../doom.x86", darkmodPath.file_string().c_str());
 #endif
-
-		// Shutdown file system and delete existing mod file
-		char * pk4ToDelete = NULL;
-		if (current != NULL) {
-			pk4ToDelete = va("%s%s.pk4", darkmodPath, current);
-			fileSystem->FreeFile( current );
-		}
 
 #ifdef _WINDOWS
 		// start up dmlauncher and exit
@@ -133,18 +193,19 @@ void CModMenu::HandleCommands(const char *menuCommand, idUserInterface *gui)
 		memset(&siStartupInfo, 0, sizeof(siStartupInfo));
 		memset(&piProcessInfo, 0, sizeof(piProcessInfo));
 		siStartupInfo.cb = sizeof(siStartupInfo);
-		char * lpCmdLine = va("%s %s +set fs_game darkmod %s", launcherExe, doomExe, pk4ToDelete);
+		char * lpCmdLine = va("%s %s +set fs_game darkmod %s",
+			launcherExe.file_string().c_str(), doomExe.file_string().c_str(), pk4ToDelete.file_string().c_str());
 		CreateProcess(NULL, lpCmdLine, NULL, NULL,  false, 0, NULL, NULL, &siStartupInfo, &piProcessInfo);
 
 		cmdSystem->BufferCommandText( CMD_EXEC_NOW, "quit" );
 #else
 		// delete the current pk4
 		idLib::fileSystem->Shutdown(false);
-		if (pk4ToDelete != NULL) {
-			remove(pk4ToDelete);
+		if (!pk4ToDelete.empty()) {
+			remove(pk4ToDelete.file_string().c_str());
 		}
 		// start doom
-		if (execlp(doomExe, doomExe, "+set", "fs_game", "darkmod", NULL)==-1) {
+		if (execlp(doomExe.file_string().c_str(), doomExe.file_string().c_str(), "+set", "fs_game", "darkmod", NULL)==-1) {
 			int errnum = errno;
 			gameLocal.Error("execlp failed with error code %d: %s", errnum, strerror(errnum));
 		}
@@ -154,8 +215,6 @@ void CModMenu::HandleCommands(const char *menuCommand, idUserInterface *gui)
 }
 void CModMenu::UpdateGUI(idUserInterface* gui) {
 	// Display the name of each FM
-
-	// Is first directory the .svn directory? If so, be sure to skip it
 	int modPos = 0;
 	while (modPos < MODS_PER_PAGE) {
 		idStr guiName = idStr("mod") + modPos + "_name";
