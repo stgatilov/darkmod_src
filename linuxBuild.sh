db670f2d81815d5f348d13c099609658ac51b9a2
orbweaver <orbweaver@49c82d7f-2e2a-0410-a16f-ae8f201b507f>
2007-12-31 10:08:11 +0000

    Pass through command line arguments to Scons, to allow selection of BUILD options etc
    git-svn-id: http://svn.thedarkmod.com/svn/darkmod_src/branches/thiefsden@1935 49c82d7f-2e2a-0410-a16f-ae8f201b507f

--- linuxBuild.sh
+++ linuxBuild.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 mkdir -p ~/.doom3/darkmod
-scons -j2 BUILD_GAMEPAK=1 && cp game01-base.pk4 ~/.doom3/darkmod/
+scons -j2 BUILD_GAMEPAK=1 "$@" && cp game01-base.pk4 ~/.doom3/darkmod/
 mv gamex86-base.so gamex86.so
 strip gamex86.so
