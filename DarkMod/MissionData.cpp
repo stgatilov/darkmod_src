f39e5c25cd763f0b045c83073bfbccb94d60169d
greebo <greebo@49c82d7f-2e2a-0410-a16f-ae8f201b507f>
2008-04-27 12:38:43 +0000

    Changed MissionData class to handle a one-column statistics GUI design.
    git-svn-id: http://svn.thedarkmod.com/svn/darkmod_src/trunk@2234 49c82d7f-2e2a-0410-a16f-ae8f201b507f

--- DarkMod/MissionData.cpp
+++ DarkMod/MissionData.cpp
@@ -2374,67 +2374,70 @@ void CMissionData::UpdateStatisticsGUI(idUserInterface* gui, const idStr& listDe
 	// The listdef item (name + _) prefix
 	idStr prefix = va("%s_item_", listDefName.c_str());
 	
+	idStr divider(": ");
+	idStr postfix("\t");
+
 	int difficultyLevel = gameLocal.m_DifficultyManager.GetDifficultyLevel();
 	key = "Difficulty Level";
 	value = gameLocal.m_DifficultyManager.GetDifficultyName(difficultyLevel);
-	gui->SetStateString(prefix + idStr(index++), key + "\t" + value);
+	gui->SetStateString(prefix + idStr(index++), key + divider + value + postfix);
 
 	key = "Time";
 	value = idStr(GamePlayTimer::TimeToStr(m_TotalGamePlayTime));
-	gui->SetStateString(prefix + idStr(index++), key + "\t" + value);
+	gui->SetStateString(prefix + idStr(index++), key + divider + value + postfix);
 
 	key = "Damage Dealt"; 
 	value = idStr(m_Stats.DamageDealt);
-	gui->SetStateString(prefix + idStr(index++), key + "\t" + value);
+	gui->SetStateString(prefix + idStr(index++), key + divider + value + postfix);
 
 	key = "Damage Received"; 
 	value = idStr(m_Stats.DamageReceived);
-	gui->SetStateString(prefix + idStr(index++), key + "\t" + value);
+	gui->SetStateString(prefix + idStr(index++), key + divider + value + postfix);
 
 	key = "Health Restored"; 
 	value = idStr(m_Stats.HealthReceived);
-	gui->SetStateString(prefix + idStr(index++), key + "\t" + value);
+	gui->SetStateString(prefix + idStr(index++), key + divider + value + postfix);
 
 	key = "Pockets Picked"; 
 	value = idStr(m_Stats.PocketsPicked);
-	gui->SetStateString(prefix + idStr(index++), key + "\t" + value);
+	gui->SetStateString(prefix + idStr(index++), key + divider + value + postfix);
 
 	key = "Loot Acquired";
 	value = idStr(m_Stats.FoundLoot);
-	gui->SetStateString(prefix + idStr(index++), key + "\t" + value);
+	gui->SetStateString(prefix + idStr(index++), key + divider + value + postfix);
 
 	key = "Total Loot in Mission";
 	value = idStr(m_Stats.TotalLootInMission);
-	gui->SetStateString(prefix + idStr(index++), key + "\t" + value);
+	gui->SetStateString(prefix + idStr(index++), key + divider + value + postfix);
 
 	key = "Killed by the Player";
 	value = idStr(m_Stats.AIStats[COMP_KILL].ByTeam[m_PlayerTeam]);
-	gui->SetStateString(prefix + idStr(index++), key + "\t" + value);
+	gui->SetStateString(prefix + idStr(index++), key + divider + value + postfix);
 
 	key = "KOed by the Player";
 	value = idStr(m_Stats.AIStats[COMP_KO].ByTeam[m_PlayerTeam]);
-	gui->SetStateString(prefix + idStr(index++), key + "\t" + value);
+	gui->SetStateString(prefix + idStr(index++), key + divider + value + postfix);
 
 	key = "Bodies found";
 	value = idStr(m_Stats.AIStats[COMP_AI_FIND_BODY].Overall);
-	gui->SetStateString(prefix + idStr(index++), key + "\t" + value);
+	gui->SetStateString(prefix + idStr(index++), key + divider + value + postfix);
 
 	for (int i = 0; i < ai::EAlertStateNum; i++) 
 	{
 		key = idStr("AI alerted to level '") + ai::AlertStateNames[i] + "'";
 		value = idStr(m_Stats.MaxAlertIndices[i]);
-		gui->SetStateString(prefix + idStr(index++), key + "\t" + value);
+		gui->SetStateString(prefix + idStr(index++), key + divider + value + postfix);
 	}
 
 	/*key = "Frames";
 	value = idStr(gameLocal.framenum);
-	gui->SetStateString(prefix + idStr(index++), key + "\t" + value);
+	gui->SetStateString(prefix + idStr(index++), key + "\t" + value + postfix);
 	key = "GameLocal.time";
 	value = idStr(gameLocal.time);
-	gui->SetStateString(prefix + idStr(index++), key + "\t" + value);
+	gui->SetStateString(prefix + idStr(index++), key + "\t" + value + postfix);
 	key = "GameLocal.realClientTime";
 	value = idStr(gameLocal.realClientTime);
-	gui->SetStateString(prefix + idStr(index++), key + "\t" + value);*/
+	gui->SetStateString(prefix + idStr(index++), key + "\t" + value + postfix);*/
 
 	// Force a redraw
 	gui->StateChanged(gameLocal.time, true);
