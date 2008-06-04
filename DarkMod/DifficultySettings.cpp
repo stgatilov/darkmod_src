47e7feac64a6043259def744ba7cae9bc925fb2e
greebo <greebo@49c82d7f-2e2a-0410-a16f-ae8f201b507f>
2008-06-04 17:06:26 +0000

    Fixed a difficulty bug (#810) due to misinterpretation of vector arguments (which can have a leading minus sign like number arguments).
    git-svn-id: http://svn.thedarkmod.com/svn/darkmod_src/trunk@2433 49c82d7f-2e2a-0410-a16f-ae8f201b507f

--- DarkMod/DifficultySettings.cpp
+++ DarkMod/DifficultySettings.cpp
@@ -93,6 +93,12 @@ void Setting::ParseFromDict(const idDict& dict, int level, int index)
 			appType = EIgnore;
 			argument.Empty(); // clear the argument
 		}
+		else if (argument.Find(' ') != -1)
+		{
+			// greebo: We have a space in the argument, hence it cannot be 
+			// a mathematical operation. This usually applies to vector arguments
+			// like '-205 10 20', which can contain a leading minus sign.
+		}
 		// Check for special modifiers
 		else if (argument[0] == '+')
 		{
