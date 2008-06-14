9926fc014b541794fb1a5009148e56acac7c1905
tels <tels@49c82d7f-2e2a-0410-a16f-ae8f201b507f>
2008-06-14 10:52:09 +0000

    fix more gcc warnings by addin const to char*
    git-svn-id: http://svn.thedarkmod.com/svn/darkmod_src/trunk@2485 49c82d7f-2e2a-0410-a16f-ae8f201b507f

--- idlib/parser.cpp
+++ idlib/parser.cpp
@@ -614,7 +614,7 @@ void idParser::AddBuiltinDefines( void ) {
 	define_t *define;
 	struct builtin
 	{
-		char *string;
+		const char *string;
 		int id;
 	} builtin[] = {
 		{ "__LINE__",	BUILTIN_LINE }, 
