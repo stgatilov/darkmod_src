69b9cebd84e205d06ad28f9a713cc0e54c1e4ed3
angua <angua@49c82d7f-2e2a-0410-a16f-ae8f201b507f>
2008-06-24 18:59:09 +0000

    Reduced the bounds for obstacle avoidance to half the diameter (from 256 to 128) and a height a little bit larger than the AI's height. This should improve performance a bit.
    git-svn-id: http://svn.thedarkmod.com/svn/darkmod_src/trunk@2565 49c82d7f-2e2a-0410-a16f-ae8f201b507f

--- game/ai/ai_pathing.cpp
+++ game/ai/ai_pathing.cpp
@@ -39,7 +39,7 @@ static bool init_version = FileVersionList("$Id$", init_version);
 ===============================================================================
 */
 
-const float MAX_OBSTACLE_RADIUS			= 256.0f;
+const float MAX_OBSTACLE_RADIUS			= 128.0f;
 const float PUSH_OUTSIDE_OBSTACLES		= 0.5f;
 const float CLIP_BOUNDS_EPSILON			= 10.0f;
 const int 	MAX_AAS_WALL_EDGES			= 256;
@@ -327,7 +327,16 @@ int GetObstacles( const idPhysics *physics, const idAAS *aas, const idEntity *ig
 	// clip bounds for the obstacle search space
 	clipBounds[0] = clipBounds[1] = startPos;
 	clipBounds.AddPoint( seekPos );
-	clipBounds.ExpandSelf( MAX_OBSTACLE_RADIUS );
+	idBounds bounds(physics->GetBounds());
+	clipBounds[0][0] -= MAX_OBSTACLE_RADIUS;
+	clipBounds[0][1] -= MAX_OBSTACLE_RADIUS;
+	clipBounds[1][0] += MAX_OBSTACLE_RADIUS;
+	clipBounds[1][1] += MAX_OBSTACLE_RADIUS;
+
+	clipBounds[0][2] -= bounds[0][2] - 20;
+	clipBounds[1][2] += bounds[1][2] + 20;
+
+	// clipBounds.ExpandSelf( MAX_OBSTACLE_RADIUS );
 	clipMask = physics->GetClipMask();
 
 	if ( ai_showObstacleAvoidance.GetBool() ) {
