1c7aefb032f777946efcb04a9377f7804be69c46
greebo <greebo@49c82d7f-2e2a-0410-a16f-ae8f201b507f>
2008-06-26 20:02:42 +0000

    Added radius_final parsing to ProjectilResult. This really needs consolidating with the main S/R parsing code.
    git-svn-id: http://svn.thedarkmod.com/svn/darkmod_src/trunk@2578 49c82d7f-2e2a-0410-a16f-ae8f201b507f

--- DarkMod/ProjectileResult.cpp
+++ DarkMod/ProjectileResult.cpp
@@ -91,6 +91,7 @@ void CProjectileResult::Init
 	int StimType = ST_DEFAULT;
 	int StimFalloffExponent = 1;
 	float StimRadius = 10.0; // we use a (hopefully) reasonable default radius if none is set.
+	float StimRadiusFinal = -1;
 	int StimDuration(0), StimEvalInterval(0);
 	float StimMagnitude(1.0f);
 	bool bStimUseBounds(false);
@@ -155,6 +156,7 @@ void CProjectileResult::Init
 			if(StimType != ST_DEFAULT)
 			{
 				spawnArgs.GetFloat(va("sr_radius_%u", stimIdx), "10", StimRadius);
+				spawnArgs.GetFloat(va("sr_radius_final_%u", stimIdx), "10", StimRadiusFinal);
 				spawnArgs.GetVector(va("sr_bounds_mins_%u", stimIdx), "0 0 0", stimBounds[0]);
 				spawnArgs.GetVector(va("sr_bounds_maxs_%u", stimIdx), "0 0 0", stimBounds[1]);
 				spawnArgs.GetInt(va("sr_falloffexponent_%u", stimIdx), "1", StimFalloffExponent);
@@ -175,6 +177,7 @@ void CProjectileResult::Init
 				stim->m_Magnitude = StimMagnitude;
 				stim->m_FallOffExponent = StimFalloffExponent;
 				stim->m_Velocity = stimVelocity;
+				stim->m_RadiusFinal = StimRadiusFinal;
 
 				// Check for valid bounds vectors
 				if (stimBounds[0] != idVec3(0,0,0)) {
