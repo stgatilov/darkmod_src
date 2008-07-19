b57d4dafc45d4185f16dce4ce1888c38733e328b
greebo <greebo@49c82d7f-2e2a-0410-a16f-ae8f201b507f>
2008-07-19 16:08:20 +0000

    Improved debug output for conversations.
    git-svn-id: http://svn.thedarkmod.com/svn/darkmod_src/trunk@2711 49c82d7f-2e2a-0410-a16f-ae8f201b507f

--- DarkMod/AI/States/ConversationState.cpp
+++ DarkMod/AI/States/ConversationState.cpp
@@ -596,6 +596,9 @@ void ConversationState::DrawDebugOutput(idAI* owner)
 		default:break;
 	};
 
+	gameRenderWorld->DrawText(str, owner->GetEyePosition() - idVec3(0,0,20), 0.25f, colorCyan, gameLocal.GetLocalPlayer()->viewAxis, 1, 48);
+
+	str = (_commandType < ConversationCommand::ENumCommands) ? ConversationCommand::TypeNames[_commandType] : "";
 	gameRenderWorld->DrawText(str, owner->GetEyePosition() - idVec3(0,0,10), 0.3f, colorCyan, gameLocal.GetLocalPlayer()->viewAxis, 1, 48);
 }
 
