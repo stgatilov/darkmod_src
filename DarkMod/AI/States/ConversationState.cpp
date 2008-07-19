9e1028c8a223aea8acc2bfb628a78b4d8c24a6bf
greebo <greebo@49c82d7f-2e2a-0410-a16f-ae8f201b507f>
2008-07-19 15:36:05 +0000

    Simplified "WalkToEntity" command.
    git-svn-id: http://svn.thedarkmod.com/svn/darkmod_src/trunk@2708 49c82d7f-2e2a-0410-a16f-ae8f201b507f

--- DarkMod/AI/States/ConversationState.cpp
+++ DarkMod/AI/States/ConversationState.cpp
@@ -234,17 +234,10 @@ void ConversationState::StartCommand(ConversationCommand& command, Conversation&
 		idEntity* ent = command.GetEntityArgument(0);
 		if (ent != NULL)
 		{
-			// Start moving
-			idVec3 delta = ent->GetPhysics()->GetOrigin() - owner->GetPhysics()->GetOrigin();
-			idVec3 deltaNorm(delta);
-			deltaNorm.NormalizeFast();
-
 			float distance = (command.GetNumArguments() >= 2) ? command.GetFloatArgument(1) : DEFAULT_WALKTOENTITY_DISTANCE;
 			
-			idVec3 goal = owner->GetPhysics()->GetOrigin() + delta - deltaNorm*distance;
-
 			owner->GetSubsystem(SubsysMovement)->PushTask(
-				TaskPtr(new MoveToPositionTask(goal))
+				TaskPtr(new MoveToPositionTask(ent, distance))
 			);
 
 			// Check if we should wait until the command is finished and set the _state accordingly
