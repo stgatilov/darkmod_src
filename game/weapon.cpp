3e696a0e9dbb2422bdf87db778c04b85f864c466
ishtvan <ishtvan@49c82d7f-2e2a-0410-a16f-ae8f201b507f>
2008-12-06 08:23:40 +0000

    update anim rates when weapons are loaded from new defs
    git-svn-id: http://svn.thedarkmod.com/svn/darkmod_src/trunk@3078 49c82d7f-2e2a-0410-a16f-ae8f201b507f

--- game/weapon.cpp
+++ game/weapon.cpp
@@ -730,6 +730,8 @@ void idWeapon::Clear( void ) {
 	}
 
 	m_Attachments.Clear();
+
+	m_animRates.Clear();
 }
 
 /*
@@ -884,6 +886,24 @@ void idWeapon::GetWeaponDef( const char *objectname, int ammoinclip ) {
 	ejectJointView = animator.GetJointHandle( "eject" );
 	guiLightJointView = animator.GetJointHandle( "guiLight" );
 	ventLightJointView = animator.GetJointHandle( "ventLight" );
+
+	// ishtvan: Cache the anim rates here to adjust weapon anim speeds
+	int anims = animator.NumAnims();
+	m_animRates.Clear();
+	m_animRates.AssureSize(anims);
+	for (int i=0; i<anims; i++) 
+	{
+		const idAnim *anim = animator.GetAnim(i);
+		if (anim != NULL) 
+		{
+			idStr spawnargname = "anim_rate_";
+			spawnargname += anim->Name();
+			m_animRates[i] = weaponDef->dict.GetFloat(spawnargname, "1");
+		} else 
+		{
+			m_animRates[i] = 1.0f;
+		}
+	}
 
 	// get the projectile
 	// greebo: Disabled, the projectile def is loaded on demand
@@ -2828,7 +2848,9 @@ void idWeapon::Event_AnimDone( int channel, int blendFrames )
 idWeapon::Event_SetBlendFrames
 ===============
 */
-void idWeapon::Event_SetBlendFrames( int channel, int blendFrames ) {
+void idWeapon::Event_SetBlendFrames( int channel, int blendFrames ) 
+{
+	// ishtvan: this is a problem with animrate??
 	animBlendFrames = blendFrames;
 }
 
