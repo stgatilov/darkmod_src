7af83f90d8fdca11b66c4e0ef764614f95071264
angua <angua@49c82d7f-2e2a-0410-a16f-ae8f201b507f>
2009-05-26 06:24:10 +0000

    Removed debug numbers from flee task.
    git-svn-id: http://svn.thedarkmod.com/svn/darkmod_src/trunk@3472 49c82d7f-2e2a-0410-a16f-ae8f201b507f

--- DarkMod/AI/Tasks/FleeTask.cpp
+++ DarkMod/AI/Tasks/FleeTask.cpp
@@ -66,8 +66,8 @@ bool FleeTask::Perform(Subsystem& subsystem)
 		return true;
 	}
 
-	gameRenderWorld->DrawText( va("%d  %d",_escapeSearchLevel, _distOpt), owner->GetPhysics()->GetAbsBounds().GetCenter(), 
-		1.0f, colorWhite, gameLocal.GetLocalPlayer()->viewAngles.ToMat3(), 1, gameLocal.msec );
+	//gameRenderWorld->DrawText( va("%d  %d",_escapeSearchLevel, _distOpt), owner->GetPhysics()->GetAbsBounds().GetCenter(), 
+	// 	1.0f, colorWhite, gameLocal.GetLocalPlayer()->viewAngles.ToMat3(), 1, gameLocal.msec );
 
 	// angua: in any case stop fleeing after max time (1 min).
 	// Might stay in flee task forever if pathing to destination not possible otherwise
