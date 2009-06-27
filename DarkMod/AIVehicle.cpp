988b02103c629a6ff0aefb2d11f2e502ceaec45b
ishtvan <ishtvan@49c82d7f-2e2a-0410-a16f-ae8f201b507f>
2009-06-27 03:39:33 +0000

    animal riding bugfixes
    git-svn-id: http://svn.thedarkmod.com/svn/darkmod_src/trunk@3494 49c82d7f-2e2a-0410-a16f-ae8f201b507f

--- DarkMod/AIVehicle.cpp
+++ DarkMod/AIVehicle.cpp
@@ -74,7 +74,7 @@ void CAIVehicle::Spawn( void )
 		speed.Anim = spawnArgs.GetString(va("speed_%d_anim",i));
 		speed.MinAnimRate = spawnArgs.GetFloat(va("speed_%d_min_rate",i),"1.0");
 		speed.MaxAnimRate = spawnArgs.GetFloat(va("speed_%d_max_rate",i),"1.0");
-		speed.NextSpeedFrac = spawnArgs.GetFloat(va("speed_%d_next_speed_control_frac","1.0"));
+		speed.NextSpeedFrac = spawnArgs.GetFloat(va("speed_%d_next_speed_control_frac",i),"1.0");
 		
 		m_Speeds.Append(speed);
 	}
@@ -344,7 +344,10 @@ void CAIVehicle::Event_GetMoveAnim( void )
 	if( m_Controller.GetEntity() )
 		idThread::ReturnString(m_CurMoveAnim.c_str());
 	else
+	{
+		// this should help with debugging if this accidentally gets called with no controller
 		idThread::ReturnString("");
+	}
 }
 
 void CAIVehicle::LinkScriptVariables( void )
