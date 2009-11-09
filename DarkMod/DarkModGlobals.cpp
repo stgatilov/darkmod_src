10497e7609454e2dee802afef614dde76080f3f0
greebo <greebo@49c82d7f-2e2a-0410-a16f-ae8f201b507f>
2009-11-09 06:48:21 +0000

    If no logfile is specified in darkmod.ini, the logfile is closed.
    git-svn-id: http://svn.thedarkmod.com/svn/darkmod_src/trunk@3755 49c82d7f-2e2a-0410-a16f-ae8f201b507f

--- DarkMod/DarkModGlobals.cpp
+++ DarkMod/DarkModGlobals.cpp
@@ -383,25 +383,36 @@ void CGlobal::LoadINISettings(void *p)
 	{
 		if(FindMap(ps, "LogFile", TRUE, &pm) != static_cast<ULONG>(-1))
 		{
-			struct tm *t;
-			time_t timer;
-
-			timer = time(NULL);
-			t = localtime(&timer);
-
-			if((logfile = fopen(pm->Value, "w+b")) != NULL)
+			if (idStr::Icmp(pm->Value, "") == 0)
 			{
-				DM_LOG(LC_INIT, LT_INIT)LOGSTRING("Switching logfile to [%s].\r", pm->Value);
-				if(m_LogFile != NULL)
-				{
-					fclose(m_LogFile);
-					m_LogFile = logfile;
-				}
+				DM_LOG(LC_INIT, LT_INIT)LOGSTRING("Logging disabled by darkmod.ini, closing logfile.\r");
 
-				DM_LOG(LC_INIT, LT_INIT)LOGSTRING("LogFile created at %04u.%02u.%02u %02u:%02u:%02u\r",
-							t->tm_year+1900, t->tm_mon, t->tm_mday, 
-							t->tm_hour, t->tm_min, t->tm_sec);
-				DM_LOG(LC_INIT, LT_INIT)LOGSTRING("DLL compiled on " __DATE__ " " __TIME__ "\r\r");
+				// No logfile defined, quit logging
+				fclose(m_LogFile);
+				m_LogFile = NULL;
+			}
+			else
+			{
+				struct tm *t;
+				time_t timer;
+
+				timer = time(NULL);
+				t = localtime(&timer);
+
+				if((logfile = fopen(pm->Value, "w+b")) != NULL)
+				{
+					DM_LOG(LC_INIT, LT_INIT)LOGSTRING("Switching logfile to [%s].\r", pm->Value);
+					if(m_LogFile != NULL)
+					{
+						fclose(m_LogFile);
+						m_LogFile = logfile;
+					}
+
+					DM_LOG(LC_INIT, LT_INIT)LOGSTRING("LogFile created at %04u.%02u.%02u %02u:%02u:%02u\r",
+								t->tm_year+1900, t->tm_mon, t->tm_mday, 
+								t->tm_hour, t->tm_min, t->tm_sec);
+					DM_LOG(LC_INIT, LT_INIT)LOGSTRING("DLL compiled on " __DATE__ " " __TIME__ "\r\r");
+				}
 			}
 		}
 
