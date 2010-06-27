24892481dac41ebc2d6ecf3d79790c79606823f7
tels <tels@49c82d7f-2e2a-0410-a16f-ae8f201b507f>
2010-06-27 12:00:50 +0000

    first stab at LODE (Level Of Detail Entity) - an entity that can spawn/cull other entities based on distance etc.
    git-svn-id: http://svn.thedarkmod.com/svn/darkmod_src/trunk@3989 49c82d7f-2e2a-0410-a16f-ae8f201b507f

---
+++ DarkMod/lode.cpp
@@ -0,0 +1,442 @@
+// vim:ts=4:sw=4:cindent
+/***************************************************************************
+ *
+ * PROJECT: The Dark Mod
+ * $Revision: 3981 $
+ * $Date: 2010-06-25 07:27:18 +0200 (Fri, 25 Jun 2010) $
+ * $Author: tels $
+ *
+ ***************************************************************************/
+
+// Copyright (C) 2010 Tels (Donated to The Dark Mod Team)
+
+/*
+Level Of Detail Entities
+*/
+
+#include "../idlib/precompiled.h"
+#pragma hdrstop
+
+static bool init_version = FileVersionList("$Id: lode.cpp 3981 2010-06-25 05:27:18Z tels $", init_version);
+
+#include "../game/game_local.h"
+#include "lode.h"
+
+/*
+   LodeRunner
+
+   Entity that spawns/culls other entities, but is invisible on its own.
+
+===============================================================================
+*/
+
+CLASS_DECLARATION( idStaticEntity, LodeRunner )
+	EVENT( EV_Activate,				LodeRunner::Event_Activate )
+END_CLASS
+
+/*
+===============
+LodeRunner::LodeRunner
+===============
+*/
+LodeRunner::LodeRunner( void ) {
+
+	spawnTime = 0;
+
+	active = false;
+
+	m_fCullRange = 0.0f;
+	m_iSeed = 3;
+	m_iScore = 0;
+
+	m_bPrepared = false;
+	m_Entities.Clear();
+
+	m_DistCheckTimeStamp = 0;
+	m_DistCheckInterval = 0.5f;
+	m_bDistCheckXYOnly = false;
+}
+
+/*
+===============
+LodeRunner::Save
+===============
+*/
+void LodeRunner::Save( idSaveGame *savefile ) const {
+	savefile->WriteInt( spawnTime );
+	savefile->WriteBool( active );
+
+	savefile->WriteFloat( m_fCullRange );
+	savefile->WriteInt( m_iSeed );
+	savefile->WriteInt( m_iScore );
+	savefile->WriteInt( m_iNumEntities );
+
+    savefile->WriteInt( m_DistCheckTimeStamp );
+	savefile->WriteInt( m_DistCheckInterval );
+	savefile->WriteBool( m_bDistCheckXYOnly );
+
+	savefile->WriteInt( m_Entities.Num() );
+	for( int i = 0; i < m_Entities.Num(); i++ ) {
+		savefile->WriteString( m_Entities[i].skin );
+		savefile->WriteVec3( m_Entities[i].origin );
+		savefile->WriteAngles( m_Entities[i].angles );
+		savefile->WriteBool( m_Entities[i].hidden );
+		savefile->WriteBool( m_Entities[i].exists );
+		savefile->WriteInt( m_Entities[i].entity );
+		savefile->WriteInt( m_Entities[i].classIdx );
+	}
+
+	savefile->WriteInt( m_Classes.Num() );
+	for( int i = 0; i < m_Classes.Num(); i++ ) {
+		savefile->WriteString( m_Classes[i].classname );
+		savefile->WriteInt( m_Classes[i].score );
+		savefile->WriteFloat( m_Classes[i].cullDist );
+		savefile->WriteFloat( m_Classes[i].spawnDist );
+	}
+}
+
+/*
+===============
+LodeRunner::Restore
+===============
+*/
+void LodeRunner::Restore( idRestoreGame *savefile ) {
+	int num;
+
+	savefile->ReadInt( spawnTime );
+	savefile->ReadBool( active );
+
+	savefile->ReadFloat( m_fCullRange );
+	savefile->ReadInt( m_iSeed );
+	savefile->ReadInt( m_iScore );
+	savefile->ReadInt( m_iNumEntities );
+	
+    savefile->ReadInt( m_DistCheckTimeStamp );
+	savefile->ReadInt( m_DistCheckInterval );
+	savefile->ReadBool( m_bDistCheckXYOnly );
+
+    savefile->ReadInt( num );
+	assert( num == m_Entities.Num() );
+	for( int i = 0; i < num; i++ ) {
+		savefile->ReadString( m_Entities[i].skin );
+		savefile->ReadVec3( m_Entities[i].origin );
+		savefile->ReadAngles( m_Entities[i].angles );
+		savefile->ReadBool( m_Entities[i].hidden );
+		savefile->ReadBool( m_Entities[i].exists );
+		savefile->ReadInt( m_Entities[i].entity );
+		savefile->ReadInt( m_Entities[i].classIdx );
+	}
+
+    savefile->ReadInt( num );
+	assert( num == m_Classes.Num() );
+	for( int i = 0; i < num; i++ ) {
+		savefile->ReadString( m_Classes[i].classname );
+		savefile->ReadString( m_Classes[i].skin );
+		savefile->ReadInt( m_Classes[i].score );
+		savefile->ReadFloat( m_Classes[i].cullDist );
+		savefile->ReadFloat( m_Classes[i].spawnDist );
+	}
+}
+
+/*
+===============
+LodeRunner::RandomFloat
+===============
+*/
+ID_INLINE float LodeRunner::RandomFloat( void ) {
+	unsigned long i;
+	m_iSeed = 1664525L * m_iSeed + 1013904223L;
+	i = LodeRunner::IEEE_ONE | ( m_iSeed & LodeRunner::IEEE_MASK );
+	return ( ( *(float *)&i ) - 1.0f );
+}
+
+/*
+===============
+LodeRunner::Spawn
+===============
+*/
+void LodeRunner::Spawn( void ) {
+	// the LodeRunner itself is sneaky and hides itsel
+	Hide();
+
+	// And is nonsolid, too!
+	GetPhysics()->SetContents( 0 );
+
+	spawnTime = gameLocal.time;
+	active = false;
+
+	m_DistCheckInterval = (int) (1000.0f * spawnArgs.GetFloat( "dist_check_period", "0" ));
+	m_iSeed = spawnArgs.GetFloat( "seed", "3" );
+
+	// do we have a cull range?
+	m_fCullRange = spawnArgs.GetFloat( "lod_cull_range", "0.0" );
+
+	gameLocal.Printf (" LodeRunner %s: cull range = %0.2f.\n", GetName(), m_fCullRange );
+
+	m_bDistCheckXYOnly = spawnArgs.GetBool( "dist_check_xy", "0" );
+
+	// add some phase diversity to the checks so that they don't all run in one frame
+	// make sure they all run on the first frame though, by initializing m_TimeStamp to
+	// be at least one interval early.
+	m_DistCheckTimeStamp = gameLocal.time - (int) (m_DistCheckInterval * (1.0f + gameLocal.random.RandomFloat()) );
+
+	// Have to start thinking
+	BecomeActive( TH_THINK );
+}
+
+/*
+===============
+Create the places for all entities that we control
+===============
+*/
+void LodeRunner::Prepare( void ) {
+	lode_class_t			LodeClass;
+	int						numEntities;
+
+	if ( targets.Num() == 0 )
+	{
+		gameLocal.Warning( "LODE %s has no targets!", GetName() );
+		return;
+	}
+
+	// Gather all targets and make a note of them, also summing their "lod_score" up
+	m_iScore = 0;
+	m_Classes.Clear();
+	for( int i = 0; i < targets.Num(); i++ )
+	{
+		idEntity *ent = targets[ i ].GetEntity();
+
+	   	if ( ent )
+		{
+			int iEntScore = ent->spawnArgs.GetInt( "lod_score", "1" );
+			if (iEntScore < 0)
+			{
+				gameLocal.Warning( "LODE %s: Target %s has invalid lod_score %i!\n", GetName(), ent->GetName(), iEntScore );
+			}
+			else
+			{
+				// add this entity
+				m_iScore += iEntScore;
+				LodeClass.score = iEntScore;
+				LodeClass.classname = ent->GetEntityDefName();
+				LodeClass.skin = ent->spawnArgs.GetString( "skin", "" );
+				LodeClass.spawnDist = 0;
+				LodeClass.cullDist = ent->spawnArgs.GetFloat( "hide_distance", "0" );
+				if (LodeClass.cullDist > 0)
+				{
+					LodeClass.cullDist += m_fCullRange;
+					LodeClass.spawnDist = LodeClass.cullDist - (m_fCullRange / 2);
+					// square for easier compare
+					LodeClass.cullDist *= LodeClass.cullDist;
+					LodeClass.spawnDist *= LodeClass.spawnDist;
+				}
+				m_Classes.Append ( LodeClass );
+				gameLocal.Printf( "LODE %s: Adding class %s.\n", GetName(), LodeClass.classname.c_str() );
+			}
+		}
+	}
+
+
+	// TODO: compute entity count dynamically from area that we cover
+	m_iNumEntities = spawnArgs.GetInt( "entity_count", "100" );
+
+	if (m_iNumEntities < 0)
+	{
+		gameLocal.Warning( "LODE %s: entity count is invalid: %i!\n", GetName(), m_iNumEntities );
+	}
+
+	gameLocal.Printf( "LODE %s: Overall score: %i.\n", GetName(), m_iScore );
+
+	// remove all our targets from the game
+	for( int i = 0; i < targets.Num(); i++ )
+	{
+		idEntity *ent = targets[ i ].GetEntity();
+		if (ent)
+		{
+			// TODO: SafeRemove?
+			ent->PostEventMS( &EV_Remove, 50 );
+		}
+	}
+	targets.Clear();
+
+	PrepareEntities();
+}
+
+void LodeRunner::PrepareEntities( void )
+{
+	idVec3					origin;
+	lode_entity_t			LodeEntity;
+
+	m_Entities.Clear();
+
+	// Compute random positions for all entities that we want to spawn
+	// for each class
+	origin = GetPhysics()->GetOrigin();
+	for (int i = 0; i < m_Classes.Num(); i++)
+	{
+		int iEntities = m_iNumEntities * (static_cast<float>(m_Classes[i].score)) / m_iScore;	// sum 2, this one 1 => 50% of this class
+		gameLocal.Printf( "LODE %s: Creating %i entities of class %s.\n", GetName(), iEntities, m_Classes[i].classname.c_str() );
+		for (int j = 0; j < iEntities; j++)
+		{
+			LodeEntity.origin = origin + idVec3( RandomFloat() * 1500, RandomFloat() * 1500, RandomFloat() * -20.0f );
+			// TODO: choose skin randomly
+			LodeEntity.skin = m_Classes[i].skin;
+			LodeEntity.angles = idAngles( 0, RandomFloat() * 359.9, 0 );
+			// will be automatically spawned when we are in range
+			LodeEntity.hidden = true;
+			LodeEntity.exists = false;
+			LodeEntity.entity = 0;
+			LodeEntity.classIdx = i;
+			m_Entities.Append( LodeEntity );
+		}
+	}
+}
+
+/*
+================
+LodeRunner::Think
+================
+*/
+void LodeRunner::Think( void ) 
+{
+	struct lode_entity_t* ent;
+	struct lode_class_t*  lclass;
+
+	// tels: neccessary?
+	// idEntity::Think();
+
+	// haven't initialized entities yet?
+	if (!m_bPrepared)
+	{
+		Prepare();
+		m_bPrepared = true;
+		if (m_Entities.Num() == 0)
+		{
+			// could not create any entities?
+			gameLocal.Printf( "LODE %s: Have no entities to control, becoming inactive.\n", GetName() );
+			// Tels: Does somehow not work, bouncing us again and again into this branch?
+			BecomeInactive(TH_PHYSICS|TH_THINK);
+			return;
+		}
+	}
+
+	idVec3 playerOrigin = gameLocal.GetLocalPlayer()->GetPhysics()->GetOrigin();
+	idVec3 vGravNorm = GetPhysics()->GetGravityNormal();
+	float lodBias = cv_lod_bias.GetFloat();
+
+	// square these to avoid taking the square root from the distance
+	lodBias *= lodBias;
+	vGravNorm = vGravNorm;
+
+	// Distance dependence checks
+	if( (gameLocal.time - m_DistCheckTimeStamp) > m_DistCheckInterval )
+	{
+
+		m_DistCheckTimeStamp = gameLocal.time;
+
+		// for each of our "entities", do the distance check
+		for (int i = 0; i < m_Entities.Num(); i++)
+		{
+
+			// TODO: cull entities that are outside "hide_distance + fade_out_distance + m_fCullRange
+			// TODO: let all auto-generated entities know about the new distance
+
+			bool bWithinDist = false;
+
+			// TODO: What to do about player looking thru spyglass?
+			idVec3 delta = playerOrigin - m_Entities[i].origin;
+
+			if( m_bDistCheckXYOnly )
+			{
+				delta -= (delta * vGravNorm) * vGravNorm;
+			}
+
+			// multiply with the user LOD bias setting, and cache that result:
+			float deltaSq = delta.LengthSqr() / lodBias;
+
+			// normal distance checks now
+
+			ent = &m_Entities[i];
+			lclass = &(m_Classes[ ent->classIdx ]);
+			if (!ent->exists && deltaSq < lclass->spawnDist)
+			{
+				// spawn the entity and note its number
+				gameLocal.Printf( "LODE %s: Spawning entity #%i (%s).\n", GetName(), i, lclass->classname.c_str() );
+
+				const char* pstr_DefName = lclass->classname.c_str();
+				const idDict *p_Def = gameLocal.FindEntityDefDict( pstr_DefName, false );
+				if( p_Def )
+				{
+					idEntity *ent2;
+					idDict args;
+
+					args.Set("classname", lclass->classname);
+					// spawn as hidden
+					//args.Set("hide", "1");
+					// disable LOD checks on entities (we take care of this)
+					args.Set("dist_check_period", "0");
+
+					gameLocal.SpawnEntityDef( args, &ent2 );
+					if (ent2)
+					{
+						gameLocal.Printf( "LODE %s: Spawned entity #%i (%s) at  %0.2f, %0.2f, %0.2f.\n",
+								GetName(), i, lclass->classname.c_str(), ent->origin.x, ent->origin.y, ent->origin.z );
+						ent->exists = true;
+						ent->entity = ent2->entityNumber;
+						// move to right place
+						ent2->SetOrigin( ent->origin );
+						// and rotate
+						ent2->SetAxis( ent->angles.ToMat3() );
+					}
+				}
+			}	
+			else
+			{
+				if (ent->exists && deltaSq > lclass->cullDist)
+				{
+					idEntity *ent2 = gameLocal.entities[ ent->entity ];
+					if (ent2)
+					{
+						// TODO: SafeRemve?
+						ent2->PostEventMS( &EV_Remove, 0 );
+					}
+
+					// cull (remove) the entity
+					gameLocal.Printf( "LODE %s: Culling entity #%i.\n", GetName(), i );
+					ent->exists = false;
+				}
+			}
+		}
+	}
+}
+
+/*
+================
+LodeRunner::Event_Activate
+================
+*/
+void LodeRunner::Event_Activate( idEntity *activator ) {
+
+	spawnTime = gameLocal.time;
+	active = !active;
+
+	// TODO: tels: what to do if activated?
+
+	/*const idKeyValue *kv = spawnArgs.FindKey( "hide" );
+	if ( kv ) {
+		if ( IsHidden() ) {
+			Show();
+		} else {
+			Hide();
+		}
+	}
+	renderEntity.shaderParms[ SHADERPARM_TIMEOFFSET ] = -MS2SEC( spawnTime );
+	renderEntity.shaderParms[5] = active;
+	// this change should be a good thing, it will automatically turn on 
+	// lights etc.. when triggered so that does not have to be specifically done
+	// with trigger parms.. it MIGHT break things so need to keep an eye on it
+	renderEntity.shaderParms[ SHADERPARM_MODE ] = ( renderEntity.shaderParms[ SHADERPARM_MODE ] ) ?  0.0f : 1.0f;
+	BecomeActive( TH_UPDATEVISUALS );
+*/
+}
+
---
+++ DarkMod/lode.h
@@ -0,0 +1,143 @@
+// vim:ts=4:sw=4:cindent
+/***************************************************************************
+ *
+ * PROJECT: The Dark Mod
+ * $Revision: 3958 $
+ * $Date: 2010-06-20 15:16:22 +0200 (Sun, 20 Jun 2010) $
+ * $Author: tels $
+ *
+ ***************************************************************************/
+
+// Copyright (C) 2010 Tels (Donated to The Dark Mod)
+
+#ifndef __GAME_LODE_H__
+#define __GAME_LODE_H__
+
+/*
+===============================================================================
+
+  LodeRunner - Level Of Detail Entity Runner
+  
+  Automatically creates/culls entities based on distance from player.
+
+===============================================================================
+*/
+
+// Defines one entity class to be spawned/culled
+struct lode_class_t {
+	idStr					classname;		// Entity class to respawn entities
+	idStr					skin;			// Either "skinname" or "random:skinname1,skinname2" etc.
+	int						score;			// to find out how many entities (calculate at spawn time from score)
+	float					cullDist;		// distance after where we remove the entity from the world
+	float					spawnDist;		// distance where we respawn the entity
+};
+
+// Defines one entity to be spawned/culled
+struct lode_entity_t {
+	idStr					skin;			// the final skin for this entity (might be randomly choosen)
+	idVec3					origin;			// (semi-random) origin relatively to LODE origin
+	idAngles				angles;			// zyx (yaw, pitch, roll) (semi-random) angles
+	bool					hidden;			// hidden?
+	bool					exists;			// false if culled
+	int						entity;			// nr of the entity if exists == true
+	int						classIdx;		// index into Classes
+};
+
+class LodeRunner : public idStaticEntity {
+public:
+	CLASS_PROTOTYPE( LodeRunner );
+
+						LodeRunner( void );
+
+	void				Save( idSaveGame *savefile ) const;
+	void				Restore( idRestoreGame *savefile );
+
+	void				Spawn( void );
+	virtual void		Think( void );
+
+private:
+	void				Event_Activate( idEntity *activator );
+
+	/**
+	* Look at our targets and create the entity classes. Calls PrepareEntities().
+	*/
+	void				Prepare( void );
+
+	/**
+	* Create the entity positions.
+	*/
+	void				PrepareEntities( void );
+
+	/**
+	* In the range 0.. 1.0, using our own m_iSeed value.
+	*/
+	float				RandomFloat();
+
+	int					spawnTime;
+	bool				active;
+
+	/**
+	* If true, we need still to prepare our entity list.
+	*/
+	bool				m_bPrepared;
+
+	/**
+	* Set to true if whether this static entity should hide
+	* or change models/skins when outside a certain distance from the player.
+	* If true, this entity will manage the LOD settings for entities, which
+	* means these entities should have a "m_DistCheckInterval" of 0.
+	**/
+	bool				m_bDistDependent;
+
+	/**
+	* Entities further from player than "hide_distance" + m_fCullRange will be culled.
+	* Set to 0 to disable.
+	**/
+	float				m_fCullRange;
+
+	/**
+	* Seed value for the random generator, which generates the sequence used to place
+	* entities. Same seed value gives same sequence, thus same placing every time.
+	**/
+	int					m_iSeed;
+
+	/**
+	* Sum of all entity scores, used to distribute the entities according to their score.
+	**/
+	int					m_iScore;
+
+	/**
+	* Number of entities to create overall.
+	**/
+	int					m_iNumEntities;
+
+	/**
+	* If true, the LOD distance check will only consider distance
+	* orthogonal to gravity.  This can be useful for things like
+	* turning on rainclouds high above the player.
+	**/
+	bool				m_bDistCheckXYOnly;
+	
+	/**
+	* Timestamp and interval between distance checks, in milliseconds
+	**/
+	int					m_DistCheckTimeStamp;
+	int					m_DistCheckInterval;
+
+	/**
+	* The classes of entities that we need to construct.
+	**/
+	idList<lode_class_t>	m_Classes;
+
+	/**
+	* Info about each entitiy that we spawn or cull.
+	**/
+	idList<lode_entity_t>	m_Entities;
+
+	static const unsigned long	IEEE_ONE = 0x3f800000;
+	static const unsigned long	IEEE_MASK = 0x007fffff;
+
+};
+
+#endif /* !__GAME_LODE_H__ */
+
