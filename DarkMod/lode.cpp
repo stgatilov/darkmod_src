7083128dbc93020c98cd8d0e89c1752587f848af
tels <tels@49c82d7f-2e2a-0410-a16f-ae8f201b507f>
2010-08-01 09:19:42 +0000

    LODE: use RandomPart() instead of re-implementing it
    git-svn-id: http://svn.thedarkmod.com/svn/darkmod_src/trunk@4117 49c82d7f-2e2a-0410-a16f-ae8f201b507f

--- DarkMod/lode.cpp
+++ DarkMod/lode.cpp
@@ -621,7 +621,7 @@ float Lode::AddClassFromEntity( idEntity *ent, const int iEntScore )
 		// find the proper skin index
 		idStr skin = kv->GetValue();
 		int skinIdx = AddSkin( &skin );
-		gameLocal.Printf( "LODE %s: Adding skin %s (idx %i) to class.\n", GetName(), skin.c_str(), skinIdx );
+		gameLocal.Printf( "LODE %s: Adding skin '%s' (idx %i) to class.\n", GetName(), skin.c_str(), skinIdx );
 		LodeClass.skins.Append ( skinIdx );
 		kv = ent->spawnArgs.MatchPrefix( "skin", kv );
 	}
@@ -1048,52 +1048,14 @@ void Lode::Prepare( void )
 
 			// spawn_classX => spawn_skinX
 			idStr skin = idStr("spawn_skin") + kv->GetKey().Mid( 11, kv->GetKey().Length() - 11 );
+
 			// spawn_classX => "abc, def, '', abc"
 			skin = spawnArgs.GetString( skin );
+			// select one at random
+			skin = skin.RandomPart();
 
-			// parse skin args
-			idList<idStr> skins;
-
-			int start, end;
-			start = 0; end = skin.Find( ',' );
-			if (end == -1)
-			{
-				// only one
-				args.Set("skin", skin);
-			}
-			else
-			{
-				while (end >= start)
-				{
-					gameLocal.Printf ("Cutting %s between %i and %i.\n", skin.c_str(), start, end );
-					// cut out between start and end (but leave off the ',')
-					idStr s = skin.Mid( start, end - start);
-					//gameLocal.Printf ("Got '%s'.\n", s.c_str() );
-					s.Strip(' ');
-					//gameLocal.Printf ("Using '%s'.\n", s.c_str() );
-					if (s == "''")
-					{
-						// empty skin
-						s = "";
-					}
-					skins.Append( s );
-
-					start = end + 1;					// leave off the ","
-					end = skin.Find( ',', start + 1 );
-					if (end == -1)
-					{
-						// ends
-						end = skin.Length();
-					}
-				}
-				// select one at random
-				if (skins.Num() > 0)
-				{
-					int i = RandomFloat() * skins.Num();
-					// gameLocal.Printf ("Setting skin to %i %s\n", i, skins[i].c_str() );
-					args.Set( "skin", skins[i] );
-				}
-			}
+			//gameLocal.Printf("Using random skin '%s'.\n", skin.c_str() );
+			args.Set( "skin", skin );
 
 			gameLocal.SpawnEntityDef( args, &ent );
 			if (ent)
