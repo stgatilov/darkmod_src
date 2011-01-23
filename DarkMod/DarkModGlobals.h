b8f5437213618a6a140035e0307a6c2d8e5968a4
tels <tels@49c82d7f-2e2a-0410-a16f-ae8f201b507f>
2011-01-23 18:16:06 +0000

    move the CImage portion from DarkModGlobals.h into a new file CImage.h so we can include w/o causing include loops
    git-svn-id: http://svn.thedarkmod.com/svn/darkmod_src/trunk@4460 49c82d7f-2e2a-0410-a16f-ae8f201b507f

--- DarkMod/DarkModGlobals.h
+++ DarkMod/DarkModGlobals.h
@@ -26,6 +26,7 @@
 
 #include <stdio.h>
 #include "../game/game_local.h"
+#include "../DarkMod/CImage.h"
 
 // greebo: Defines the darkmod release version
 #define TDM_VERSION_MAJOR 1
@@ -50,14 +51,6 @@ Darkmod LAS
 #include <boost/filesystem.hpp>
 
 class CRenderPipe;
-
-#ifndef ILuint
-typedef unsigned int ILuint;
-#endif
-
-#ifndef ILenum
-typedef unsigned int ILenum;
-#endif
 
 typedef enum {
 	LT_INIT,
@@ -105,102 +98,6 @@ typedef enum {
 
 class idCmdArgs;
 class CDarkModPlayer;
-
-class CImage
-{
-public:
-	enum Type
-	{
-		AUTO_DETECT = 0,
-		TGA,
-		PNG,
-		JPG,
-		GIF,
-	};
-
-	CImage();
-	CImage(const idStr& name);
-	
-	~CImage();
-
-	/**
-	 * Call this to let the image library assume a certain filetype when loading images
-	 * from files or buffers.
-	 */
-	void SetDefaultImageType(Type type);
-
-	/**
-	 * Load the image from the given file (loaded via D3's filesystem) into memory 
-	 * and allow access to it. If the filename is not NULL, it is assumed that a new 
-	 * image is to be loaded from disk and the any previous one is unloaded.
-	 */
-	bool LoadImageFromVfs(const char* filename = NULL);
-
-	/**
-	 * Load the image from the given file (absolute OS paths) into memory 
-	 * and allow access to it. If the filename is not NULL, it is assumed that a new 
-	 * image is to be loaded from disk and the any previous one is unloaded.
-	 */
-	bool LoadImageFromFile(const boost::filesystem::path& path);
-
-	/**
-	 * Load the image into memory and allow access to it, reading the image
-	 * data from a renderpipe.
-	 */
-	bool LoadImage(CRenderPipe *FileHandle);
-
-	/**
-	 * greebo: Saves the loaded image to the given path. Existing target files
-	 * will be overwritten without further confirmation.
-	 */
-	bool SaveToFile(const boost::filesystem::path& path, Type type = TGA);
-
-	/**
-	 * Initialize Imageinfo like bitmap width, height and other stuff.
-	 */
-	void InitImageInfo();
-
-	/**
-	 * GetImage returns the pointer to the actual image data. the image has to be already
-	 * loaded, otherwise NULL is returned.
-	 */
-	unsigned char* GetImage();
-
-	/**
-	 * Returns the buffer length of the loaded image data in bytes. Each pixel will have
-	 * m_Bpp bytes (1 or 3 or 4, depending on format), and there are m_Width * m_Height pixel.
-	 * The data will not be padded per-line (as f.i. with BMP) if you loaded a TGA file.
-	 */
-	unsigned long GetBufferLen();
-
-	/**
-	 * Unload will set the image to not loaded. If FreeMemory == false then the memory is not
-	 * discarded and when you next load another image and it fits in the previous memory
-	 * it will be loaded there. If it does not fit, then the memory is reallocated. This means that
-	 * you can grow the memory usage by subsequently calling this with every groing imagesizes
-	 * but on the other hand it will not constantly allocate and deallocate in case like the
-	 * renderimages.
-	 */
-	void Unload(bool FreeMemory);
-
-protected:
-	unsigned long	m_BufferLength;
-	unsigned char*	m_Image;
-	ILuint			m_ImageId;
-	bool			m_Loaded;
-
-	// The image type used when opening files (defaults to IL_TGA)
-	ILenum			m_defaultImageType;
-
-	// Convert CImage::Type to ILenum
-	ILenum GetILTypeForImageType(Type type);
-
-public:
-	idStr			m_Name;
-	int				m_Width;	//!< Image width in pixel
-	int				m_Height;	//!< Image height in pixel
-	int				m_Bpp;		//!< Bytes per pixel (not bits!)
-};
 
 class CLightMaterial {
 public:
