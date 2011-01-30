57777f3fcff049f87dcec6df7f5a803341e49a66
tels <tels@49c82d7f-2e2a-0410-a16f-ae8f201b507f>
2011-01-30 21:55:31 +0000

    partial fix for #2593: use inverse-transposed matrix for vector rotation (not 100% right yet) and precompute vMat for speed
    git-svn-id: http://svn.thedarkmod.com/svn/darkmod_src/trunk@4514 49c82d7f-2e2a-0410-a16f-ae8f201b507f

--- DarkMod/ModelGenerator.cpp
+++ DarkMod/ModelGenerator.cpp
@@ -588,8 +588,19 @@ idRenderModel * CModelGenerator::DuplicateLODModels (const idList<const idRender
 		//gameLocal.Warning(" Offset %0.2f, %0.2f, %0.2f LOD %i.\n", op.offset.x, op.offset.y, op.offset.z, op.lod );
 
 		// precompute these
-		idMat3 a = op.angles.ToMat3();
+		idMat4 a = op.angles.ToMat4();
+		idMat4 at = op.angles.ToRotation().ToMat4();
+		if (at.InverseSelf())
+		{
+			at.TransposeSelf();
+		}
+		else
+		{
+			gameLocal.Warning("Could not inverse matrix based on angles %s", op.angles.ToString() );
+		}
 		idVec3 scale = op.scale;
+
+		idMat4 vMat = idMat4( op.angles.ToRotation().ToMat3(), op.offset );
 
 		const idRenderModel* source = LODs->Ptr()[op.lod];
 
@@ -691,37 +702,36 @@ idRenderModel * CModelGenerator::DuplicateLODModels (const idList<const idRender
 				v->xyz[0] = v->xyz[0] * scale.x;
 				v->xyz[1] = v->xyz[1] * scale.y;
 				v->xyz[2] = v->xyz[2] * scale.z;
-				// rotate
-				v->xyz *= a;
-				// then offset
-				v->xyz += op.offset;
+				// rotate and translate
+				v->xyz *= vMat;
 				// rotate normal and tangents
-				v->normal *= a;
-				v->tangents[0] *= a;
-				v->tangents[1] *= a;
+				v->normal *= at;
+				v->normal += op.offset;
+				v->normal.Normalize();
+				v->tangents[0] *= at;
+				v->tangents[1] *= at;
 				// Set "per-entity" color if we have more than one entity:
 				v->SetColor( newColor );
 #else
 				idDrawVert *vs = &(surf->geometry->verts[j]);
 
-			//		idVec3 * idMat3 = 
-			//mat[ 0 ].x * vec.x + mat[ 1 ].x * vec.y + mat[ 2 ].x * vec.z,
-			//mat[ 0 ].y * vec.x + mat[ 1 ].y * vec.y + mat[ 2 ].y * vec.z,
-			//mat[ 0 ].z * vec.x + mat[ 1 ].z * vec.y + mat[ 2 ].z * vec.z );
-				
 				// copy-and-modify at once
 				// scale - rotate - translate
 				v->xyz[0] = vs->xyz[0] * scale.x;
 				v->xyz[1] = vs->xyz[1] * scale.y;
 				v->xyz[2] = vs->xyz[2] * scale.z;
-				v->xyz *= a;
-				v->xyz += op.offset;
-				v->normal = vs->normal * a;
-				v->tangents[0] = vs->tangents[0] * a;
-				v->tangents[1] = vs->tangents[1] * a;
+				v->xyz *= vMat;
+				//v->normal = vs->normal * at;
+				v->normal = (vs->normal * at) + op.offset;
+				v->normal.Normalize();
+				v->tangents[0] = vs->tangents[0] * at;
+				v->tangents[1] = vs->tangents[1] * at;
 				v->SetColor( newColor );
 				v->st = vs->st;
 #endif
+
+				// seems unnec.?
+				//v->Normalize();
 
 /*				if (o == 1 || o == 2)
 				{
