f8595faac775edf93577a829ff87029ee0e75fa6
tels <tels@49c82d7f-2e2a-0410-a16f-ae8f201b507f>
2011-01-30 15:01:49 +0000

    add time to linuxBuild.sh
    git-svn-id: http://svn.thedarkmod.com/svn/darkmod_src/trunk@4508 49c82d7f-2e2a-0410-a16f-ae8f201b507f

--- linuxBuild.sh
+++ linuxBuild.sh
@@ -3,6 +3,6 @@
 mkdir -p ~/.doom3/darkmod
 
 # $@ = pass along the flags like "BUILD=profile" or "BUILD=debug"
-scons -j2 BUILD_GAMEPAK=1 "$@" && cp tdm_game02.pk4 ~/.doom3/darkmod/
+time scons -j2 BUILD_GAMEPAK=1 "$@" && cp tdm_game02.pk4 ~/.doom3/darkmod/
 mv gamex86-base.so gamex86.so
 strip gamex86.so
