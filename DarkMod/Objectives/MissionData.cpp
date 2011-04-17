a9e9edeaa97aec97d00bcfbcb825c4b1f717c818
greebo <greebo@49c82d7f-2e2a-0410-a16f-ae8f201b507f>
2011-04-17 08:28:06 +0000

    Moved CObjective class implementation to its own source file.
    git-svn-id: http://svn.thedarkmod.com/svn/darkmod_src/trunk@4799 49c82d7f-2e2a-0410-a16f-ae8f201b507f

--- DarkMod/Objectives/MissionData.cpp
+++ DarkMod/Objectives/MissionData.cpp
@@ -1596,45 +1596,6 @@ void CMissionData::LoadDirectlyFromMapFile(idMapFile* mapFile)
 	}
 }
 
-/**==========================================================================
-* CObjective
-*==========================================================================**/
-
-CObjective::CObjective()
-{
-	Clear();
-}
-
-CObjective::~CObjective()
-{
-	Clear();
-}
-
-void CObjective::Clear( void )
-{
-	m_ObjNum = -1;
-	m_state = STATE_INCOMPLETE;
-	m_text = "";
-	m_bNeedsUpdate = false;
-	m_bMandatory = false;
-	m_bReversible = true;
-	m_bLatched = false;
-	m_bVisible = true;
-	m_bOngoing = false;
-	m_bApplies = true;
-	m_handle = 0;
-	m_Components.Clear();
-	m_EnablingObjs.Clear();
-	m_CompletionTarget.Clear();
-	m_FailureTarget.Clear();
-	m_CompletionScript.Clear();
-	m_FailureScript.Clear();
-	m_SuccessLogicStr.Clear();
-	m_FailureLogicStr.Clear();
-	m_SuccessLogic.Clear();
-	m_FailureLogic.Clear();
-}
-
 void CMissionData::InventoryCallback(idEntity *ent, idStr ItemName, int value, int OverallVal, bool bPickedUp)
 {
 	SObjEntParms Parms;
@@ -1702,83 +1663,6 @@ void CMissionData::AddMissionLoot(LootType lootType, int amount)
 {
 	// greebo: add to the individual sum
 	m_Stats.LootInMission[lootType] += amount;
-}
-
-// =============== Boolean Logic Parsing for Objective Failure/Success ==============
-
-bool CObjective::CheckFailure( void )
-{
-	bool bTest(false);
-
-	if( !m_FailureLogic.IsEmpty() )
-		bTest = gameLocal.m_MissionData->EvalBoolLogic( &m_FailureLogic, false, m_ObjNum );
-	else
-	{
-		// Default logic: If ANY components of an ongoing objective are false, the objective is failed
-		if( m_bOngoing && !(m_state == STATE_INVALID) )
-		{
-			bTest = true;
-			for( int j=0; j < m_Components.Num(); j++ )
-			{
-				bTest = bTest && m_Components[j].m_bState;
-			}
-
-			bTest = !bTest;
-		}
-	}
-	return bTest;
-}
-
-bool CObjective::CheckSuccess( void )
-{
-	bool bTest(true);
-	DM_LOG(LC_OBJECTIVES,LT_DEBUG)LOGSTRING("[Objective Logic] Check Success Called \r");
-
-	if( !m_SuccessLogic.IsEmpty() )
-	{
-		DM_LOG(LC_OBJECTIVES,LT_DEBUG)LOGSTRING("[Objective Logic] Evaluating custom success logic \r");
-		bTest = gameLocal.m_MissionData->EvalBoolLogic( &m_SuccessLogic, false, m_ObjNum );
-	}
-	else
-	{
-		DM_LOG(LC_OBJECTIVES,LT_DEBUG)LOGSTRING("[Objective Logic] Evaluating default success logic \r");
-		// Default logic: All components must be true to succeed
-		for( int j=0; j < m_Components.Num(); j++ )
-		{
-			bTest = bTest && m_Components[j].m_bState;
-		}
-	}
-	return bTest;
-}
-
-
-/**
-* Parse the boolean logic strings into matrices.
-* Returns false if there was an error in the parsing.
-**/
-bool CObjective::ParseLogicStrs( void )
-{
-	bool bReturnVal(true), bTemp(false);
-
-	if( m_SuccessLogicStr != "" )
-	{
-		bReturnVal = gameLocal.m_MissionData->ParseLogicStr( &m_SuccessLogicStr, &m_SuccessLogic );
-		
-		if( !bReturnVal )
-			gameLocal.Error("Objective success logic failed to parse \n");
-	}
-
-	if( m_FailureLogicStr != "" )
-	{
-		bTemp = gameLocal.m_MissionData->ParseLogicStr( &m_FailureLogicStr, &m_FailureLogic );
-		
-		if( !bTemp )
-			gameLocal.Error("Objective failure logic failed to parse \n");
-
-		bReturnVal = bReturnVal && bTemp;
-	}
-
-	return bReturnVal;
 }
 
 /**
@@ -2610,75 +2494,6 @@ void CMissionData::UpdateStatisticsGUI(idUserInterface* gui, const idStr& listDe
 	//value = idStr(m_Stats.AIAlerts[1].Overall + m_Stats.AIAlerts[2].Overall) + " Minor, " + idStr(m_Stats.AIAlerts[3].Overall + m_Stats.AIAlerts[4].Overall) + " Searches, " + idStr(m_Stats.AIAlerts[5].Overall) + " Sightings";
 	//gui->SetStateString(prefix + idStr(index++), key + divider + value + postfix);	
 	
-}
-
-void CObjective::Save( idSaveGame *savefile ) const
-{
-	savefile->WriteString( m_text );
-	savefile->WriteBool( m_bMandatory );
-	savefile->WriteBool( m_bVisible );
-	savefile->WriteBool( m_bOngoing );
-	savefile->WriteBool( m_bApplies );
-	savefile->WriteInt( m_ObjNum );
-	savefile->WriteInt( m_handle );
-	savefile->WriteInt( m_state );
-	savefile->WriteBool( m_bNeedsUpdate );
-	savefile->WriteBool( m_bReversible );
-	savefile->WriteBool( m_bLatched );
-
-	savefile->WriteInt( m_Components.Num() );
-	for( int i=0; i < m_Components.Num(); i++ )
-		m_Components[i].Save( savefile );
-
-	savefile->WriteInt( m_EnablingObjs.Num() );
-	for( int j=0; j < m_EnablingObjs.Num(); j++ )
-		savefile->WriteInt( m_EnablingObjs[j] );
-
-	savefile->WriteString(m_CompletionTarget);
-	savefile->WriteString(m_FailureTarget);
-
-	savefile->WriteString( m_CompletionScript );
-	savefile->WriteString( m_FailureScript );
-	savefile->WriteString( m_SuccessLogicStr );
-	savefile->WriteString( m_FailureLogicStr );
-}
-
-void CObjective::Restore( idRestoreGame *savefile )
-{
-	int num(0), tempInt(0);
-	savefile->ReadString( m_text );
-	savefile->ReadBool( m_bMandatory );
-	savefile->ReadBool( m_bVisible );
-	savefile->ReadBool( m_bOngoing );
-	savefile->ReadBool( m_bApplies );
-	savefile->ReadInt( m_ObjNum );
-	savefile->ReadInt( m_handle );
-	savefile->ReadInt( tempInt );
-	m_state = (EObjCompletionState) tempInt;
-	savefile->ReadBool( m_bNeedsUpdate );
-	savefile->ReadBool( m_bReversible );
-	savefile->ReadBool( m_bLatched );
-
-	savefile->ReadInt( num );
-	m_Components.SetNum( num );
-	for( int i=0; i < num; i++ )
-		m_Components[i].Restore( savefile );
-
-	savefile->ReadInt( num );
-	m_EnablingObjs.SetNum( num );
-	for( int j=0; j < num; j++ )
-		savefile->ReadInt( m_EnablingObjs[j] );
-
-	savefile->ReadString(m_CompletionTarget);
-	savefile->ReadString(m_FailureTarget);
-
-	savefile->ReadString( m_CompletionScript );
-	savefile->ReadString( m_FailureScript );
-	savefile->ReadString( m_SuccessLogicStr );
-	savefile->ReadString( m_FailureLogicStr );
-
-	// We have to re-parse the logic since the parse nodes involve raw pointer linkages
-	ParseLogicStrs();
 }
 
 void CMissionData::SetPlayerTeam(int team)
