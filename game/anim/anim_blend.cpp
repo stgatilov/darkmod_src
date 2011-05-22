73fdb85248b847f399c2ae0f2f50aa215f07bcdd
greebo <greebo@49c82d7f-2e2a-0410-a16f-ae8f201b507f>
2011-05-22 06:45:07 +0000

    Fix compilation in Linx, now that the preprocessor indirection in Misc.h is gone.
    git-svn-id: http://svn.thedarkmod.com/svn/darkmod_src/trunk@4861 49c82d7f-2e2a-0410-a16f-ae8f201b507f

--- game/anim/anim_blend.cpp
+++ game/anim/anim_blend.cpp
@@ -3569,11 +3569,12 @@ bool idDeclModelDef::Parse( const char *text, const int textLength ) {
 			}
 
 			for( i = ANIMCHANNEL_ALL + 1; i < ANIM_NumAnimChannels; i++ ) {
-#ifdef MACOS_X
-				if ( !strcasecmp( channelNames[ i ], token2.c_str() ) ) {
+#if MACOS_X || __linux__
+				if ( !strcasecmp( channelNames[ i ], token2.c_str() ) )
 #else
-				if ( !stricmp( channelNames[ i ], token2.c_str() ) ) {
+				if ( !stricmp( channelNames[ i ], token2.c_str() ) )
 #endif
+				{
 					break;
 				}
 			}
