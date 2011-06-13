3b6fd2c409e5ab14cb9982554621dbc7986156cf
greebo <greebo@49c82d7f-2e2a-0410-a16f-ae8f201b507f>
2011-06-13 08:43:26 +0000

    Fix issue #2774: Restart Mission crashes after saving and dying when a weapon is drawn
    git-svn-id: http://svn.thedarkmod.com/svn/darkmod_src/branches/release1.06@4885 49c82d7f-2e2a-0410-a16f-ae8f201b507f

--- game/player.cpp
+++ game/player.cpp
@@ -572,7 +572,7 @@ void idPlayer::Init( void ) {
 	oldFlags				= 0;
 
 	currentWeapon			= -1;
-	idealWeapon				= -1;
+	idealWeapon				= 0;
 	previousWeapon			= -1;
 	weaponSwitchTime		= 0;
 	weaponEnabled			= true;
@@ -2397,8 +2397,13 @@ void idPlayer::SavePersistantInfo( void ) {
 	idDict &playerInfo = gameLocal.persistentPlayerInfo[entityNumber];
 
 	playerInfo.Clear();
+
+	// greebo: Don't persist current weapon when switching maps (issue #2774)
+	// Keep writing the health value to the dict, the engine is using that to query the player's health
 	playerInfo.SetInt( "health", health );
+#if 0
 	playerInfo.SetInt( "current_weapon", currentWeapon );
+#endif
 }
 
 /*
@@ -2413,12 +2418,16 @@ void idPlayer::RestorePersistantInfo( void ) {
 		gameLocal.persistentPlayerInfo[entityNumber].Clear();
 	}
 
+	// greebo: TDM doesn't need to load health or weapon from the dict, that is not (yet) intended since
+	// map switching within missions is not yet planned or fleshed out
+#if 0
 	spawnArgs.Copy( gameLocal.persistentPlayerInfo[entityNumber] );
 
 	health = spawnArgs.GetInt( "health", "100" );
 	if ( !gameLocal.isClient ) {
 		idealWeapon = spawnArgs.GetInt( "current_weapon", "0" );
 	}
+#endif
 }
 
 /*
