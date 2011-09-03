6119e0a011b2fef91d93cdc98a939a00b828fe7d
tels <tels@49c82d7f-2e2a-0410-a16f-ae8f201b507f>
2011-09-03 14:20:43 +0000

    fix #2812: transform 0xff into 0xb6 at load-time of any lang dict, so it works with the menu, the HUD, and any FM dict including readables.
    git-svn-id: http://svn.thedarkmod.com/svn/darkmod_src/trunk@4968 49c82d7f-2e2a-0410-a16f-ae8f201b507f

--- idlib/langdict.cpp
+++ idlib/langdict.cpp
@@ -1,3 +1,4 @@
+// vim:ts=4:sw=4:cindent
 /***************************************************************************
  *
  * PROJECT: The Dark Mod
@@ -51,7 +52,7 @@ void idLangDict::Clear( void ) {
 idLangDict::Load
 ============
 */
-bool idLangDict::Load( const char *fileName, bool clear /* _D3XP */ ) {
+bool idLangDict::Load( const char *fileName, const bool clear /* _D3XP */, const char fix_0xff ) {
 	
 	if ( clear ) {
 		Clear();
@@ -83,6 +84,11 @@ bool idLangDict::Load( const char *fileName, bool clear /* _D3XP */ ) {
 			idLangKeyValue kv;
 			kv.key = tok;
 			kv.value = tok2;
+			if (fix_0xff != 0x00) {
+				// tels: fix #2812, character 0xFF ("я" in russian) is not rendered in the GUI
+				// so replace it with 0xb6 (182) as the font contains the character there:
+				kv.value.Replace( '\xFF', fix_0xff );
+			}
 			assert( kv.key.Cmpn( STRTABLE_ID, STRTABLE_ID_LENGTH ) == 0 );
 			hash.Add( GetHashKey( kv.key ), args.Append( kv ) );
 		}
@@ -130,7 +136,7 @@ void idLangDict::Save( const char *fileName ) {
 idLangDict::GetString
 ============
 */
-const char *idLangDict::GetString( const char *str, bool dowarn ) const {
+const char *idLangDict::GetString( const char *str, const bool dowarn ) const {
 
 	if ( str == NULL || str[0] == '\0' ) {
 		return "";
@@ -198,7 +204,7 @@ int idLangDict::GetNumKeyVals( void ) const {
 idLangDict::GetKeyVal
 ============
 */
-const idLangKeyValue * idLangDict::GetKeyVal( int i ) const {
+const idLangKeyValue * idLangDict::GetKeyVal( const int i ) const {
 	return &args[i];
 }
 
--- idlib/langdict.h
+++ idlib/langdict.h
@@ -1,3 +1,4 @@
+// vim:ts=4:sw=4:cindent
 /***************************************************************************
  *
  * PROJECT: The Dark Mod
@@ -33,11 +34,12 @@ public:
 							~idLangDict( void );
 
 	void					Clear( void );
-	bool					Load( const char *fileName, bool clear = true );
+	// Tels: #2812: replace 0xff by 0xb6 (for correct russian rendering in the GUI, HUD and readables)
+	bool					Load( const char *fileName, const bool clear = true, const char replace_0xff = '\xb6' );
 	void					Save( const char *fileName );
 
 	const char *			AddString( const char *str );
-	const char *			GetString( const char *str, bool dowarn = true ) const;
+	const char *			GetString( const char *str, const bool dowarn = true ) const;
 
 	/**
 	* Tels: Print some statistics about memory usage.
@@ -47,9 +49,9 @@ public:
 	void					AddKeyVal( const char *key, const char *val );
 
 	int						GetNumKeyVals( void ) const;
-	const idLangKeyValue *	GetKeyVal( int i ) const;
+	const idLangKeyValue *	GetKeyVal(const int i) const;
 
-	void					SetBaseID(int id) { baseID = id; };
+	void					SetBaseID(const int id) { baseID = id; };
 
 private:
 	idList<idLangKeyValue>	args;
