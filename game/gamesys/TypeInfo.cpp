d894b7aa49aa65cb872c5c8c0ad5e24e259d6b5b
greebo <greebo@49c82d7f-2e2a-0410-a16f-ae8f201b507f>
2012-01-06 19:47:56 +0000

    Compilation fix in Mac OSX, since the precompiled header must be applicable to the entire project.
    git-svn-id: http://svn.thedarkmod.com/svn/darkmod_src/trunk@5169 49c82d7f-2e2a-0410-a16f-ae8f201b507f

--- game/gamesys/TypeInfo.cpp
+++ game/gamesys/TypeInfo.cpp
@@ -869,7 +869,7 @@ int idTypeInfoTools::WriteVariable_r( const void *varPtr, const char *varName, c
 		}
 
 	} else if ( token == "idExtrapolate" ) {
-
+#ifndef MACOS_X // greebo: Hack to get precompiled headers working, where the private==public hack won't work
 		const idExtrapolate<float> *interpolate = ((idExtrapolate<float> *)varPtr);
 		Write( varName, varType, scope, prefix, ".extrapolationType", idStr( interpolate->GetExtrapolationType() ).c_str(), &interpolate->extrapolationType, sizeof( interpolate->extrapolationType ) );
 		Write( varName, varType, scope, prefix, ".startTime", idStr( interpolate->GetStartTime() ).c_str(), &interpolate->startTime, sizeof( interpolate->startTime ) );
@@ -974,7 +974,7 @@ int idTypeInfoTools::WriteVariable_r( const void *varPtr, const char *varName, c
 				Write( varName, varType, scope, prefix, "", va( "<unknown template argument type '%s' for idInterpolateAccelDecelSine>", templateArgs.c_str() ), NULL, 0 );
 			}
 		}
-
+#endif
 	} else if ( token == "idUserInterface" ) {
 
 		typeSize = sizeof( idUserInterface );
