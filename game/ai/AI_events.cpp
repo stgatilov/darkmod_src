775f3201d13944aa609a4c0b90470f54566af69a
tels <tels@49c82d7f-2e2a-0410-a16f-ae8f201b507f>
2012-01-13 23:20:44 +0000

    try to silence warning about NULL in arithmetic
    git-svn-id: http://svn.thedarkmod.com/svn/darkmod_src/trunk@5211 49c82d7f-2e2a-0410-a16f-ae8f201b507f

--- game/ai/AI_events.cpp
+++ game/ai/AI_events.cpp
@@ -828,7 +828,7 @@ void idAI::Event_SpawnThrowableProjectile(const char* projectileName, const char
 	EnsureActiveProjectileInfo();
 
 	// Bind to joint
-	if (jointName == NULL || jointName[0] == NULL)
+	if ((jointName == NULL) || (jointName[0] == NULL))
 	{
 		// No valid joint name
 		activeProjectile.projEnt.GetEntity()->Bind(this, true);
