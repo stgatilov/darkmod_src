64502fea069b8c0eda0761bb847fdb4d0b647f46
tels <tels@49c82d7f-2e2a-0410-a16f-ae8f201b507f>
2012-03-10 14:14:30 +0000

    add linux build script again
    git-svn-id: http://svn.thedarkmod.com/svn/darkmod_src/trunk@5327 49c82d7f-2e2a-0410-a16f-ae8f201b507f

---
+++ linuxBuild.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+mkdir -p ~/.doom3/darkmod
+
+# make sure this file exists
+touch scons.signatures.dblite
+
+# FAST=true
+# --debug=explain
+
+# $@ = pass along the flags like "BUILD=profile" or "BUILD=debug"
+time scons -j2 BUILD_GAMEPAK=1 NO_GCH=0 --debug=explain "$@"
+mv gamex86-base.so gamex86.so
+#strip gamex86.so
+#strip thedarkmod.x86
+
+cp thedarkmod.x86 ~/.doom3/
+
+# Only if this directory exists
+if [ -d ~/games/doom3/ ]; then
+  cp thedarkmod.x86 ~/games/doom3/thedarkmod.x86
+fi
+
+cp tdm_game02.pk4 ~/.doom3/darkmod/tdm_game02.pk4
+if [ -f ~/.doom3/darkmod/gamex86.so ]; then
+  rm ~/.doom3/darkmod/gamex86.so
+fi
+
