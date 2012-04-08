3f3b8c4baca93f93f19d7eada5fce708ca839ccb
serpentine <serpentine@49c82d7f-2e2a-0410-a16f-ae8f201b507f>
2012-04-08 18:39:09 +0000

    Newer, cleaner implementation of the RSA MD4 - added the block hash as calls expect.
    git-svn-id: http://svn.thedarkmod.com/svn/darkmod_src/trunk@5372 49c82d7f-2e2a-0410-a16f-ae8f201b507f

--- idlib/hashing/MD4.h
+++ idlib/hashing/MD4.h
@@ -1,14 +1,41 @@
+/*
+ * This is an OpenSSL-compatible implementation of the RSA Data Security, Inc.
+ * MD4 Message-Digest Algorithm (RFC 1320).
+ *
+ * Homepage:
+ * http://openwall.info/wiki/people/solar/software/public-domain-source-code/md4
+ *
+ * Author:
+ * Alexander Peslyak, better known as Solar Designer <solar at openwall.com>
+ *
+ * This software was written by Alexander Peslyak in 2001.  No copyright is
+ * claimed, and the software is hereby placed in the public domain.
+ * In case this attempt to disclaim copyright and place the software in the
+ * public domain is deemed null and void, then the software is
+ * Copyright (c) 2001 Alexander Peslyak and it is hereby released to the
+ * general public under the following terms:
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted.
+ *
+ * There's ABSOLUTELY NO WARRANTY, express or implied.
+ *
+ * See md4.c for more information.
+ */
+ 
 #ifndef __MD4_H__
 #define __MD4_H__
 
-/*
-===============================================================================
-
-	Calculates a checksum for a block of data
-	using the MD4 message-digest algorithm.
-
-===============================================================================
-*/
+ 
+/* Any 32-bit or wider unsigned integer data type will do */
+typedef unsigned int MD4_u32plus;
+ 
+typedef struct {
+	MD4_u32plus lo, hi;
+	MD4_u32plus a, b, c, d;
+	unsigned char buffer[64];
+	MD4_u32plus block[16];
+} MD4_CTX;
 
 unsigned long MD4_BlockChecksum( const void *data, int length );
 
