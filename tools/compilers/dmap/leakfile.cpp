05fcd6e82bde30619dc370bb96e8457c27668362
tels <tels@49c82d7f-2e2a-0410-a16f-ae8f201b507f>
2012-05-11 14:19:58 +0000

    fix #3115 (thanx taaki!)
    git-svn-id: http://svn.thedarkmod.com/svn/darkmod_src/trunk@5446 49c82d7f-2e2a-0410-a16f-ae8f201b507f

--- tools/compilers/dmap/leakfile.cpp
+++ tools/compilers/dmap/leakfile.cpp
@@ -61,7 +61,7 @@ void LeakFile (tree_t *tree)
 	// write the points to the file
 	//
 	sprintf( filename, "%s.lin", dmapGlobals.mapFileBase );
-	ospath = fileSystem->RelativePathToOSPath( filename );
+	ospath = fileSystem->RelativePathToOSPath( filename, "fs_basepath" );
 	linefile = fopen( ospath, "w" );
 	if ( !linefile ) {
 		common->Error( "Couldn't open %s\n", ospath.c_str() );
