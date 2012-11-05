5bc34bf9f97f27b7202cd3743061820c01f393f5
greebo <greebo@49c82d7f-2e2a-0410-a16f-ae8f201b507f>
2012-11-05 05:19:00 +0000

    Remove tdmlauncher sources, retrieve them from the history if needed.
    git-svn-id: http://svn.thedarkmod.com/svn/darkmod_src/trunk@5649 49c82d7f-2e2a-0410-a16f-ae8f201b507f

--- dmlauncher/macosx_build.sh
+++
@@ -1,6 +0,0 @@
-scons -c
-scons BUILD="release" MACOSX_TARGET_ARCH="i386"
-cp tdmlauncher.macosx tdmlauncher.i386.macosx
-scons BUILD="release" MACOSX_TARGET_ARCH="ppc"
-cp tdmlauncher.macosx tdmlauncher.ppc.macosx
-lipo -arch i386 tdmlauncher.i386.macosx -arch ppc tdmlauncher.ppc.macosx -create -output tdmlauncher.macosx
