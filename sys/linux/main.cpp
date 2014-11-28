1480a90aa39bae56e0e07977494c8c92f9894ec2
greebo <greebo@49c82d7f-2e2a-0410-a16f-ae8f201b507f>
2014-11-28 14:36:30 +0000

    Apply dhewm3 / ab201cfc06886fef4390be5a4541c1999f5f55bc: Stub Sys_GetClockTicks for x86_64 This is only used for debugging/stats.
    git-svn-id: http://svn.thedarkmod.com/svn/darkmod_src/branches/x64@6188 49c82d7f-2e2a-0410-a16f-ae8f201b507f

--- sys/linux/main.cpp
+++ sys/linux/main.cpp
@@ -299,7 +299,7 @@ double Sys_GetClockTicks( void ) {
 						  : "=r" (lo), "=r" (hi) );
 	return (double) lo + (double) 0xFFFFFFFF * hi;
 #else
-#error unsupported CPU
+    return 0.0;
 #endif
 }
 
