4ec036c543fc92cecda300c42097516572a782e8
greebo <greebo@49c82d7f-2e2a-0410-a16f-ae8f201b507f>
2014-11-30 08:32:45 +0000

    Apply dhewm3 / 5e05c5b031309fd7e803253536fc5c4553c03e05: Get rid of s_libOpenAL and s_useOpenAL Both unused.
    git-svn-id: http://svn.thedarkmod.com/svn/darkmod_src/branches/x64@6300 49c82d7f-2e2a-0410-a16f-ae8f201b507f

--- sound/snd_system.cpp
+++ sound/snd_system.cpp
@@ -64,13 +64,9 @@ idCVar idSoundSystemLocal::s_enviroSuitVolumeScale( "s_enviroSuitVolumeScale", "
 idCVar idSoundSystemLocal::s_skipHelltimeFX( "s_skipHelltimeFX", "0", CVAR_SOUND | CVAR_BOOL, "" );
 
 #if ID_OPENAL
-idCVar idSoundSystemLocal::s_libOpenAL( "s_libOpenAL", "openal32.dll", CVAR_SOUND | CVAR_ARCHIVE, "Deprecated, kept for compatibility" );
-idCVar idSoundSystemLocal::s_useOpenAL( "s_useOpenAL", "0", CVAR_SOUND | CVAR_BOOL | CVAR_ARCHIVE, "Deprecated, kept for compatibility" );
 idCVar idSoundSystemLocal::s_useEAXReverb( "s_useEAXReverb", "0", CVAR_SOUND | CVAR_BOOL | CVAR_ARCHIVE, "use EAX reverb" );
 idCVar idSoundSystemLocal::s_decompressionLimit( "s_decompressionLimit", "6", CVAR_SOUND | CVAR_INTEGER | CVAR_ARCHIVE, "specifies maximum uncompressed sample length in seconds" );
 #else
-idCVar idSoundSystemLocal::s_libOpenAL( "s_libOpenAL", "openal32.dll", CVAR_SOUND | CVAR_ARCHIVE, "OpenAL is not supported in this build" );
-idCVar idSoundSystemLocal::s_useOpenAL( "s_useOpenAL", "0", CVAR_SOUND | CVAR_BOOL | CVAR_ROM, "OpenAL is not supported in this build" );
 idCVar idSoundSystemLocal::s_useEAXReverb( "s_useEAXReverb", "0", CVAR_SOUND | CVAR_BOOL | CVAR_ROM, "EAX not available in this build" );
 idCVar idSoundSystemLocal::s_decompressionLimit( "s_decompressionLimit", "6", CVAR_SOUND | CVAR_INTEGER | CVAR_ROM, "specifies maximum uncompressed sample length in seconds" );
 #endif
@@ -318,8 +314,6 @@ void idSoundSystemLocal::Init() {
 	}
 
 	// set up openal device and context
-	common->StartupVariable( "s_useOpenAL", true );
-
     common->Printf("Setup OpenAL device and context\n");
 
     const char *device = s_device.GetString();
