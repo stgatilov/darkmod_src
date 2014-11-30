7c47e69da0f67652bc74e8ef6fbbca0b41338215
greebo <greebo@49c82d7f-2e2a-0410-a16f-ae8f201b507f>
2014-11-30 07:04:41 +0000

    Apply dhewm3 / 668802f09ad23f0fcf59617cb2460d37b58d742e: Cleanup OpenAL includes We use OpenAL Soft on all platforms.
    git-svn-id: http://svn.thedarkmod.com/svn/darkmod_src/branches/x64@6282 49c82d7f-2e2a-0410-a16f-ae8f201b507f

--- sound/snd_local.h
+++ sound/snd_local.h
@@ -21,23 +21,12 @@
 #define __SND_LOCAL_H__
 
 // you need the OpenAL headers for build, even if AL is not enabled - http://www.openal.org/
-#ifdef _WIN32
-#include "AL/al.h"
-#include "AL/alc.h"
-#include "AL/alext.h"
-// broken OpenAL SDK ?
-#define ID_ALCHAR (ALubyte *)
-#elif defined( MACOS_X )
-#include <OpenAL/al.h>
-#include <OpenAL/alc.h>
-#include <OpenAL/alext.h>
-#define ID_ALCHAR
-#else         
 #include <AL/al.h>
 #include <AL/alc.h>
 #include <AL/alext.h>
-#define ID_ALCHAR
-#endif
+
+#include "../framework/UsercmdGen.h"
+#include "sound.h"
 #include "efxlib.h"
 
 // demo sound commands
