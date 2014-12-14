bb89242cee7e92b1057d34dfac32fb97ef797c42
greebo <greebo@49c82d7f-2e2a-0410-a16f-ae8f201b507f>
2014-12-14 11:58:56 +0000

    Remove the temporary videoMap_mpeg keyword.
    git-svn-id: http://svn.thedarkmod.com/svn/darkmod_src/branches/x64@6406 49c82d7f-2e2a-0410-a16f-ae8f201b507f

--- renderer/Material.cpp
+++ renderer/Material.cpp
@@ -1188,25 +1188,6 @@ void idMaterial::ParseStage( idLexer &src, const textureRepeat_t trpDefault ) {
 			ts->cinematic->InitFromFile( token.c_str(), loop );
 			continue;
 		}
-        else if (!token.Icmp("videomap_mpeg")) {
-            // note that videomaps will always be in clamp mode, so texture
-            // coordinates had better be in the 0 to 1 range
-            if (!src.ReadToken(&token)) {
-                common->Warning("missing parameter for 'videoMap_mpeg' keyword in material '%s'", GetName());
-                continue;
-            }
-            bool loop = false;
-            if (!token.Icmp("loop")) {
-                loop = true;
-                if (!src.ReadToken(&token)) {
-                    common->Warning("missing parameter for 'videoMap' keyword in material '%s'", GetName());
-                    continue;
-                }
-            }
-            ts->cinematic = new idCinematicFFMpeg();
-            ts->cinematic->InitFromFile(token.c_str(), loop);
-            continue;
-        }
 		else if ( !token.Icmp( "soundmap" ) ) {
 			if ( !src.ReadToken( &token ) ) {
 				common->Warning( "missing parameter for 'soundmap' keyword in material '%s'", GetName() );
