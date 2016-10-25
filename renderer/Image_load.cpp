13661d38bac9fe9440c27139f1755237b0b07c40
duzenko <duzenko@49c82d7f-2e2a-0410-a16f-ae8f201b507f>
2016-10-25 21:37:15 +0000

    #4401 - gl vendor check
    git-svn-id: http://svn.thedarkmod.com/svn/darkmod_src/trunk@6646 49c82d7f-2e2a-0410-a16f-ae8f201b507f

--- renderer/Image_load.cpp
+++ renderer/Image_load.cpp
@@ -647,7 +647,6 @@ void idImage::GenerateImage( const byte *pic, int width, int height,
 	// upload the main image level
 	Bind();
 
-
 	if ( internalFormat == GL_COLOR_INDEX8_EXT ) {
 		/*
 		if ( depth == TD_BUMP ) {
@@ -662,7 +661,8 @@ void idImage::GenerateImage( const byte *pic, int width, int height,
 		if (automaticMipmaps && !preserveBorder) // duzenko #4401
 			glTexParameteri(GL_TEXTURE_2D, GL_GENERATE_MIPMAP, GL_TRUE);
 		qglTexImage2D( GL_TEXTURE_2D, 0, internalFormat, scaled_width, scaled_height, 0, GL_RGBA, GL_UNSIGNED_BYTE, scaledBuffer );
-		glTexParameteri(GL_TEXTURE_2D, GL_GENERATE_MIPMAP, GL_FALSE);
+		if (strcmp(glConfig.vendor_string, "Intel"))
+			glTexParameteri(GL_TEXTURE_2D, GL_GENERATE_MIPMAP, GL_FALSE);
 	}
 	
 	// create and upload the mip map levels, which we do in all cases, even if we don't think they are needed
