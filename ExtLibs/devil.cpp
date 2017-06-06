13ec1e2e0f2caa68ca7cc30f7c894af90c3f608e
duzenko <duzenko@49c82d7f-2e2a-0410-a16f-ae8f201b507f>
2017-06-06 21:08:59 +0000

    #4538 - Fixed x64 build
    git-svn-id: http://svn.thedarkmod.com/svn/darkmod_src/trunk@6923 49c82d7f-2e2a-0410-a16f-ae8f201b507f

---
+++ ExtLibs/devil.cpp
@@ -0,0 +1,11 @@
+#include "stdafx.h"
+#include "devil.h"
+
+namespace ExtLibs {
+	ILAPI ILvoid    ILAPIENTRY ilInit( void ) {
+		return ::ilInit();
+	}
+	EXTLIB ILAPI ILboolean ILAPIENTRY ilLoadL( ILenum Type, const ILvoid *Lump, ILuint Size ) {
+		return ::ilLoadL( Type, Lump, Size );
+	}
+}
---
+++ ExtLibs/devil.h
@@ -0,0 +1,13 @@
+#include <IL/il.h>
+
+#ifdef MAKEDLL
+#  define EXTLIB __declspec(dllexport)
+#else
+#  define EXTLIB __declspec(dllimport)
+#endif
+
+namespace ExtLibs {
+	EXTLIB ILAPI ILvoid    ILAPIENTRY ilInit( void );
+	EXTLIB ILAPI ILboolean ILAPIENTRY ilLoadL( ILenum Type, const ILvoid *Lump, ILuint Size );
+
+}
