34ac109d489d86269ae1389a0214cf1f9f0bc2fd
stgatilov <stgatilov@49c82d7f-2e2a-0410-a16f-ae8f201b507f>
2017-07-01 10:56:34 +0000

    Fixed Linux build: 1. ExtLibs now has method ov_use_custom_alloc, it is used to install custom allocator from core engine. 2. Separated ogg into yet another Scons script, now it returns object files.
    git-svn-id: http://svn.thedarkmod.com/svn/darkmod_src/branches/linuxbuild_2017_06@6986 49c82d7f-2e2a-0410-a16f-ae8f201b507f

--- ExtLibs/vorbis.h
+++ ExtLibs/vorbis.h
@@ -3,7 +3,16 @@
 
 #include "Export.h"
 
+struct ov_alloc_callbacks {
+	void* (*_decoder_malloc)( size_t size );
+	void* (*_decoder_calloc)( size_t num, size_t size );
+	void* (*_decoder_realloc)( void *memblock, size_t size );
+	void (*_decoder_free)( void *memblock );
+};
+
 namespace ExtLibs {
+	EXTLIB void ov_use_custom_alloc(ov_alloc_callbacks callbacks);
+
 	EXTLIB int ov_clear( OggVorbis_File *vf );
 	EXTLIB extern int ov_open_callbacks( void *datasource, OggVorbis_File *vf,
 		char *initial, long ibytes, ov_callbacks callbacks );
