e23ea30c2ad01a7b7c0a8beac4b4555b26264cc9
cabalistic <cabalistic@49c82d7f-2e2a-0410-a16f-ae8f201b507f>
2018-07-02 19:40:50 +0000

    Fix post-processing only rendering to quarter of the screen
    git-svn-id: http://svn.thedarkmod.com/svn/darkmod_src/trunk@7537 49c82d7f-2e2a-0410-a16f-ae8f201b507f

--- renderer/draw_common.cpp
+++ renderer/draw_common.cpp
@@ -1433,6 +1433,7 @@ void RB_Bloom() {
 	globalImages->bloomImage->CopyFramebuffer( 0, 0, w / 2, h / 2, false );
 
 	FB_SelectPrimary();
+	qglViewport( 0, 0, w, h );
 	FB_TogglePrimary( false );
 	GL_SelectTexture( 0 );
 	globalImages->currentRenderImage->Bind();
