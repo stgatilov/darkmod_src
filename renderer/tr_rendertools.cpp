53f1269765535ee621e50b4cff01967f4ada0bbe
duzenko <duzenko@49c82d7f-2e2a-0410-a16f-ae8f201b507f>
2018-12-07 14:35:14 +0000

    r_showlights: simpler visualization
    git-svn-id: http://svn.thedarkmod.com/svn/darkmod_src/trunk@7790 49c82d7f-2e2a-0410-a16f-ae8f201b507f

--- renderer/tr_rendertools.cpp
+++ renderer/tr_rendertools.cpp
@@ -1454,12 +1454,16 @@ void RB_ShowLights( void ) {
 		}
 
 		// non-hidden lines
-		if ( r_showLights.GetInteger() >= 2 ) {
-			GL_State( GLS_POLYMODE_LINE | GLS_DEPTHMASK  );
+		if ( r_showLights.GetInteger() == 2 ) {
+			GL_State( GLS_POLYMODE_LINE | GLS_DEPTHMASK );
 			qglDisable( GL_DEPTH_TEST );
+			GL_State( GLS_POLYMODE_LINE | GLS_DEPTHMASK | GLS_SRCBLEND_SRC_ALPHA );
 			int c = light->index % 7 + 1;
-			GL_FloatColor( c & 1, c & 2, c & 4 );
+			GL_FloatColor( c & 1, c & 2, c & 4, 0.4f );
+			qglDisable( GL_DEPTH_TEST );
 			RB_DrawElementsImmediate( tri );
+			GL_FloatColor( c & 1, c & 2, c & 4 );
+			qglEnable( GL_DEPTH_TEST );
 		}
 		int index = backEnd.viewDef->renderWorld->lightDefs.FindIndex( vLight->lightDef );
 
