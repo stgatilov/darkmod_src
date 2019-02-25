d211b5c45cd69b64f5d54cb0834d097d5c738f58
stgatilov <stgatilov@49c82d7f-2e2a-0410-a16f-ae8f201b507f>
2019-02-25 16:30:38 +0000

    #4817. Reimplemented Pk4Mappings without boost::spirit (medium risk).
    git-svn-id: http://svn.thedarkmod.com/svn/darkmod_src/trunk@7990 49c82d7f-2e2a-0410-a16f-ae8f201b507f

--- tdm_update/libtdm_update/Pk4Mappings.h
+++ tdm_update/libtdm_update/Pk4Mappings.h
@@ -19,11 +19,8 @@
 #include <list>
 #include <fstream>
 #include "StdFilesystem.h"
-#include <boost/spirit/include/classic.hpp>
 #include <functional>
 #include <regex>
-
-namespace bs = boost::spirit::classic;
 
 namespace tdm
 {
@@ -71,66 +68,57 @@ public:
 	void LoadFromString(const std::string& str)
 	{
 		clear();
+		auto npos = std::string::npos;
 
-		// Comment starting character: #
-		bs::rule<> char_start_comment = bs::ch_p('#');
+		//split file into non-empty lines
+		std::vector<std::string> lines;
+		stdext::split(lines, str, "\r\n");
 
-		// Define blank characters
-		bs::rule<> blanks_p = * bs::blank_p;
+		for (std::string line : lines) {
+			//remove comment (if present)
+			size_t commentStart = line.find_first_of("#");
+			if (commentStart != npos)
+				line.resize(commentStart);
 
-		// Define comment lines
-		bs::rule<> l_comment = blanks_p >> char_start_comment >> *(bs::print_p | bs::blank_p) >> bs::eol_p; 
+			//remove spaces at both ends
+			stdext::trim(line);
+			if (line.empty())
+				continue;
+			
+			//get position of colon
+			auto colon = line.find(':');
+			if (colon == npos) {
+				TraceLog::WriteLine(LOG_VERBOSE, "Unknown line in mappings file: " + line);
+				continue;
+			}
+			//break into pk4 name and regexes
+			AddFile(line.substr(0, colon));
+			std::string remains = line.substr(colon + 1);
 
-		// Define empty lines
-		bs::rule<> l_empty = blanks_p >> bs::eol_p; 
-
-		// A filename, is stopped by a colon
-		bs::rule<> filename_ident = +(bs::print_p - bs::ch_p(':') - bs::ch_p(' '));
-
-		// A pattern is one or more of any character except comma and space
-		bs::rule<> pattern = +(bs::print_p - bs::ch_p(' ') - bs::ch_p(','));
-
-		// The mapping
-		bs::rule<> l_mapping = 
-					blanks_p >> 
-					filename_ident[ boost::bind(&Pk4Mappings::AddFile, this, _1, _2) ] >> 
-					blanks_p >> 
-					bs::ch_p(':') >>
-					blanks_p >> 
-					+(pattern[ boost::bind(&Pk4Mappings::AddPattern, this, _1, _2) ] >> blanks_p >> (*bs::ch_p(',')) >> blanks_p) >>
-					blanks_p >> 
-					bs::eol_p
-		;
-
-		bs::rule<> lines = l_empty | l_comment | l_mapping;
-		bs::rule<> mappingDef =  bs::lexeme_d [ * lines ] ;
-
-		bs::parse_info<> info = bs::parse(str.c_str(), mappingDef);
-
-		if (info.full)
-		{
-			TraceLog::WriteLine(LOG_VERBOSE, "Successfully parsed the whole mapping file.");
+			//split remaining part into individual regexes (by spaces and commas)
+			std::vector<std::string> regexes;
+			stdext::split(regexes, remains, " ,");
+			for (const std::string &s : regexes)
+				AddPattern(s);
 		}
-		else
-		{
-			TraceLog::WriteLine(LOG_VERBOSE, "Could not fully parse the mapping file.");
-		}
+
+		TraceLog::WriteLine(LOG_VERBOSE, "Parsed the mapping file.");
 	}
 
 private:
-	void AddFile(char const* beg, char const* end)
+	void AddFile(const std::string &token)
 	{
-		push_back(Pk4Mapping(std::string(beg, end), Patterns()));
+		push_back(Pk4Mapping(token, Patterns()));
 	}
 
-	void AddPattern(char const* beg, char const* end)
+	void AddPattern(const std::string &token)
 	{
-		if (beg == end) return;
+		if (token.empty()) return;
 
 		// Set the destination on the last element
 		assert(!empty());
 
-        back().second.push_back(std::regex(beg, end, std::regex::ECMAScript));
+        back().second.push_back(std::regex(token, std::regex::ECMAScript));
 	}
 };
 
