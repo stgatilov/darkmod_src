9f6761e6d45e0eb9db0e5360a29d842120c72fc9
cabalistic <cabalistic@49c82d7f-2e2a-0410-a16f-ae8f201b507f>
2019-03-05 21:09:01 +0000

    Hide helper functions in anonymous namespace and move the unit tests
    git-svn-id: http://svn.thedarkmod.com/svn/darkmod_src/trunk@8029 49c82d7f-2e2a-0410-a16f-ae8f201b507f

--- renderer/GLSLProgram.cpp
+++ renderer/GLSLProgram.cpp
@@ -55,7 +55,6 @@ void GLSLProgram::AddUniformAlias( int alias, const char *uniformName ) {
 	int location = qglGetUniformLocation( program, uniformName );
 	if( location == -1 ) {
 		common->Warning( "Did not find active uniform: %s\n", uniformName );
-		return;
 	}
 	aliasLocationMap.Append( aliasLocation_t { alias, location } );
 }
@@ -188,88 +187,93 @@ void GLSLProgramLoader::LoadAndAttachShader( GLint shaderType, const char *sourc
 	}
 }
 
-std::string ReadFile( const char *sourceFile ) {
-	void *buf = nullptr;
-	int len = fileSystem->ReadFile( sourceFile, &buf );
-	if( buf == nullptr ) {
-		common->Warning( "Could not open shader file %s", sourceFile );
-		return "";
+
+namespace {
+
+	std::string ReadFile( const char *sourceFile ) {
+		void *buf = nullptr;
+		int len = fileSystem->ReadFile( sourceFile, &buf );
+		if( buf == nullptr ) {
+			common->Warning( "Could not open shader file %s", sourceFile );
+			return "";
+		}
+		std::string contents( static_cast< char* >( buf ), len );
+		fileSystem->FreeFile( buf );
+
+		return contents;
 	}
-	std::string contents( static_cast< char* >( buf ), len );
-	fileSystem->FreeFile( buf );
 
-	return contents;
-}
+	/**
+	 * Resolves include statements in GLSL source files.
+	 * Note that the parsing is primitive and not context-sensitive. It will not respect multi-line comments
+	 * or conditional preprocessor blocks, so keep includes simple in the source files!
+	 * 
+	 * Include directives should look like this:
+	 * 
+	 * #pragma tdm_include "somefile.glsl" // optional comment
+	 */
+	void ResolveIncludes( std::string &source, std::vector<std::string> &includedFiles ) {
+		static const std::regex includeRegex( R"regex(^[ \t]*#[ \t]*pragma[ \t]+tdm_include[ \t]+"(.*)"[ \t]*(?:\/\/.*)?\r?$)regex" );
 
-/**
- * Resolves include statements in GLSL source files.
- * Note that the parsing is primitive and not context-sensitive. It will not respect multi-line comments
- * or conditional preprocessor blocks, so keep includes simple in the source files!
- * 
- * Include directives should look like this:
- * 
- * #pragma tdm_include "somefile.glsl" // optional comment
- */
-void ResolveIncludes( std::string &source, std::vector<std::string> &includedFiles ) {
-	static const std::regex includeRegex( R"regex(^[ \t]*#[ \t]*pragma[ \t]+tdm_include[ \t]+"(.*)"[ \t]*(?:\/\/.*)?\r?$)regex" );
+		unsigned int currentFileNo = includedFiles.size() - 1;
+		unsigned int totalIncludedLines = 0;
 
-	unsigned int currentFileNo = includedFiles.size() - 1;
-	unsigned int totalIncludedLines = 0;
+		std::smatch match;
+		while( std::regex_search( source, match, includeRegex ) ) {
+			std::string fileToInclude( match[ 1 ].first, match[ 1 ].second );
+			if( std::find( includedFiles.begin(), includedFiles.end(), fileToInclude ) == includedFiles.end() ) {
+				int nextFileNo = includedFiles.size();
+				std::string includeContents = ReadFile( fileToInclude.c_str() );
+				includedFiles.push_back( fileToInclude );
+				ResolveIncludes( includeContents, includedFiles );
 
-	std::smatch match;
-	while( std::regex_search( source, match, includeRegex ) ) {
-		std::string fileToInclude( match[ 1 ].first, match[ 1 ].second );
-		if( std::find( includedFiles.begin(), includedFiles.end(), fileToInclude ) == includedFiles.end() ) {
-			int nextFileNo = includedFiles.size();
-			std::string includeContents = ReadFile( fileToInclude.c_str() );
-			includedFiles.push_back( fileToInclude );
-			ResolveIncludes( includeContents, includedFiles );
+				// also add a #line instruction at beginning and end of include so that
+				// compile errors are mapped to the correct file and line
+				// unfortunately, #line does not take an actual filename, but only an integral reference to a file :(
+				unsigned int currentLine = std::count( source.cbegin(), match[ 0 ].first, '\n' ) + 1 - totalIncludedLines;
+				std::string includeBeginMarker = "#line 0 " + std::to_string( nextFileNo ) + '\n';
+				std::string includeEndMarker = "\n#line " + std::to_string( currentLine ) + ' ' + std::to_string( currentFileNo );
+				totalIncludedLines += std::count( includeContents.begin(), includeContents.end(), '\n' ) + 2;
 
-			// also add a #line instruction at beginning and end of include so that
-			// compile errors are mapped to the correct file and line
-			// unfortunately, #line does not take an actual filename, but only an integral reference to a file :(
-			unsigned int currentLine = std::count( source.cbegin(), match[ 0 ].first, '\n' ) + 1 - totalIncludedLines;
-			std::string includeBeginMarker = "#line 0 " + std::to_string( nextFileNo ) + '\n';
-			std::string includeEndMarker = "\n#line " + std::to_string( currentLine ) + ' ' + std::to_string( currentFileNo );
-			totalIncludedLines += std::count( includeContents.begin(), includeContents.end(), '\n' ) + 2;
-
-			// replace include statement with content of included file
-			source.replace( match[ 0 ].first, match[ 0 ].second, includeBeginMarker + includeContents + includeEndMarker );
-		} else {
-			source.replace( match[ 0 ].first, match[ 0 ].second, "// already included " + fileToInclude );
+				// replace include statement with content of included file
+				source.replace( match[ 0 ].first, match[ 0 ].second, includeBeginMarker + includeContents + includeEndMarker );
+			} else {
+				source.replace( match[ 0 ].first, match[ 0 ].second, "// already included " + fileToInclude );
+			}
 		}
 	}
-}
 
-/**
- * Resolves dynamic defines statements in GLSL source files.
- * Note that the parsing is primitive and not context-sensitive. It will not respect multi-line comments
- * or conditional preprocessor blocks!
- * 
- * Define directives should look like this:
- * 
- * #pragma tdm_define "DEF_NAME" // optional comment
- * 
- * If DEF_NAME is contained in defines, the line will be replaced by
- * #define DEF_NAME <value>
- * 
- * Otherwise, it will be commented out.
- */
-void ResolveDefines( std::string &source, const idDict &defines ) {
-	static const std::regex defineRegex( R"regex(^[ \t]*#[ \t]*pragma[ \t]+tdm_define[ \t]+"(.*)"[ \t]*(?:\/\/.*)?\r?$)regex" );
-	
-	std::smatch match;
-	while( std::regex_search( source, match, defineRegex ) ) {
-		std::string define( match[ 1 ].first, match[ 1 ].second );
-		auto defIt = defines.FindKey( define.c_str() );
-		if( defIt != nullptr ) {
-			std::string replacement = "#define " + define + " " + defIt->GetValue().c_str();
-			source.replace( match[ 0 ].first, match[ 0 ].second, replacement );
-		} else {
-			std::string replacement = "// #undef " + define;
-			source.replace( match[ 0 ].first, match[ 0 ].second, replacement );
+	/**
+	 * Resolves dynamic defines statements in GLSL source files.
+	 * Note that the parsing is primitive and not context-sensitive. It will not respect multi-line comments
+	 * or conditional preprocessor blocks!
+	 * 
+	 * Define directives should look like this:
+	 * 
+	 * #pragma tdm_define "DEF_NAME" // optional comment
+	 * 
+	 * If DEF_NAME is contained in defines, the line will be replaced by
+	 * #define DEF_NAME <value>
+	 * 
+	 * Otherwise, it will be commented out.
+	 */
+	void ResolveDefines( std::string &source, const idDict &defines ) {
+		static const std::regex defineRegex( R"regex(^[ \t]*#[ \t]*pragma[ \t]+tdm_define[ \t]+"(.*)"[ \t]*(?:\/\/.*)?\r?$)regex" );
+		
+		std::smatch match;
+		while( std::regex_search( source, match, defineRegex ) ) {
+			std::string define( match[ 1 ].first, match[ 1 ].second );
+			auto defIt = defines.FindKey( define.c_str() );
+			if( defIt != nullptr ) {
+				std::string replacement = "#define " + define + " " + defIt->GetValue().c_str();
+				source.replace( match[ 0 ].first, match[ 0 ].second, replacement );
+			} else {
+				std::string replacement = "// #undef " + define;
+				source.replace( match[ 0 ].first, match[ 0 ].second, replacement );
+			}
 		}
 	}
+
 }
 
 GLuint GLSLProgramLoader::CompileShader( GLint shaderType, const char *sourceFile, const idDict &defines ) {
@@ -331,4 +335,142 @@ void GLSL_DestroyPrograms() {
 
 	delete globalPrograms.stencilInteractionShader;
 	globalPrograms.stencilInteractionShader = nullptr;
+}
+
+
+/// UNIT TESTS FOR SHADER INCLUDES AND DEFINES
+
+#include "../tests/doctest.h"
+
+namespace {
+	const std::string BASIC_SHADER =
+		"#version 150\n"
+		"void main() {}";
+	const std::string SHARED_COMMON =
+		"uniform vec4 someParam;\n"
+		"\n"
+		"vec4 doSomething {\n"
+		"  return someParam * 2;\n"
+		"}\n";
+	const std::string INCLUDE_SHADER =
+		"#version 140\n"
+		"#pragma tdm_include \"tests/shared_common.glsl\"\r\n"
+		"void main() {}\n";
+
+	const std::string NESTED_INCLUDE =
+		"#pragma tdm_include \"tests/shared_common.glsl\"\n"
+		"float myFunc() {\n"
+		"  return 0.3;\n"
+		"}";
+
+	const std::string ADVANCED_INCLUDES =
+		"#version 330\n"
+		"\n"
+		" #  pragma tdm_include \"tests/nested_include.glsl\"\n"
+		"#pragma  tdm_include \"tests/shared_common.glsl\"  // ignore this comment\n"
+		"#pragma tdm_include \"tests/advanced_includes.glsl\"\n"
+		"void main() {\n"
+		"  float myVar = myFunc();\n"
+		"}\n"
+		"#pragma tdm_include \"tests/basic_shader.glsl\"\n";
+
+	const std::string EXPANDED_INCLUDE_SHADER =
+		"#version 140\n"
+		"#line 0 1\n"
+		"uniform vec4 someParam;\n"
+		"\n"
+		"vec4 doSomething {\n"
+		"  return someParam * 2;\n"
+		"}\n"
+		"\n#line 2 0\n"
+		"void main() {}\n";
+	const std::string EXPANDED_ADVANCED_INCLUDES =
+		"#version 330\n"
+		"\n"
+		"#line 0 1\n"
+		"#line 0 2\n"
+		"uniform vec4 someParam;\n"
+		"\n"
+		"vec4 doSomething {\n"
+		"  return someParam * 2;\n"
+		"}\n"
+		"\n#line 1 1\n"
+		"float myFunc() {\n"
+		"  return 0.3;\n"
+		"}"
+		"\n#line 3 0\n"
+		"// already included tests/shared_common.glsl\n"
+		"// already included tests/advanced_includes.glsl\n"
+		"void main() {\n"
+		"  float myVar = myFunc();\n"
+		"}\n"
+		"#line 0 3\n"
+		"#version 150\n"
+		"void main() {}"
+		"\n#line 9 0\n";
+
+	std::string LoadSource( const std::string &sourceFile ) {
+		std::vector<std::string> includedFiles { sourceFile };
+		std::string source = ReadFile( sourceFile.c_str() );
+		ResolveIncludes( source, includedFiles );
+		return source;
+	}
+
+	TEST_CASE("Shader include handling", "[shaders]") {
+		INFO( "Preparing test shaders" );
+		fileSystem->WriteFile( "tests/basic_shader.glsl", BASIC_SHADER.c_str(), BASIC_SHADER.size(), "fs_savepath", "" );
+		fileSystem->WriteFile( "tests/shared_common.glsl", SHARED_COMMON.c_str(), SHARED_COMMON.size(), "fs_savepath", "" );
+		fileSystem->WriteFile( "tests/include_shader.glsl", INCLUDE_SHADER.c_str(), INCLUDE_SHADER.size(), "fs_savepath", "" );
+		fileSystem->WriteFile( "tests/nested_include.glsl", NESTED_INCLUDE.c_str(), NESTED_INCLUDE.size(), "fs_savepath", "" );
+		fileSystem->WriteFile( "tests/advanced_includes.glsl", ADVANCED_INCLUDES.c_str(), ADVANCED_INCLUDES.size(), "fs_savepath", "" );
+
+		SUBCASE( "Basic shader without includes remains unaltered" ) {
+			REQUIRE( LoadSource( "tests/basic_shader.glsl" ) == BASIC_SHADER );
+		}
+
+		SUBCASE( "Simple include works" ) {
+			REQUIRE( LoadSource( "tests/include_shader.glsl" ) == EXPANDED_INCLUDE_SHADER );
+		}
+
+		SUBCASE( "Multiple and nested includes" ) {
+			REQUIRE( LoadSource( "tests/advanced_includes.glsl" ) == EXPANDED_ADVANCED_INCLUDES );
+		}
+
+		INFO( "Cleaning up" );
+		fileSystem->RemoveFile( "tests/basic_shader.glsl", "" );
+		fileSystem->RemoveFile( "tests/shared_common.glsl", "" );
+		fileSystem->RemoveFile( "tests/include_shader.glsl", "" );
+		fileSystem->RemoveFile( "tests/nested_include.glsl", "" );
+		fileSystem->RemoveFile( "tests/advanced_includes.glsl", "" );
+	}
+
+	TEST_CASE("Shader defines handling", "[shaders]") {
+		const std::string shaderWithDynamicDefines =
+			"#version 140\n"
+			"#pragma tdm_define \"FIRST_DEFINE\"\n"
+			"\n"
+			"  # pragma   tdm_define   \"SECOND_DEFINE\"\n"
+			"void main() {\n"
+			"#ifdef FIRST_DEFINE\n"
+			"  return;\n"
+			"#endif\n"
+			"}\n" ;
+
+		const std::string expectedResult =
+			"#version 140\n"
+			"#define FIRST_DEFINE 1\n"
+			"\n"
+			"// #undef SECOND_DEFINE\n"
+			"void main() {\n"
+			"#ifdef FIRST_DEFINE\n"
+			"  return;\n"
+			"#endif\n"
+			"}\n" ;
+
+		std::string source = shaderWithDynamicDefines;
+		idDict defines;
+		defines.Set( "FIRST_DEFINE", "1" );
+		ResolveDefines( source, defines );
+		REQUIRE( source == expectedResult );
+	}
 }
--- tests/TestRun.cpp
+++ tests/TestRun.cpp
@@ -5,7 +5,11 @@
 #include "doctest.h"
 #include "TestRun.h"
 
-idCVar com_runTests( "com_runTests", "0", CVAR_SYSTEM|CVAR_INTEGER|CVAR_INIT, "If set, will run unit tests instead of main game" );
+/*
+ * To run tests, start TDM like this:
+ * TheDarkModx64.exe +set com_skipRenderer 1 +set com_runTests 2
+ */
+idCVar com_runTests( "com_runTests", "0", CVAR_SYSTEM|CVAR_INTEGER|CVAR_INIT, "If set, will run unit tests instead of main game. A value of 2 will spawn a console for output on Windows." );
 idCVar com_testParams( "com_testParams", "", CVAR_SYSTEM|CVAR_INIT, "Specify command line args for the test runner" );
 
 int RunTests() {
