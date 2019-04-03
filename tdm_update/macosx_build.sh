bc1f9e388c2fb042f1364bfc514d741ef2760631
stgatilov <stgatilov@49c82d7f-2e2a-0410-a16f-ae8f201b507f>
2019-04-03 15:26:23 +0000

    Fixed scons build scripts for tdm_update on Linux.
    Minizip is no longer built manually: it is taken just like any other dependency (like on Windows).
    
    
    git-svn-id: http://svn.thedarkmod.com/svn/darkmod_src/branches/3rdparty_4822@8140 49c82d7f-2e2a-0410-a16f-ae8f201b507f

--- tdm_update/macosx_build.sh
+++
@@ -1,6 +0,0 @@
-scons -c
-scons BUILD="release" TARGET_ARCH="x86"
-cp tdm_update.macosx tdm_update.i386.macosx
-scons BUILD="release" TARGET_ARCH="ppc"
-cp tdm_update.macosx tdm_update.ppc.macosx
-lipo -arch i386 tdm_update.i386.macosx -arch ppc tdm_update.ppc.macosx -create -output tdm_update.macosx
