e6db93e6b41e25c52f3c127e01bd260992c95b2a
stgatilov <stgatilov@49c82d7f-2e2a-0410-a16f-ae8f201b507f>
2019-04-04 16:35:04 +0000

    Added debug build for some libraries (pugixml), upgraded doctest to 2.2.3 to restore threadlocal workaround.
    git-svn-id: http://svn.thedarkmod.com/svn/darkmod_src/branches/3rdparty_4822@8144 49c82d7f-2e2a-0410-a16f-ae8f201b507f

--- ThirdParty/2_build_all.py
+++ ThirdParty/2_build_all.py
@@ -11,7 +11,7 @@ def check_msvc_env():
     print("CL compiler: version [%s], arch [%s]" % res)
     return res
 
-def build_arch(compiler, arch, *, libcxx = None, runtime = None, buildtype = 'Release'):
+def build_arch(compiler, arch, *, libcxx = None, runtime = None, buildtype = 'Release', options = {}):
     cmd = 'conan install . -s compiler="%s" -s arch="%s"' % (compiler, arch)
     if libcxx is not None:
         cmd += ' -s compiler.libcxx="%s"' % libcxx
@@ -19,6 +19,8 @@ def build_arch(compiler, arch, *, libcxx = None, runtime = None, buildtype = 'Re
         cmd += ' -s compiler.runtime=%s' % runtime
     if buildtype is not None:
         cmd += ' -s build_type=%s' % buildtype
+    for k,v in options.items():
+        cmd += ' -o %s=%s' % (str(k), str(v))
     cmd += ' --build outdated'
     print("CMD: %s" % cmd)
     yesno = input('continue? (yes/no):')
@@ -32,10 +34,9 @@ assert platform.machine().endswith('64'), "Use 64-bit OS for builds"
 sysname = platform.system().lower()
 if 'windows' in sysname:
     is64bit = (check_msvc_env()[1] == 'x64')
-    if is64bit:
-        build_arch('Visual Studio', 'x86_64', runtime='MT') #, buildtype='RelWithDebInfo')
-    else:
-        build_arch('Visual Studio', 'x86', runtime='MT') #, buildtype='RelWithDebInfo')
+    arch = ('x86_64' if is64bit else 'x86')
+    build_arch('Visual Studio', arch, runtime='MT') #, buildtype='RelWithDebInfo')
+    build_arch('Visual Studio', arch, runtime='MTd', buildtype='Debug', options={'with_headeronly':False,'with_releaseonly':False})
 else:
     build_arch('gcc', 'x86_64', libcxx='libstdc++')
     build_arch('gcc', 'x86', libcxx='libstdc++')
--- ThirdParty/conanfile.py
+++ ThirdParty/conanfile.py
@@ -1,6 +1,7 @@
 from conans import ConanFile
 import os
 
+
 def get_build_name(settings, shared=False):
     os = {'Windows': 'win', 'Linux': 'lnx'}[str(settings.os)]
     bitness = {'x86': '32', 'x86_64': '64'}[str(settings.arch)]
@@ -16,22 +17,55 @@ def get_build_name(settings, shared=False):
         stdlib = {'libstdc++': 'stdcpp'}[str(settings.compiler.libcxx)]
     return '%s%s_%s_%s_%s_%s' % (os, bitness, dynamic, compiler, buildtype, stdlib)
 
+
 class TdmDepends(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
-    requires = (
+    options = {
+        "with_headeronly": [True, False],
+        "with_releaseonly": [True, False],
+        "with_perbuild": [True, False],
+    }
+    default_options = {
+        "with_headeronly": True,
+        "with_releaseonly": True,
+        "with_perbuild": True,
+        # build minizip too (it is part of zlib package)
+        "zlib:minizip": True,
+    }
+
+    # these deps are header-only, hence they don't need per-configuration builds
+    requires_headeronly = (
+        "tinyformat/2.1.0@thedarkmod/local",
+        "doctest/2.2.3@thedarkmod/local",
+    )
+    # these deps can be built only once in release mode, and then used both in release and debug builds
+    # this is possible only for pure C libraries built with /MT, and needs /nodefaultlib:LIBCMT.lib flag in debug build
+    requires_releaseonly = (
         "zlib/1.2.11@conan/stable",
-        "pugixml/1.9@bincrafters/stable",
+        "libcurl/7.61.1@thedarkmod/local",
+        "devil/1.7.8@thedarkmod/local",
         "libjpeg/9c@thedarkmod/local",
         "libpng/1.6.34@bincrafters/stable",
-        "devil/1.7.8@thedarkmod/local",
-        "openal/1.19.0@bincrafters/stable",
-        "libcurl/7.61.1@thedarkmod/local",
         "ffmpeg/4.0.2@thedarkmod/local",
+        "openal/1.19.0@bincrafters/stable",
         "vorbis/1.3.6@bincrafters/stable",
-        "tinyformat/2.1.0@thedarkmod/local",
-        "doctest/2.2.0@bincrafters/stable",
     )
-    default_options = {"zlib:minizip": True}
+    # these deps must be built separately for each configuration (both debug and release)
+    # this is required for C++ libs because iterator debugging and runtime differences are not allowed by MSVC
+    requires_perbuild = (
+        "pugixml/1.9@bincrafters/stable",
+    )
+
+    def requirements(self):
+        if self.options.with_headeronly:
+            for dep in self.requires_headeronly:
+                self.requires.add(dep)
+        if self.options.with_releaseonly:
+            for dep in self.requires_releaseonly:
+                self.requires.add(dep)
+        if self.options.with_perbuild:
+            for dep in self.requires_perbuild:
+                self.requires.add(dep)
 
     def imports(self):
         platform = get_build_name(self.settings, False)
@@ -42,6 +76,6 @@ class TdmDepends(ConanFile):
             self.copy("*.h"  , root_package=name, src="include" , dst="artefacts/%s/include" % name)
             self.copy("*.hpp", root_package=name, src="include" , dst="artefacts/%s/include" % name)
             self.copy("*"    , root_package=name, src="licenses", dst="artefacts/%s/licenses" % name)
-            # compiled binaries are put under compiler/arch subdirectory
+            # compiled binaries are put under subdirectory named by build settings
             self.copy("*.lib", root_package=name, src="lib"     , dst="artefacts/%s/lib/%s" % (name, platform))
             self.copy("*.a"  , root_package=name, src="lib"     , dst="artefacts/%s/lib/%s" % (name, platform))
