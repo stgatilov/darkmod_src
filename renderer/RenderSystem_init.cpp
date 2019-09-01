1631e175d0f34225762dc80ee882d8b3732d87fc
duzenko <duzenko@49c82d7f-2e2a-0410-a16f-ae8f201b507f>
2019-09-01 07:24:49 +0000

    We should be using the new uniform MVP matrices from now on, as well as the GLSL shader programs replacing ARB2 Else depth fighting
    git-svn-id: http://svn.thedarkmod.com/svn/darkmod_src/trunk@8280 49c82d7f-2e2a-0410-a16f-ae8f201b507f

--- renderer/RenderSystem_init.cpp
+++ renderer/RenderSystem_init.cpp
@@ -240,8 +240,8 @@ idCVar r_postprocess_bloomKernelSize( "r_postprocess_bloomKernelSize", "2", CVAR
 idCVar r_useAnonreclaimer( "r_useAnonreclaimer", "0", CVAR_RENDERER | CVAR_BOOL | CVAR_ARCHIVE, "test anonreclaimer patch" );
 idCVarBool r_useGLSL( "r_useGLSL", "1", CVAR_RENDERER | CVAR_ARCHIVE, "Use GLSL shaders instead of ARB2" );
 //stgatilov: temporary cvar, to be removed when ARB->GLSL migration is complete and settled
-idCVar r_forceGlslPrograms( "r_forceGlslPrograms", "0", CVAR_RENDERER | CVAR_BOOL | CVAR_ARCHIVE, "Force using GLSL shaders instead of ARB ones (when shader is used in material's new stage)" );
-idCVar r_uniformTransforms( "r_uniformTransforms", "0", CVAR_RENDERER | CVAR_BOOL | CVAR_ARCHIVE, "Use uniform variables in shaders for vertex transformations instead of the deprecated ftransform" );
+idCVar r_forceGlslPrograms( "r_forceGlslPrograms", "1", CVAR_RENDERER | CVAR_BOOL | CVAR_ARCHIVE, "Force using GLSL shaders instead of ARB ones (when shader is used in material's new stage)" );
+idCVar r_uniformTransforms( "r_uniformTransforms", "1", CVAR_RENDERER | CVAR_BOOL | CVAR_ARCHIVE, "Use uniform variables in shaders for vertex transformations instead of the deprecated ftransform" );
 idCVarBool r_newFrob( "r_newFrob", "0", CVAR_RENDERER | CVAR_ARCHIVE, "1 = use the frob shader instead of material stages" );
 
 // FBO
