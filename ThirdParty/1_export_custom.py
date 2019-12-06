717702e849d8303137209eac818354ae4033a045
stgatilov <stgatilov@49c82d7f-2e2a-0410-a16f-ae8f201b507f>
2019-12-06 15:55:44 +0000

    Tiny print fix.
    git-svn-id: http://svn.thedarkmod.com/svn/darkmod_src/trunk@8439 49c82d7f-2e2a-0410-a16f-ae8f201b507f

--- ThirdParty/1_export_custom.py
+++ ThirdParty/1_export_custom.py
@@ -6,7 +6,7 @@ print('\n'.join(['  ' + fn for fn in custom_recipes_list]))
 
 conan_user_home = os.getenv('CONAN_USER_HOME')
 print("Current conan cache: %s" % (conan_user_home if conan_user_home is not None else '[system-wide]'))
-yn = input("Do you really want to export them all to %s (yes/no): ")
+yn = input("Do you really want to export them all (yes/no): ")
 if yn != 'yes':
     sys.exit(1)
 
