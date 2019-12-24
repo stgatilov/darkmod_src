6e6fa6cd396b8aeb9b04bc1b9b65f52cad8b2c4c
duzenko <duzenko@49c82d7f-2e2a-0410-a16f-ae8f201b507f>
2019-12-24 17:19:05 +0000

    Added purgeimages console command
    git-svn-id: http://svn.thedarkmod.com/svn/darkmod_src/trunk@8462 49c82d7f-2e2a-0410-a16f-ae8f201b507f

--- renderer/RenderSystem_init.cpp
+++ renderer/RenderSystem_init.cpp
@@ -1494,6 +1494,10 @@ static void GfxInfo_f( const idCmdArgs &args ) {
 #endif
 }
 
+void R_PurgeImages_f( const idCmdArgs& args ) {
+	globalImages->PurgeAllImages();
+}
+
 /*
 =================
 R_VidRestart_f
@@ -1700,6 +1704,7 @@ void R_InitCommands( void ) {
 	cmdSystem->AddCommand( "listRenderEntityDefs", R_ListRenderEntityDefs_f, CMD_FL_RENDERER, "lists the entity defs" );
 	cmdSystem->AddCommand( "listRenderLightDefs", R_ListRenderLightDefs_f, CMD_FL_RENDERER, "lists the light defs" );
 	cmdSystem->AddCommand( "reloadSurface", R_ReloadSurface_f, CMD_FL_RENDERER, "reloads the decl and images for selected surface" );
+	cmdSystem->AddCommand( "purgeimages", R_PurgeImages_f, CMD_FL_RENDERER, "deletes all currently loaded images" );
 }
 
 /*
