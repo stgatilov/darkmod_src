3819d9a36fd5d32d29e034515597f422a373e832
stgatilov <stgatilov@49c82d7f-2e2a-0410-a16f-ae8f201b507f>
2020-08-22 17:53:30 +0000

    #5170. Supposedly fixed Linux build (thanks r.a.sattarov)
    git-svn-id: http://svn.thedarkmod.com/svn/darkmod_src/trunk@8943 49c82d7f-2e2a-0410-a16f-ae8f201b507f

--- framework/LoadStack.cpp
+++ framework/LoadStack.cpp
@@ -13,25 +13,25 @@ void LoadStack::Clear() {
 	memset(this, 0, sizeof(*this));
 }
 
-template<> static LoadStack::Level LoadStack::LevelOf(idDecl *ptr) {
+template<> LoadStack::Level LoadStack::LevelOf(idDecl *ptr) {
 	return Level{tDecl, ptr};
 }
-template<> static LoadStack::Level LoadStack::LevelOf(idImage *ptr) {
+template<> LoadStack::Level LoadStack::LevelOf(idImage *ptr) {
 	return Level{tImage, ptr};
 }
-template<> static LoadStack::Level LoadStack::LevelOf(idSoundSample *ptr) {
+template<> LoadStack::Level LoadStack::LevelOf(idSoundSample *ptr) {
 	return Level{tSoundSample, ptr};
 }
-template<> static LoadStack::Level LoadStack::LevelOf(idRenderModel *ptr) {
+template<> LoadStack::Level LoadStack::LevelOf(idRenderModel *ptr) {
 	return Level{tModel, ptr};
 }
-template<> static LoadStack::Level LoadStack::LevelOf(idEntity *ptr) {
+template<> LoadStack::Level LoadStack::LevelOf(idEntity *ptr) {
 	return Level{tEntity, ptr};
 }
-template<> static LoadStack::Level LoadStack::LevelOf(idMapEntity *ptr) {
+template<> LoadStack::Level LoadStack::LevelOf(idMapEntity *ptr) {
 	return Level{tMapEntity, ptr};
 }
-template<> static LoadStack::Level LoadStack::LevelOf(idWindow *ptr) {
+template<> LoadStack::Level LoadStack::LevelOf(idWindow *ptr) {
 	return Level{tWindow, ptr};
 }
 
