a702e49a88a7170a6c1dc4522b64686c48af7351
stgatilov <stgatilov@49c82d7f-2e2a-0410-a16f-ae8f201b507f>
2020-12-12 04:45:09 +0000

    Removed debug output from frob shader.
    git-svn-id: http://svn.thedarkmod.com/svn/darkmod_src/trunk@9037 49c82d7f-2e2a-0410-a16f-ae8f201b507f

--- glprogs/frob.fs
+++ glprogs/frob.fs
@@ -19,7 +19,7 @@ void main() {
     vec3 normal = normalize(var_normal);
     float x = dot(normal, viewDir); 
 	draw_Color.rgb = u_pulse * vec3(1-x);
-    draw_Color.a = 1;    
-	draw_Color.rgb = abs(var_normal);
+    draw_Color.a = 1;
+//	draw_Color.rgb = abs(var_normal);
 	//draw_Color.rgb = vec3(1,0,0);
 }
