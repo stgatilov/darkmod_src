d1f7a1bbe736e2d4dac59f7f8924a0a3e311cb6c
stgatilov <stgatilov@49c82d7f-2e2a-0410-a16f-ae8f201b507f>
2020-12-27 10:26:24 +0000

    s_drawSounds now prints volumeLoss too, plus distance is printed with one digit after dot.
    git-svn-id: http://svn.thedarkmod.com/svn/darkmod_src/trunk@9054 49c82d7f-2e2a-0410-a16f-ae8f201b507f

--- sound/snd_world.cpp
+++ sound/snd_world.cpp
@@ -1299,7 +1299,7 @@ void idSoundWorldLocal::ForegroundUpdate( int current44kHzTime ) {
 				// draw the index
 				idVec3	textPos = def->origin;
 				textPos[2] -= 8;
-				rw->DrawText( va("%i", def->index), textPos, 0.1f, idVec4(1,0,0,1), listenerAxis );
+				rw->DrawText( va("%i (%.1f)", def->index, def->volumeLoss), textPos, 0.1f, idVec4(1,0,0,1), listenerAxis );
 				textPos[2] += 8;
 
 				// run through all the channels
@@ -1315,8 +1315,8 @@ void idSoundWorldLocal::ForegroundUpdate( int current44kHzTime ) {
 					float	min = chan->parms.minDistance;
 					float	max = chan->parms.maxDistance;
 					const char	*defaulted = chan->leadinSample->defaultSound ? "(DEFAULTED)" : "";
-					sprintf( text, "%s (%i/%i %i/%i)%s", chan->soundShader->GetName(), (int)def->distance,
-						(int)def->realDistance, (int)min, (int)max, defaulted );
+					sprintf( text, "%s (%.1f/%.1f %.0f/%.0f)%s", chan->soundShader->GetName(), def->distance,
+						def->realDistance, min, max, defaulted );
 					rw->DrawText( text, textPos, 0.1f, idVec4(1,0,0,1), listenerAxis );
 					textPos[2] += 8;
 				}
