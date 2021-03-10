63f2aeefd1736cc8f75c751d1bdbdab486436606
stgatilov <stgatilov@49c82d7f-2e2a-0410-a16f-ae8f201b507f>
2021-03-10 05:04:15 +0000

    #5562. Preliminary change: added PlaneSet.cpp and moved implementation code there.
    git-svn-id: http://svn.thedarkmod.com/svn/darkmod_src/trunk@9201 49c82d7f-2e2a-0410-a16f-ae8f201b507f

---
+++ idlib/containers/PlaneSet.cpp
@@ -0,0 +1,33 @@
+#include "precompiled.h"
+#include "PlaneSet.h"
+
+
+int idPlaneSet::FindPlane( const idPlane &plane, const float normalEps, const float distEps ) {
+	int i, border, hashKey;
+
+	assert( distEps <= 0.125f );
+
+	hashKey = (int)( idMath::Fabs( plane.Dist() ) * 0.125f );
+	for ( border = -1; border <= 1; border++ ) {
+		for ( i = hash.First( hashKey + border ); i >= 0; i = hash.Next( i ) ) {
+			if ( (*this)[i].Compare( plane, normalEps, distEps ) ) {
+				return i;
+			}
+		}
+	}
+
+	if ( plane.Type() >= PLANETYPE_NEGX && plane.Type() < PLANETYPE_TRUEAXIAL ) {
+		Append( -plane );
+		hash.Add( hashKey, Num()-1 );
+		Append( plane );
+		hash.Add( hashKey, Num()-1 );
+		return ( Num() - 1 );
+	}
+	else {
+		Append( plane );
+		hash.Add( hashKey, Num()-1 );
+		Append( -plane );
+		hash.Add( hashKey, Num()-1 );
+		return ( Num() - 2 );
+	}
+}
--- idlib/containers/PlaneSet.h
+++ idlib/containers/PlaneSet.h
@@ -35,34 +35,4 @@ private:
 	idHashIndex				hash;
 };
 
-ID_INLINE int idPlaneSet::FindPlane( const idPlane &plane, const float normalEps, const float distEps ) {
-	int i, border, hashKey;
-
-	assert( distEps <= 0.125f );
-
-	hashKey = (int)( idMath::Fabs( plane.Dist() ) * 0.125f );
-	for ( border = -1; border <= 1; border++ ) {
-		for ( i = hash.First( hashKey + border ); i >= 0; i = hash.Next( i ) ) {
-			if ( (*this)[i].Compare( plane, normalEps, distEps ) ) {
-				return i;
-			}
-		}
-	}
-
-	if ( plane.Type() >= PLANETYPE_NEGX && plane.Type() < PLANETYPE_TRUEAXIAL ) {
-		Append( -plane );
-		hash.Add( hashKey, Num()-1 );
-		Append( plane );
-		hash.Add( hashKey, Num()-1 );
-		return ( Num() - 1 );
-	}
-	else {
-		Append( plane );
-		hash.Add( hashKey, Num()-1 );
-		Append( -plane );
-		hash.Add( hashKey, Num()-1 );
-		return ( Num() - 2 );
-	}
-}
-
 #endif /* !__PLANESET_H__ */
