1a34b8006e7da171727cba44010ee036ce99296e
stgatilov <stgatilov@49c82d7f-2e2a-0410-a16f-ae8f201b507f>
2021-04-14 05:38:48 +0000

    #4550. testSIMD now uses sys->GetClockTicks() to measure time, instead of platform-specific hacks.
    git-svn-id: http://svn.thedarkmod.com/svn/darkmod_src/trunk@9269 49c82d7f-2e2a-0410-a16f-ae8f201b507f

--- idlib/math/Simd.cpp
+++ idlib/math/Simd.cpp
@@ -142,179 +142,9 @@ idSIMDProcessor *p_simd;
 idSIMDProcessor *p_generic;
 int baseClocks = 0;
 
-#if defined(_MSC_VER) && defined(_M_IX86)
-
-#define TIME_TYPE int
-
-#pragma warning(disable : 4731)     // frame pointer register 'ebx' modified by inline assembly code
-
-int saved_ebx = 0;
-
-#define StartRecordTime( start )			\
-	__asm mov saved_ebx, ebx				\
-	__asm xor eax, eax						\
-	__asm cpuid								\
-	__asm rdtsc								\
-	__asm mov start, eax					\
-	__asm xor eax, eax						\
-	__asm cpuid
-
-#define StopRecordTime( end )				\
-	__asm xor eax, eax						\
-	__asm cpuid								\
-	__asm rdtsc								\
-	__asm mov end, eax						\
-	__asm mov ebx, saved_ebx				\
-	__asm xor eax, eax						\
-	__asm cpuid
-
-#elif MACOS_X
-
-#include <stdlib.h>
-#include <unistd.h>			// this is for sleep()
-#include <sys/time.h>
-#include <sys/resource.h>
-#include <mach/mach_time.h>
-
-double ticksPerNanosecond;
-
-#define TIME_TYPE uint64_t
-
-#ifdef __MWERKS__ //time_in_millisec is missing
-/*
-
-    .text
-	.align 2
-	.globl _GetTB
-_GetTB:
-
-loop:
-	        mftbu   r4	;  load from TBU
-	        mftb    r5	;  load from TBL
-	        mftbu   r6	;  load from TBU
-	        cmpw    r6, r4	;  see if old == new
-	        bne     loop	;  if not, carry occured, therefore loop
-
-	        stw     r4, 0(r3)
-	        stw     r5, 4(r3)
-
-done:
-	        blr		;  return
-
-*/
-typedef struct {
-	unsigned int hi;
-	unsigned int lo;
-} U64;
-
-
-asm void GetTB( U64 *in ) {
-	nofralloc			// suppress prolog
-	machine 603			// allows the use of mftb & mftbu functions
-
-loop:
-	mftbu	r5			// grab the upper time base register (TBU)
-	mftb	r4			// grab the lower time base register (TBL)
-	mftbu	r6			// grab the upper time base register (TBU) again
-
-	cmpw	r6, r5		// see if old TBU == new TBU
-	bne -	loop		// loop if carry occurred (predict branch not taken)
-
-	stw  	r4, 4( r3 )	// store TBL in the low 32 bits of the return value
-	stw  	r5, 0( r3 )	// store TBU in the high 32 bits of the return value
-
-	blr
-}
-
-double TBToDoubleNano( U64 startTime, U64 stopTime, double ticksPerNanosecond );
-
-#if __MWERKS__
-asm void GetTB( U64 * );
-#else
-void GetTB( U64 * );
-#endif
-
-double TBToDoubleNano( U64 startTime, U64 stopTime, double ticksPerNanosecond ) {
-#define K_2POWER32 4294967296.0
-#define TICKS_PER_NANOSECOND 0.025
-	double nanoTime;
-	U64 diffTime;
-
-	// calc the difference in TB ticks
-	diffTime.hi = stopTime.hi - startTime.hi;
-	diffTime.lo = stopTime.lo - startTime.lo;
-
-	// convert TB ticks into time
-	nanoTime = ( double )( diffTime.hi ) * ( ( double )K_2POWER32 ) + ( double )( diffTime.lo );
-	nanoTime = nanoTime / ticksPerNanosecond;
-	return ( nanoTime );
-}
-
-TIME_TYPE time_in_millisec( void ) {
-#define K_2POWER32 4294967296.0
-#define TICKS_PER_NANOSECOND 0.025
-
-	U64 the_time;
-	double nanoTime, milliTime;
-
-	GetTB( &the_time );
-
-	// convert TB ticks into time
-	nanoTime = ( double )( the_time.hi ) * ( ( double )K_2POWER32 ) + ( double )( the_time.lo );
-	nanoTime = nanoTime / ticksPerNanosecond;
-
-	// nanoseconds are 1 billionth of a second. I want milliseconds
-	milliTime = nanoTime * 1000000.0;
-
-	printf( "ticks per nanosec -- %lf\n", ticksPerNanosecond );
-	printf( "nanoTime is %lf -- milliTime is %lf -- as int is %i\n", nanoTime, milliTime, ( int )milliTime );
-
-	return ( int )milliTime;
-}
-
-#define StartRecordTime( start )			\
-	start = time_in_millisec();
-
-#define StopRecordTime( end )				\
-	end = time_in_millisec();
-
-
-#else
-#define StartRecordTime( start )			\
-	start = mach_absolute_time();
-
-#define StopRecordTime( end )				\
-	end = mach_absolute_time();
-#endif
-
-#elif defined(_MSC_VER)		//MSVC, but e.g. 64-bit
-
-#define TIME_TYPE int
-
-#define StartRecordTime( start ) {\
-	int temp[4];\
-	__cpuid(temp, 0);\
-	start = (TIME_TYPE)__rdtsc();\
-}
-
-#define StopRecordTime( end ) {\
-	int temp[4];\
-	__cpuid(temp, 0);\
-	end = (TIME_TYPE)__rdtsc();\
-}
-
-#else
-
-#define TIME_TYPE int
-
-#define StartRecordTime( start )			\
-	start = 0;
-
-#define StopRecordTime( end )				\
-	end = 1;
-
-#endif
-
+#define TIME_TYPE double
+#define StartRecordTime( start ) start = sys->GetClockTicks();
+#define StopRecordTime( end ) StartRecordTime( end )
 #define GetBest( start, end, best )			\
 	if ( !best || end - start < best ) {	\
 		best = end - start;					\
@@ -326,7 +156,7 @@ TIME_TYPE time_in_millisec( void ) {
 PrintClocks
 ============
 */
-void PrintClocks( const char *string, const int dataCount, int clocks, int otherClocks = 0 ) {
+void PrintClocks( const char *string, const int dataCount, TIME_TYPE clocks, TIME_TYPE otherClocks = 0 ) {
 	int i;
 
 	idLib::common->Printf( string );
@@ -337,9 +167,9 @@ void PrintClocks( const char *string, const int dataCount, int clocks, int other
 	if ( otherClocks && clocks ) {
 		otherClocks -= baseClocks;
 		int p = ( int )( ( float )( otherClocks - clocks ) * 100.0f / ( float ) otherClocks );
-		idLib::common->Printf( "c = %4d, clcks = %5d, %d%%\n", dataCount, clocks, p );
+		idLib::common->Printf( "c = %4d, clcks = %5d, %d%%\n", int(dataCount), int(clocks), p );
 	} else {
-		idLib::common->Printf( "c = %4d, clcks = %5d\n", dataCount, clocks );
+		idLib::common->Printf( "c = %4d, clcks = %5d\n", int(dataCount), int(clocks) );
 	}
 }
 
