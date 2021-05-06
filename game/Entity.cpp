d77204007d1e6f809aa6947e643dcee249f51d15
stgatilov <stgatilov@49c82d7f-2e2a-0410-a16f-ae8f201b507f>
2021-05-06 12:05:47 +0000

    #3871. Maximum supported number of contacts in idEntity::ActivateContacts reduced back from 128 to 32.
    Also removed memory allocation and zero-filling here.
    
    git-svn-id: http://svn.thedarkmod.com/svn/darkmod_src/trunk@9344 49c82d7f-2e2a-0410-a16f-ae8f201b507f

--- game/Entity.cpp
+++ game/Entity.cpp
@@ -6774,11 +6774,9 @@ idEntity::ActivateContacts
 */
 void idEntity::ActivateContacts()
 {
-
-// nbohr1more: #3871 - increase contact limit to 128 watch for future issues with this limit
-
-	idList<contactInfo_t> contacts;
-	contacts.SetNum( 128, false );
+	// nbohr1more: #3871 - increase contact limit to 128 watch for future issues with this limit
+	// stgatilov: lowered back to 32 --- the same number is now used in physics classes
+	idRaw<contactInfo_t> contacts[32];		//avoid zero-filling
 
 	idVec6 dir;
 	int num;
@@ -6789,7 +6787,7 @@ void idEntity::ActivateContacts()
 
 	if ( clipModel->IsTraceModel() )
 	{
-		num = gameLocal.clip.Contacts( &contacts[0], 128, GetPhysics()->GetOrigin(),dir, CONTACT_EPSILON, clipModel, mat3_identity, CONTENTS_SOLID, this );
+		num = gameLocal.clip.Contacts( contacts[0].Ptr(), 32, GetPhysics()->GetOrigin(),dir, CONTACT_EPSILON, clipModel, mat3_identity, CONTENTS_SOLID, this );
 	}
 	else
 	{
@@ -6798,14 +6796,12 @@ void idEntity::ActivateContacts()
 	
 		idTraceModel trm(GetPhysics()->GetBounds());
 		idClipModel clip(trm);
-		num = gameLocal.clip.Contacts( &contacts[0], 128, GetPhysics()->GetOrigin(),dir, CONTACT_EPSILON, &clip, mat3_identity, CONTENTS_SOLID, this );
+		num = gameLocal.clip.Contacts( contacts[0].Ptr(), 32, GetPhysics()->GetOrigin(),dir, CONTACT_EPSILON, &clip, mat3_identity, CONTENTS_SOLID, this );
 	}
-	
-	contacts.SetNum( num, false );
 
 	for ( int i = 0 ; i < num ; i++ )
 	{
-		idEntity* found = gameLocal.entities[contacts[i].entityNum];
+		idEntity* found = gameLocal.entities[contacts[i].Get().entityNum];
 		if ( found != gameLocal.world )
 		{
 			if ( found && found->IsType(idMoveable::Type) )
