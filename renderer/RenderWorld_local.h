e5a40a406a218eeb0b638b663c87d22cbb4fc715
stgatilov <stgatilov@49c82d7f-2e2a-0410-a16f-ae8f201b507f>
2021-07-18 07:03:45 +0000

    #5597. Removed idDenseHash container.
    Use idHashMap instead.
    
    git-svn-id: http://svn.thedarkmod.com/svn/darkmod_src/trunk@9472 49c82d7f-2e2a-0410-a16f-ae8f201b507f

--- renderer/RenderWorld_local.h
+++ renderer/RenderWorld_local.h
@@ -16,7 +16,6 @@ Project: The Dark Mod (http://www.thedarkmod.com/)
 #ifndef __RENDERWORLDLOCAL_H__
 #define __RENDERWORLDLOCAL_H__
 
-#include "containers/DenseHash.h"
 #include "containers/HashMap.h"
 
 // assume any lightDef or entityDef index above this is an internal error
@@ -116,10 +115,8 @@ private:
 	int useInteractionTable = -1;
 	//r_useInteractionTable = 1: Single Matrix  (light x entity)
 	idInteraction** SM_matrix;
-	//r_useInteractionTable = 2: Single Hash Table (old code)
-	idDenseHash<int, idInteraction*, InterTableHashFunction> SHT_table;
-	//r_useInteractionTable = 3: Single Hdsh Table (new code)
-	idHashMap<int, idInteraction*> SHT_tableNew;
+	//r_useInteractionTable = 2: Single Hash Table
+	idHashMap<int, idInteraction*> SHT_table;
 };
 
 class idRenderWorldLocal : public idRenderWorld {
