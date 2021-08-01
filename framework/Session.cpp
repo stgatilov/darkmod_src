c65db282ca8292d90b0a13d9f269377c5a8e0e9e
cabalistic <cabalistic@49c82d7f-2e2a-0410-a16f-ae8f201b507f>
2021-08-01 10:29:17 +0000

    Immediately use the drawn frame in the backend in non-SMP mode.
    Otherwise, we are rendering one frame late, which is unnecessary.
    Implicitly also fixes #5673.
    
    git-svn-id: http://svn.thedarkmod.com/svn/darkmod_src/trunk@9527 49c82d7f-2e2a-0410-a16f-ae8f201b507f

--- framework/Session.cpp
+++ framework/Session.cpp
@@ -3162,6 +3162,9 @@ void idSessionLocal::ActivateFrontend() {
 		// run game tics and frontend drawing serially
 		RunGameTics();
 		DrawFrame();
+		// since the backend runs after us, we can hand over the frame we just calculated immediately.
+		// avoids an unnecessary frame delay
+		R_ToggleSmpFrame();
 	}
 }
 
