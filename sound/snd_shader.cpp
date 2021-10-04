8dc70ea2aff604a384b15e759592cdb56336c42b
nbohr1more <nbohr1more@49c82d7f-2e2a-0410-a16f-ae8f201b507f>
2021-10-04 21:18:39 +0000

    nbohr1more: improve last commit with idMath::Fmin
    git-svn-id: http://svn.thedarkmod.com/svn/darkmod_src/trunk@9620 49c82d7f-2e2a-0410-a16f-ae8f201b507f

--- sound/snd_shader.cpp
+++ sound/snd_shader.cpp
@@ -228,10 +228,7 @@ bool idSoundShader::ParseShader( idLexer &src ) {
 		}
 		// volume
 		else if ( !token.Icmp( "volume" ) ) {
-			parms.volume = src.ParseFloat();
-            if ( parms.volume > 1.5 ) {
-                parms.volume = 1.5;
-            }
+			parms.volume = idMath::Fmin(1.5f, src.ParseFloat());
 		}
 		// leadinVolume is used to allow light breaking leadin sounds to be much louder than the broken loop
 		else if ( !token.Icmp( "leadinVolume" ) ) {
