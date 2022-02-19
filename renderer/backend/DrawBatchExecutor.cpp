97ff7974c6b56b1aa96c6b5079281ccc1a9f3f0e
stgatilov <stgatilov@49c82d7f-2e2a-0410-a16f-ae8f201b507f>
2022-02-19 11:31:04 +0000

    Merged one commit from trunk:
    r9852
    New backend: don't execute draw calls if either index or vertex cache is missing.
    ---------------------
    
    
    git-svn-id: http://svn.thedarkmod.com/svn/darkmod_src/branches/release210@9853 49c82d7f-2e2a-0410-a16f-ae8f201b507f

--- renderer/backend/DrawBatchExecutor.cpp
+++ renderer/backend/DrawBatchExecutor.cpp
@@ -202,8 +202,13 @@ void DrawBatchExecutor::ExecuteBatch( int numDrawSurfs, int numInstances, GLuint
 		}
 	}
 
-	vertexCache.VertexPosition( attribBind == ATTRIB_REGULAR ? drawSurfs[0]->ambientCache : drawSurfs[0]->shadowCache, attribBind );
-	vertexCache.IndexPosition( drawSurfs[0]->indexCache );
+	const vertCacheHandle_t &vertexHandle = attribBind == ATTRIB_REGULAR ? drawSurfs[0]->ambientCache : drawSurfs[0]->shadowCache;
+	const vertCacheHandle_t &indexHandle = drawSurfs[0]->indexCache;
+	if ( !vertexHandle.IsValid() || !indexHandle.IsValid() )
+		return;		// stgatilov: happens briefly during cache resize
+	vertexCache.VertexPosition( vertexHandle, attribBind );
+	vertexCache.IndexPosition( indexHandle );
+
 	if ( ShouldUseMultiDraw() ) {
 		BatchMultiDraw( numDrawSurfs, numInstances, baseVertexFn );
 	} else {
