0a6c9d66112ab817147c52c1ee3b72d1b1dbf840
stgatilov <stgatilov@49c82d7f-2e2a-0410-a16f-ae8f201b507f>
2022-03-14 17:18:09 +0000

    #5881. tracy package converted to versionless format.
    I did not change version, because there is no official release after 0.7.8 yet.
    I know that the only version on conan-center is from 2022.01 master, which is much newer...
    But I'm hesitant to use anything except for official releases.
    
    Patching was not converted, because both patches are TDM-specific and global:
    1) Add RecreateQueries specifically for TDM (regardless of Tracy version).
    2) Replace glXXX with qglXXX specifically for TDM (regardless of Tracy version) --- better not try to store a patch for this change...
    
    
    git-svn-id: http://svn.thedarkmod.com/svn/darkmod_src/trunk@9870 49c82d7f-2e2a-0410-a16f-ae8f201b507f

--- ThirdParty/custom/tracy/conanfile.py
+++ ThirdParty/custom/tracy/conanfile.py
@@ -4,7 +4,6 @@ import os
 
 class TracyConan(ConanFile):
     name = "tracy"
-    version = "0.7.8"
     license = "BSD-3-Clause"
     author = "Stepan Gatilov stgatilov@gmail.com"
     description = "A real time, nanosecond resolution, remote telemetry, hybrid frame and sampling profiler for games and other applications."
@@ -13,8 +12,7 @@ class TracyConan(ConanFile):
 
     def source(self):
         # Download and extract tag tarball from github
-        source_url = "https://github.com/wolfpld/tracy"
-        tools.get("{0}/archive/refs/tags/v{1}.tar.gz".format(source_url, self.version))
+        tools.get(**self.conan_data["sources"][self.version])
         extracted_dir = "tracy-" + self.version
         os.rename(extracted_dir, "fullsource")
 
