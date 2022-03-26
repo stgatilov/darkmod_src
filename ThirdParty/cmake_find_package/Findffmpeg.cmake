f660425b0b07eb94b1cb1d5fa72e0227246541ef
stgatilov <stgatilov@49c82d7f-2e2a-0410-a16f-ae8f201b507f>
2022-03-26 15:53:24 +0000

    #5881. ffmpeg package updated from 4.0.2 to 4.4.1.
    Took the recipe from conan-center, then reapplied the main adjustments:
      1) disable all except for a tiny subset
      2) disable debug info in release (although it is probably already disabled)
    
    
    git-svn-id: http://svn.thedarkmod.com/svn/darkmod_src/trunk@9880 49c82d7f-2e2a-0410-a16f-ae8f201b507f

--- ThirdParty/cmake_find_package/Findffmpeg.cmake
+++ ThirdParty/cmake_find_package/Findffmpeg.cmake
@@ -5,11 +5,11 @@ set(ffmpeg_INCLUDE_DIRS "${ARTEFACTS_DIR}/ffmpeg/include")
 set(ffmpeg_LIBRARY_DIR "${ARTEFACTS_DIR}/ffmpeg/lib/${PACKAGE_PLATFORM}")
 if(MSVC)
 	set(ffmpeg_LIBRARIES
-		"${ffmpeg_LIBRARY_DIR}/libavformat.lib"
-		"${ffmpeg_LIBRARY_DIR}/libavcodec.lib"
-		"${ffmpeg_LIBRARY_DIR}/libavutil.lib"
-		"${ffmpeg_LIBRARY_DIR}/libswresample.lib"
-		"${ffmpeg_LIBRARY_DIR}/libswscale.lib"
+		"${ffmpeg_LIBRARY_DIR}/avformat.lib"
+		"${ffmpeg_LIBRARY_DIR}/avcodec.lib"
+		"${ffmpeg_LIBRARY_DIR}/avutil.lib"
+		"${ffmpeg_LIBRARY_DIR}/swresample.lib"
+		"${ffmpeg_LIBRARY_DIR}/swscale.lib"
 	)
 else()
 	set(ffmpeg_LIBRARIES
