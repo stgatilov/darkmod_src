035ce9e63d3a6fddb3a9cdc56208e20b56f56ed0
stgatilov <stgatilov@49c82d7f-2e2a-0410-a16f-ae8f201b507f>
2023-01-19 19:56:37 +0000

    tdm_installer: version bump.
    git-svn-id: http://svn.thedarkmod.com/svn/darkmod_src/trunk@10253 49c82d7f-2e2a-0410-a16f-ae8f201b507f

--- tdm_installer/Constants.h
+++ tdm_installer/Constants.h
@@ -14,7 +14,7 @@ Project: The Dark Mod (http://www.thedarkmod.com/)
 ******************************************************************************/
 #pragma once
 
-#define TDM_INSTALLER_VERSION "1.10"
+#define TDM_INSTALLER_VERSION "1.11"
 
 #define TDM_INSTALLER_LOG_FORMAT "tdm_installer_%lld.log"
 #define TDM_INSTALLER_CONFIG_FILENAME "tdm_installer.ini"
