cda1729f1096a7ee3aa855d9d7510df559db652a
nbohr1more <nbohr1more@49c82d7f-2e2a-0410-a16f-ae8f201b507f>
2025-03-28 18:17:47 +0000

    nbohr1more: Port Dhewm3 fix for model handle corruption on restore
    See:
    
    https://forums.thedarkmod.com/index.php?/topic/22673-214-fix-possible-handle-corruption-on-savegame-restore/
    
    git-svn-id: http://svn.thedarkmod.com/svn/darkmod_src/trunk@10952 49c82d7f-2e2a-0410-a16f-ae8f201b507f

--- game/Moveable.cpp
+++ game/Moveable.cpp
@@ -1227,6 +1227,21 @@ void idExplodingBarrel::Restore( idRestoreGame *savefile ) {
 	savefile->ReadInt( particleTime );
 	savefile->ReadInt( lightTime );
 	savefile->ReadFloat( time );
+
+	// DG: enforce getting fresh handle, else this may be tied to an unrelated light!
+
+	if ( lightDefHandle != -1 ) {
+
+		lightDefHandle = gameRenderWorld->AddLightDef( &light );
+
+	}
+
+	// DG: same for render entity
+
+	if ( particleModelDefHandle != -1 ) {
+
+		particleModelDefHandle = gameRenderWorld->AddEntityDef( &particleRenderEntity );
+	}
 }
 
 /*
--- game/Weapon.cpp
+++ game/Weapon.cpp
@@ -455,11 +455,31 @@ void idWeapon::Restore( idRestoreGame *savefile ) {
 	savefile->ReadInt( guiLightHandle );
 	savefile->ReadRenderLight( guiLight );
 
+	// DG: we need to get a fresh handle, otherwise this will be tied to a completely unrelated light!
+
+	if ( guiLightHandle != -1 ) {
+
+		guiLightHandle = gameRenderWorld->AddLightDef( &guiLight );
+
+	}
+
 	savefile->ReadInt( muzzleFlashHandle );
 	savefile->ReadRenderLight( muzzleFlash );
 
+	if ( muzzleFlashHandle = -1 ) { // DG: enforce getting fresh handle
+
+		muzzleFlashHandle = gameRenderWorld->AddLightDef( &muzzleFlash );
+
+	}
+
 	savefile->ReadInt( worldMuzzleFlashHandle );
 	savefile->ReadRenderLight( worldMuzzleFlash );
+
+	if ( worldMuzzleFlashHandle != -1 ) { // DG: enforce getting fresh handle
+
+		worldMuzzleFlashHandle = gameRenderWorld->AddLightDef( &worldMuzzleFlash );
+
+	}
 
 	savefile->ReadVec3( flashColor );
 	savefile->ReadInt( muzzleFlashEnd );
@@ -515,6 +535,12 @@ void idWeapon::Restore( idRestoreGame *savefile ) {
 
 	savefile->ReadInt( nozzleGlowHandle );
 	savefile->ReadRenderLight( nozzleGlow );
+
+	if ( nozzleGlowHandle != -1 ) { // DG: enforce getting fresh handle
+
+		nozzleGlowHandle = gameRenderWorld->AddLightDef( &nozzleGlow );
+
+	}
 
 	savefile->ReadVec3( nozzleGlowColor );
 	savefile->ReadMaterial( nozzleGlowShader );
