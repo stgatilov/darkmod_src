c84aef2a06f3cf096f6dd435dd5dc89ccdf36652
nbohr1more <nbohr1more@49c82d7f-2e2a-0410-a16f-ae8f201b507f>
2025-04-18 23:11:15 +0000

    Dhewm3 Port: Reduce compiler warnings for unitialized AI and Light Bounds checking ( also optimized checkbounds bool behavior )
    See:
    
    https://github.com/dhewm/dhewm3/commit/6730ddcb2918a3b09c704b36f2d964b20ce02752
    
    git-svn-id: http://svn.thedarkmod.com/svn/darkmod_src/trunk@10985 49c82d7f-2e2a-0410-a16f-ae8f201b507f

--- renderer/frontend/tr_light.cpp
+++ renderer/frontend/tr_light.cpp
@@ -1117,8 +1117,9 @@ R_IssueEntityDefCallback
 bool R_IssueEntityDefCallback( idRenderEntityLocal *def ) {
 	bool update;
 	idBounds	oldBounds;
+	const bool checkBounds = r_checkBounds.GetBool();
 
-	if ( r_checkBounds.GetBool() ) {
+	if ( checkBounds ) {
 		oldBounds = def->referenceBounds;
 	}
 	def->archived = false;		// will need to be written to the demo file
@@ -1135,7 +1136,7 @@ bool R_IssueEntityDefCallback( idRenderEntityLocal *def ) {
 		common->Error( "R_IssueEntityDefCallback: dynamic entity callback didn't set model" );
 	}
 
-	if ( r_checkBounds.GetBool() ) {
+	if ( checkBounds ) {
 		if (	oldBounds[0][0] > def->referenceBounds[0][0] + CHECK_BOUNDS_EPSILON ||
 				oldBounds[0][1] > def->referenceBounds[0][1] + CHECK_BOUNDS_EPSILON ||
 				oldBounds[0][2] > def->referenceBounds[0][2] + CHECK_BOUNDS_EPSILON ||
