a509ba20d588caf7a635aa27dc9dc7e6759da31c
stgatilov <stgatilov@49c82d7f-2e2a-0410-a16f-ae8f201b507f>
2026-03-28 19:19:03 +0000

    Deleted some old mess for AppVeyor, hopefully today it works without it.
    git-svn-id: http://svn.thedarkmod.com/svn/darkmod_src/branches/appveyor3@11236 49c82d7f-2e2a-0410-a16f-ae8f201b507f

--- CiScripts/linux_install.py
+++ CiScripts/linux_install.py
@@ -10,5 +10,3 @@ if 'windows' not in sysname:
     # this is enough for 64 bit 
     os.system('sudo apt-get update')
     os.system('sudo apt-get -y install mesa-common-dev libglu1-mesa-dev xorg-dev libxcb-*-dev libx11-xcb-dev libxxf86vm-dev libxext-dev uuid-dev')
-    # workaround for FLTK build broken due to: https://github.com/appveyor/ci/issues/3842
-    os.system('sudo rm -f /usr/local/bin/doxygen')
