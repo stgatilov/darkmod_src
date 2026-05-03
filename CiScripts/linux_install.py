6a15166b97fd64c261acc99a6b6f8fdb19a23cf7
stgatilov <stgatilov@49c82d7f-2e2a-0410-a16f-ae8f201b507f>
2026-05-03 09:24:44 +0000

    Linux CI: Fixed missing "install" after apt-get =)
    git-svn-id: http://svn.thedarkmod.com/svn/darkmod_src/trunk@11318 49c82d7f-2e2a-0410-a16f-ae8f201b507f

--- CiScripts/linux_install.py
+++ CiScripts/linux_install.py
@@ -2,13 +2,13 @@ import os, platform, sys
 
 def execute(cmd):
     print("CMD: " + cmd)
-    assert os.system(cmd) == 0
+    os.system(cmd)  # errors ignored
 
 
 sysname = platform.system().lower()
 if 'windows' not in sysname:
     # this is enough for 64 bit 
-    os.system('sudo apt-get update')
-    os.system('sudo apt-get -y install xorg-dev libwayland-dev libxkbcommon-dev')
-    os.system('sudo apt-get -y mesa-common-dev libglu1-mesa-dev')
-    os.system('sudo apt-get -y libxcb-*-dev libx11-xcb-dev libxxf86vm-dev libxext-dev uuid-dev')
+    execute('sudo apt-get update')
+    execute('sudo apt-get -y install xorg-dev libwayland-dev libxkbcommon-dev')
+    execute('sudo apt-get -y install mesa-common-dev libglu1-mesa-dev')
+    execute('sudo apt-get -y install libxcb-*-dev libx11-xcb-dev libxxf86vm-dev libxext-dev uuid-dev')
